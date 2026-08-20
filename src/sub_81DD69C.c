90a30a71d5ec60961a143a570e5be962c170423e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-19 19:22:04 -0600

    Match sub_81DD69C
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_81DD69C.c
+++ src/sub_81DD69C.c
@@ -7,9 +7,6 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81DD69C.s\"");
-#else
 u32* sub_81DD69C(u32* param_1) {
     u32* puVar1;
     
@@ -20,4 +17,3 @@ u32* sub_81DD69C(u32* param_1) {
     param_1[0x08/4] = 0;
     return param_1;
 }
-#endif
