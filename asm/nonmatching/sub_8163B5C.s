7caa059bec1a182f8f4581203d4ee833b670c42c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:08:11 -0600

    Match sub_8163B5C
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- asm/nonmatching/sub_8163B5C.s
+++
@@ -1,7 +0,0 @@
-	.syntax unified
-	.text
-
-	thumb_func_start sub_8163B5C
-sub_8163B5C:
-	str r1, [r0, #0x64]
-	bx lr
--- src/sub_8163B5C.c
+++ src/sub_8163B5C.c
@@ -7,11 +7,6 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8163B5C.s\"");
-#else
-u32* sub_8163B5C(u32* param_1, u32 param_2) {
-    *param_1 = param_2;
-    return param_1;
+void sub_8163B5C(void *arg0, s32 arg1) {
+    (*(s32 *)((s8 *)(arg0) + (0x64))) = arg1;
 }
-#endif
