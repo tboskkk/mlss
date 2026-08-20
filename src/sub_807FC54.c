1a716edd47ca46e97ea95fc90f0803ee6b858e56
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 16:59:50 -0600

    Match sub_807FC54
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_807FC54.c
+++ src/sub_807FC54.c
@@ -7,30 +7,8 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_807FC54.s\"");
-#else
-void sub_807FC54(u32 a0, u32 a1, u32 a2, u32 a3)
-{
-    u32 *r4;
-    u32 *r5;
-    u32 *r6;
-    u32 *r0;
-    u32 *r1;
-    u32 *r2;
-    u32 *r3;
-    
-    r4 = (u32*)a0;
-    r5 = (u32*)a1;
-    r6 = (u32*)a2;
-    r0 = (u32*)0x03000FD8;
-    r0 = (u32*)r0[0];
-    r0 = (u32*)(r0 + 0x93 * 4);
-    r0 = (u32*)r0[0];
-    r1 = (u32*)r0;
-    r2 = (u32*)r1;
-    r3 = (u32*)r2;
-    
-    sub_8082CCC(r4, r5, r6);
+s32 sub_8082CCC(s32, s32, s32, s32, s32, s32);  /* extern */
+
+void sub_807FC54(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4) {
+    sub_8082CCC((*(s32 *)((s8 *)(*(void **)0x03000FD8) + (0x24C))), arg0, arg1, arg2, arg3, arg4);
 }
-#endif
