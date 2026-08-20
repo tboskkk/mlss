e48c5991a88b3dca1913a08a6dfe16073da107f4
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:13:34 -0600

    Match sub_80679FC
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80679FC.c
+++ src/sub_80679FC.c
@@ -7,8 +7,15 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80679FC.s\"");
-#else
-#error "TODO: write sub_80679FC to match asm/nonmatching/sub_80679FC.s, then delete this #error"
-#endif
+extern s32 sub_8067A4C;
+
+void sub_80679FC(void *arg0) {
+    s32 temp_r0_11;
+
+    temp_r0_11 = (*(s32 *)((s8 *)(arg0) + (0x84))) - 1;
+    (*(s32 *)((s8 *)(arg0) + (0x84))) = temp_r0_11;
+    if (temp_r0_11 <= 0) {
+        (*(s32 *)((s8 *)(arg0) + (0x8C))) = 0x300;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8067A4C;
+    }
+}
