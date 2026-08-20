f39f4150b248290dcd1dfd0c4ee6179d5659378a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:50:03 -0600

    Match sub_8066540
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8066540.c
+++ src/sub_8066540.c
@@ -7,15 +7,15 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8066540.s\"");
-#else
-void sub_8066540(void *arg0, s32 arg1, s32 arg2, s32 arg3) {
-    void *temp_r0_7;
-
-    temp_r0_7 = arg0 + 0x84;
-    (*(s32 *)((s8 *)(arg0) + (0x84))) = arg1;
-    (*(s32 *)((s8 *)(temp_r0_7) + (4))) = arg2;
-    (*(s32 *)((s8 *)((temp_r0_7 + 4)) + (4))) = arg3;
+void sub_8066540(void *arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+  void *new_var;
+  int new_var2;
+  void *temp_r0_7;
+  temp_r0_7 = arg0 + 0x84;
+  new_var = arg0;
+  *((s32 *) (((s8 *) new_var) + 0x84)) = arg1;
+  new_var2 = 4;
+  *((s32 *) (((s8 *) temp_r0_7) + new_var2)) = arg2;
+  *((s32 *) (((s8 *) (temp_r0_7 + new_var2)) + new_var2)) = arg3;
 }
-#endif
