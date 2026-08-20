e4ff779051e9df961a4941662f71d11bbe8d48f7
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 11:49:03 -0600

    Match sub_8082158
    Factory pipeline (tools/factory), source=bench:m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8082158.c
+++ src/sub_8082158.c
@@ -7,23 +7,15 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8082158.s\"");
-#else
-extern void sub_8080A40();
+extern s32 sub_8080A40;
 
-void sub_8082158(void* p0) {
-    void** ptr = (void**)((u8*)p0 + 0x0C);
-    u32* r4 = *(u32**)ptr;
-    u32* r1 = (u32*)((u8*)r4 + 0x4C);
-    if (*r1 == 0) {
-        void (*func)(void) = (void (*)(void))*r1;
-        func();
-        r1 = (u32*)((u8*)r4 + 0x4C);
-        if (*r1 != 0) {
-            return;
-        }
+void sub_8082158(void *arg0) {
+    s32 (*temp_r1_9)(void *);
+    void *temp_r4_8;
+
+    temp_r4_8 = (*(void **)((s8 *)(arg0) + (0xC)));
+    temp_r1_9 = (*(s32 (**)(void *))((s8 *)(temp_r4_8) + (0x4C)));
+    if ((temp_r1_9 == NULL) || (temp_r1_9(temp_r4_8), ((*(s32 (**)(void *))((s8 *)(temp_r4_8) + (0x4C))) == NULL))) {
+        (*(s32 **)((s8 *)(arg0) + (4))) = &sub_8080A40;
     }
-    *(u32*)((u8*)p0 + 0x04) = (u32)&sub_8080A40;
 }
-#endif
