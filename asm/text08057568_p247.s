074d6c24075d4030ac01e45cb7216032c0edb88f
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:45:04 -0600

    Extract sub_81DD670
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p247.s
+++ asm/text08057568_p247.s
@@ -41,27 +41,3 @@ _081DD65E:
 	.byte 0x00, 0x00
 _081DD664: .4byte 0x08CDCA30
 	.byte 0x70, 0x47, 0x00, 0x00, 0x70, 0x47, 0x00, 0x00
-	thumb_func_start sub_81DD670
-sub_81DD670:
-	push {lr}
-	adds r2, r0, #0x0
-	ldr r0, _081DD68C @ =0x08CDCF50
-	str r0, [r2, #0x04]
-	movs r0, #0x01
-	ands r0, r1
-	cmp r0, #0x00
-	beq _081DD686
-	adds r0, r2, #0x0
-	bl free_heap_8018DA8
-_081DD686:
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_081DD68C: .4byte 0x08CDCF50
-	thumb_func_start sub_81DD690
-sub_81DD690:
-	push {lr}
-	bl sub_8161330
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
