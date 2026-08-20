b8d11c042d12637ee13e8ea2a0cdba49e34f811d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:26:08 -0600

    Match sub_8150DEC
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- asm/nonmatching/sub_8150DEC.s
+++
@@ -1,7 +0,0 @@
-	.syntax unified
-	.text
-
-	thumb_func_start sub_8150DEC
-sub_8150DEC:
-	ldr r0, [r0, #0x34]
-	bx lr
--- src/sub_8150DE8.c
+++ src/sub_8150DE8.c
@@ -11,10 +11,6 @@ void sub_8150DE8(void *arg0, s32 arg1) {
     (*(s32 *)((s8 *)(arg0) + (0x30))) = arg1;
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8150DEC.s\"");
-#else
 s32 sub_8150DEC(void *arg0) {
     return (*(s32 *)((s8 *)(arg0) + (0x34)));
 }
-#endif
