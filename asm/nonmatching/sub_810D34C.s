1e98e5101e894591ab6e99e4be2d93f8b58d6ab1
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:29:59 -0600

    Match sub_80290CC
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- asm/nonmatching/sub_810D34C.s
+++ asm/nonmatching/sub_810D34C.s
@@ -1,39 +1,37 @@
-	.include "asm/macros.inc"
-
 	.syntax unified
 	.text
 
-	thumb_func_start sub_810D2E0
-sub_810D2E0:
+	thumb_func_start sub_810D34C
+sub_810D34C:
 	push {r4, r5, r6, r7, lr}
 	adds r5, r0, #0x0
 	adds r4, r1, #0x0
 	adds r6, r2, #0x0
 	cmp r3, #0x00
-	beq _0810D2F2
+	beq _0810D35E
 	bl sub_810D260
 	adds r4, r0, #0x0
-_0810D2F2:
+_0810D35E:
 	movs r2, #0xFF
 	ands r2, r6
-	ldr r0, _0810D33C @ =0x0300034C
+	ldr r0, _0810D3A8 @ =0x0300034C
 	movs r3, #0x88
 	lsls r3, r3, #0x04
 	adds r1, r0, r3
 	ldr r1, [r1, #0x00]
 	mov r12, r0
 	movs r7, #0x0F
-	ldr r6, _0810D340 @ =0x000001FF
-	ldr r3, _0810D344 @ =0xFFFF8000
+	ldr r6, _0810D3AC @ =0x000001FF
+	ldr r3, _0810D3B0 @ =0xFFFF8000
 	adds r0, r3, #0x0
 	orrs r2, r0
-	ldr r0, _0810D348 @ =0xFFFFC000
+	ldr r0, _0810D3B4 @ =0xFFFFB000
 	adds r3, r0, #0x0
-_0810D310:
+_0810D37C:
 	adds r0, r5, #0x0
 	ands r0, r7
 	lsls r0, r0, #0x01
-	adds r0, #0x49
+	adds r0, #0xC0
 	subs r4, #0x08
 	ands r4, r6
 	strh r2, [r1, #0x00]
@@ -43,7 +41,7 @@ _0810D310:
 	adds r1, #0x08
 	lsrs r5, r5, #0x04
 	cmp r5, #0x00
-	bne _0810D310
+	bne _0810D37C
 	movs r0, #0x88
 	lsls r0, r0, #0x04
 	add r0, r12
@@ -52,7 +50,7 @@ _0810D310:
 	pop {r0}
 	bx r0
 	.byte 0x00, 0x00
-_0810D33C: .4byte 0x0300034C
-_0810D340: .4byte 0x000001FF
-_0810D344: .4byte 0xFFFF8000
-_0810D348: .4byte 0xFFFFC000
+_0810D3A8: .4byte 0x0300034C
+_0810D3AC: .4byte 0x000001FF
+_0810D3B0: .4byte 0xFFFF8000
+_0810D3B4: .4byte 0xFFFFB000
--- src/sub_80290CC.c
+++ src/sub_80290CC.c
@@ -7,16 +7,11 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80290CC.s\"");
-#else
-u8 sub_80290CC(u8* a, u32 b) {
-    u32 r2 = 0x96 << 1;
-    a += r2;
-    b <<= 2;
-    u8 r3 = *a;
-    u8 r2_new = (r3 & 3) | b;
-    *a = r2_new;
-    return r2_new;
+void sub_80290CC(void *arg0, s32 arg1)
+{
+  u8 *new_var;
+  int new_var2;
+  new_var = (u8 *) (((s8 *) arg0) + 0x12C);
+  new_var2 = arg1 * 4;
+  *new_var = (u8) ((3 & (*((u8 *) (((s8 *) arg0) + 0x12C)))) | new_var2);
 }
-#endif
