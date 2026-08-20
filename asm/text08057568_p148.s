ad6fec297166da2c51833537895c68fda3f0ee1c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 13:50:55 -0600

    Extract sub_80F8A90
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p148.s
+++ asm/text08057568_p148.s
@@ -3,41 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80F8A90
-sub_80F8A90:
-	push {lr}
-	ldr r1, [r2, #0x04]
-	lsls r1, r1, #0x08
-	ldr r0, [r0, #0x2C]
-	adds r0, r0, r1
-	ldr r2, [r2, #0x00]
-	cmp r2, #0x00
-	beq _080F8AA6
-	cmp r2, #0x01
-	beq _080F8AB4
-	b _080F8AC2
-_080F8AA6:
-	movs r1, #0xF4
-	lsls r1, r1, #0x03
-	adds r2, r0, r1
-	ldrh r1, [r2, #0x00]
-	movs r0, #0x04
-	orrs r0, r1
-	b _080F8AC0
-_080F8AB4:
-	movs r1, #0xF4
-	lsls r1, r1, #0x03
-	adds r2, r0, r1
-	ldrh r1, [r2, #0x00]
-	ldr r0, _080F8AC8 @ =0x0000FFFB
-	ands r0, r1
-_080F8AC0:
-	strh r0, [r2, #0x00]
-_080F8AC2:
-	movs r0, #0x01
-	pop {r1}
-	bx r1
-_080F8AC8: .4byte 0x0000FFFB
 	thumb_func_start ybai_script_update_80F8ACC
 ybai_script_update_80F8ACC: @ 080F8ACC
 	push {r4, r5, lr}
--- src/sub_80F8A08.c
+++ src/sub_80F8A08.c
@@ -157,3 +157,9 @@ void sub_80F8A28(u32* p0, u32* p1, u32* p2, s8 p3)
     r1 += r1;
     r1 += r1
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80F8A90.s\"");
+#else
+#error "TODO: write sub_80F8A90 to match asm/nonmatching/sub_80F8A90.s, then delete this #error"
+#endif
