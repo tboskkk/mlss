d11105226c8064f970503b02def49044ec96b40c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:38:17 -0600

    Extract sub_80F5924
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/script_cmd_wait_for_user_input.c
+++ src/script_cmd_wait_for_user_input.c
@@ -10,5 +10,11 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/script_cmd_wait_for_user_input.s\"");
 #else
-#error "TODO: write script_cmd_wait_for_user_input to match asm/nonmatching/script_cmd_wait_for_user_input.s, then delete this #error"
+s32 script_cmd_wait_for_user_input(void *arg1, void *arg2, s32 *arg3) {
+    if (!(((s32) (*(u8 *)((s8 *)((*(void **)((s8 *)(arg1) + (4)))) + (0x1B4))) >> *arg3) & 1)) {
+        return 1;
+    }
+    (*(s32 *)((s8 *)(arg2) + (0))) = (s32) (*(s32 *)((s8 *)(arg2) + (0x14)));
+    return 0;
+}
 #endif
