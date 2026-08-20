047da7035ca2de5c3ff4521dafedbd5b4873184d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:46:41 -0600

    Extract stage_room_solidity_grid
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ src/stage_room_solidity_grid.c
@@ -0,0 +1,14 @@
+#include "global.h"
+#include "common.h"
+
+// TODO: add other includes (a matching *.h, other subsystem headers) as
+// this file grows. Left minimal since split_func.py doesn't know yet what
+// stage_room_solidity_grid needs.
+
+asm_unified(".include \"asm/macros.inc\"");
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/stage_room_solidity_grid.s\"");
+#else
+#error "TODO: write stage_room_solidity_grid to match asm/nonmatching/stage_room_solidity_grid.s, then delete this #error"
+#endif
