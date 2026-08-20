60b13273dae2045a4de3e76e7eab0c8f577801d4
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 15:57:57 -0600

    Match sub_816166C
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_816166C.c
+++ src/sub_816166C.c
@@ -7,13 +7,9 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_816166C.s\"");
-#else
 void sub_816166C(u32* p0, u32 p1)
 {
     *(u32*)((u8*)p0) = 0x08CDD0B8;
     if (p1 & 1)
         free_heap_8018DA8(p0);
 }
-#endif
