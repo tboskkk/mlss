fd141c0c8fb167e6ea86dccbdcaf4f7f6357ef4f
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-19 19:21:51 -0600

    Match sub_8136688
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8136688.c
+++ src/sub_8136688.c
@@ -7,13 +7,9 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8136688.s\"");
-#else
 u32* sub_8136688(u32* param_1, u32* param_2) {
     param_1[0] = param_2[0];
     param_1[1] = param_2[1];
     param_1[2] = param_2[2];
     return param_1;
 }
-#endif
