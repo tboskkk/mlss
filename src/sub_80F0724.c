b6f2303f1e786ca1b53d8eee55b6b9a5951c99ff
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:27:08 -0600

    Match script_enable_flag_2
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80F0724.c
+++ src/sub_80F0724.c
@@ -3,15 +3,12 @@
 
 // TODO: add other includes (a matching *.h, other subsystem headers) as
 // this file grows. Left minimal since split_func.py doesn't know yet what
-// script_enable_flag_2 needs.
+// sub_80F0724 needs.
 
 asm_unified(".include \"asm/macros.inc\"");
 
-void script_enable_flag_2(void *arg2)
-{
-  int new_var2;
-  s8 *new_var;
-  new_var2 = 2 | (*((u16 *) (new_var + 0xA0)));
-  *((u16 *) (new_var + 0xA0)) = (u16) new_var2;
-  new_var = arg2;
-}
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80F0724.s\"");
+#else
+#error "TODO: write sub_80F0724 to match asm/nonmatching/sub_80F0724.s, then delete this #error"
+#endif
