601e02d529e73de4d96188412f70c97f9dd6db3f
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 15:29:00 -0600

    Extract sub_8029910
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_813BB60.c
+++ src/sub_813BB60.c
@@ -10,56 +10,21 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813BB60.s\"");
 #else
-void sub_813BB60(u32* p1, u32* p2, u32* p3)
-{
-    u32* r3 = p1;
-    u32* r4 = p2;
-    u32 r0;
-    u32 r1;
-    s16 r5;
-    
-    r0 = 0x000002DE;
-    r2 = r3 + r0;
-    r0 = *(u16*)r2;
-    if (r0 == 0)
-    {
-        r0 = 0;
-        goto _0813BBA4;
+void sub_813BB60(void *arg1, void *arg2) {
+    u16 temp_r0_34;
+    u32 var_r0_14;
+
+    if ((*(u16 *)((s8 *)(arg1) + (0x2DE))) == 0) {
+        var_r0_14 = 0;
+    } else {
+        (*(s32 *)((s8 *)(arg1) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0xC))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        (*(s32 *)((s8 *)(arg1) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        temp_r0_34 = (*(u16 *)((s8 *)(arg1) + (0x2DE))) - 1;
+        (*(u16 *)((s8 *)(arg1) + (0x2DE))) = temp_r0_34;
+        var_r0_14 = (u32) (0 - temp_r0_34) >> 0x1F;
     }
-    
-    r1 = 0x00000242;
-    r0 = r3 + r1;
-    r5 = 0;
-    r1 = *(s16*)((u8*)r0 + r5);
-    r0 = *(u32*)((u8*)r3 + 0x0C);
-    r0 += r1;
-    *(u32*)((u8*)r3 + 0x0C) = r0;
-    
-    r1 = 0x91;
-    r1 <<= 2;
-    r0 = r3 + r1;
-    r5 = 0;
-    r1 = *(s16*)((u8*)r0 + r5);
-    r0 = *(u32*)((u8*)r3 + 0x10);
-    r0 += r1;
-    *(u32*)((u8*)r3 + 0x10) = r0;
-    
-    r0 = *(u16*)r2;
-    r0--;
-    *(u16*)r2 = r0;
-    r0 <<= 16;
-    r0 >>= 16;
-    r0 = -r0;
-    r0 >>= 31;
-    
-_0813BBA4:
-    if (r0 != 0)
-        goto _0813BBAC;
-    
-    r0 = *(u32*)((u8*)r4 + 0x04);
-    *(u32*)((u8*)r4) = r0;
-    
-_0813BBAC:
-    return;
+    if (var_r0_14 == 0) {
+        (*(s32 *)((s8 *)(arg2) + (0))) = (s32) (*(s32 *)((s8 *)(arg2) + (4)));
+    }
 }
 #endif
