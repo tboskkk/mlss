6c0dafec20969bf5e17a0860b82b269dcf2ebf48
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:52:11 -0600

    Extract sub_80EDAB0
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_8163238.c
+++ src/sub_8163238.c
@@ -10,5 +10,20 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8163238.s\"");
 #else
-#error "TODO: write sub_8163238 to match asm/nonmatching/sub_8163238.s, then delete this #error"
+void sub_8163238(void *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4) {
+    s32 var_r0_20;
+
+    (*(s32 *)((s8 *)(arg0) + (0x10))) = (s32) (*(s32 *)((s8 *)(arg0) + (0)));
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = (s32) (*(s32 *)((s8 *)(arg0) + (4)));
+    (*(s32 *)((s8 *)(arg0) + (8))) = arg1;
+    (*(s32 *)((s8 *)(arg0) + (0xC))) = arg2;
+    (*(s32 *)((s8 *)(arg0) + (0x28))) = arg3;
+    (*(s32 *)((s8 *)(arg0) + (0x2C))) = 0;
+    if (arg4 == 0) {
+        var_r0_20 = 1;
+    } else {
+        var_r0_20 = 2;
+    }
+    (*(s32 *)((s8 *)(arg0) + (0x24))) = var_r0_20;
+}
 #endif
