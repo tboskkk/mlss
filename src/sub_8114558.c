757da6e510d7cf2cd3fb1bd1da36350e91ea3bdc
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:14:02 -0600

    Match sub_8114558
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8114558.c
+++ src/sub_8114558.c
@@ -7,8 +7,20 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8114558.s\"");
-#else
-#error "TODO: write sub_8114558 to match asm/nonmatching/sub_8114558.s, then delete this #error"
-#endif
+extern s32 sub_81140C0;
+extern s32 sub_811458C;
+
+void sub_8114558(void *arg0) {
+    s32 *var_r1_21;
+    s32 temp_r0_19;
+
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)((*(void **)((s8 *)((*(void **)((s8 *)(arg0) + (0x30)))) + (0x30)))) + (8)))) + (0x12)))) {
+        temp_r0_19 = (*(s32 *)((s8 *)(arg0) + (0xA0))) - 1;
+        (*(s32 *)((s8 *)(arg0) + (0xA0))) = temp_r0_19;
+        var_r1_21 = &sub_81140C0;
+        if (temp_r0_19 != 0) {
+            var_r1_21 = &sub_811458C;
+        }
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = var_r1_21;
+    }
+}
