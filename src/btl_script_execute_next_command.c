052962594c1e3661798e2f208f55b34df7cc9189
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 15:32:14 -0600

    Extract btl_script_execute_next_command
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ src/btl_script_execute_next_command.c
@@ -0,0 +1,14 @@
+#include "global.h"
+#include "common.h"
+
+// TODO: add other includes (a matching *.h, other subsystem headers) as
+// this file grows. Left minimal since split_func.py doesn't know yet what
+// btl_script_execute_next_command needs.
+
+asm_unified(".include \"asm/macros.inc\"");
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/btl_script_execute_next_command.s\"");
+#else
+#error "TODO: write btl_script_execute_next_command to match asm/nonmatching/btl_script_execute_next_command.s, then delete this #error"
+#endif
--- src/sub_81065D0.c
+++ src/sub_81065D0.c
@@ -10,7 +10,19 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81065D0.s\"");
 #else
-#error "TODO: write sub_81065D0 to match asm/nonmatching/sub_81065D0.s, then delete this #error"
+void sub_81065D0(void *arg0) {
+    s32 temp_r4_10;
+    void *temp_r1_18;
+
+    temp_r4_10 = M2C_ERROR(/* unknown instruction: ldsh $r4, ($mem_loc_fictive_) */);
+    if (temp_r4_10 != 0) {
+        (*(u16 *)((s8 *)(arg0) + (0x10))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0x10))) - 1);
+        return;
+    }
+    temp_r1_18 = *(void **)0x03000FD8;
+    (*(u8 *)((s8 *)(temp_r1_18) + (0x2BF))) = (u8) (-0x21 & (*(u8 *)((s8 *)(temp_r1_18) + (0x2BF))));
+    (*(s32 *)((s8 *)(arg0) + (4))) = temp_r4_10;
+}
 #endif
 
 #ifndef NONMATCHING
