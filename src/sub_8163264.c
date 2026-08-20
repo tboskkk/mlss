ff792e74d9ed5295335de187f2ba67720a024a57
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:25:50 -0600

    Extract sub_8163298
    Factory pipeline (tools/factory) -- re-extraction via reclaim_extraction.py, not yet matched.

--- src/sub_8163264.c
+++ src/sub_8163264.c
@@ -24,3 +24,9 @@ asm_unified(".include \"asm/nonmatching/sub_8163280.s\"");
 #else
 #error "TODO: write sub_8163280 to match asm/nonmatching/sub_8163280.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8163298.s\"");
+#else
+#error "TODO: write sub_8163298 to match asm/nonmatching/sub_8163298.s, then delete this #error"
+#endif
