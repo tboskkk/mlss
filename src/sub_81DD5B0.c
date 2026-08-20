f06424649670023d96950f589413260e5a058fdb
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:12:43 -0600

    Match sub_81DD5B0
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_81DD5B0.c
+++ src/sub_81DD5B0.c
@@ -7,17 +7,12 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81DD5B0.s\"");
-#else
-void sub_81DD5B0(void* p0, u32 p1)
-{
-    u32* ptr = (u32*)((u8*)p0 + 0x338);
-    *ptr = 0x08CDBDE8;
-    if (p1 & 1)
-        free_heap_8018DA8(p0);
+void sub_81DD5B0(void *arg0, s32 arg1) {
+    (*(s32 *)((s8 *)(arg0) + (0x338))) = 0x08CDBDE8;
+    if (1 & arg1) {
+        free_heap_8018DA8(arg0);
+    }
 }
-#endif
 
 void sub_81DD5D4(void* p0, u32 p1)
 {
