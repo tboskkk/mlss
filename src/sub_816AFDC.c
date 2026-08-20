46165825b3b5f833275c3858bbe251e1a28e4ea8
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:10:10 -0600

    Match sub_816AFDC
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_816AFDC.c
+++ src/sub_816AFDC.c
@@ -7,12 +7,6 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_816AFDC.s\"");
-#else
-u32 sub_816AFDC(u32 param_1) {
-    u32* ptr = (u32*)((u32)param_1 + 0x490);
-    *ptr &= 0xFFFFFFF6;
-    return (u32)ptr;
+void sub_816AFDC(void *arg0) {
+    (*(s32 *)((s8 *)(arg0) + (0x490))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x490))) & ~8);
 }
-#endif
