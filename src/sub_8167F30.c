783e30770ddc82ff35c900315a315ada42f7d8c0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:09:27 -0600

    Match sub_8167F30
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8167F30.c
+++ src/sub_8167F30.c
@@ -7,12 +7,7 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8167F30.s\"");
-#else
-u32* sub_8167F30(u32* param_1) {
-    param_1[0x1B] = 0;
-    param_1[0x1D] = 0;
-    return param_1;
+void sub_8167F30(void *arg0) {
+    (*(s16 *)((s8 *)(arg0) + (0x6C))) = 0;
+    (*(s16 *)((s8 *)(arg0) + (0x6E))) = 0;
 }
-#endif
