d13fcf0bc1c88a765f9bd2c1e56183d63de8df7b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:49:48 -0600

    Extract sub_815F898
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p240.s
+++ asm/text08057568_p240.s
@@ -3,21 +3,3 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_815F898
-sub_815F898:
-	push {lr}
-	adds r2, r0, #0x0
-	ldr r0, _0815F8B4 @ =0x08CDCF50
-	str r0, [r2, #0x04]
-	movs r0, #0x01
-	ands r0, r1
-	cmp r0, #0x00
-	beq _0815F8AE
-	adds r0, r2, #0x0
-	bl free_heap_8018DA8
-_0815F8AE:
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_0815F8B4: .4byte 0x08CDCF50
-	.byte 0x70, 0x47, 0x00, 0x00
