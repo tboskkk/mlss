f5e3f1e352e8942d5fb7acc4d5a13d748a3dffea
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 14:24:13 -0600

    Match sub_80F087C
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80F087C.c
+++ src/sub_80F087C.c
@@ -7,8 +7,21 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80F087C.s\"");
-#else
-#error "TODO: write sub_80F087C to match asm/nonmatching/sub_80F087C.s, then delete this #error"
-#endif
+s32 sub_80F087C(void *arg0, void *arg1)
+{
+  int new_var3;
+  s8 *new_var;
+  void *new_var2;
+  new_var3 = 1;
+  new_var = (s8 *) arg0;
+  new_var2 = *((void **) (new_var + 0x14));
+  if (0x10 & (*((u8 *) (((s8 *) new_var2) + 0x208))))
+  {
+    return 1;
+  }
+  *((s32 *) (((s8 *) arg1) + 0)) = (s32) (*((s32 *) (((s8 *) arg1) + 0x14)));
+  if (new_var3)
+  {
+    return 0;
+  }
+}
--- src/sub_80F7ED0.c
+++ src/sub_80F7ED0.c
@@ -10,5 +10,16 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F7ED0.s\"");
 #else
-#error "TODO: write sub_80F7ED0 to match asm/nonmatching/sub_80F7ED0.s, then delete this #error"
+s32 sub_80F7ED0(void *arg2) {
+    s32 temp_r3_8;
+    void *temp_r0_12;
+
+    temp_r3_8 = (*(s32 *)((s8 *)(arg2) + (0)));
+    if (temp_r3_8 == 0) {
+        temp_r0_12 = *(void **)0x03000FD8;
+        (*(s16 *)((s8 *)((*(void **)((s8 *)(temp_r0_12) + (0x2D8)))) + (8))) = (s16) (*(s32 *)((s8 *)(arg2) + (4)));
+        (*(s16 *)((s8 *)((*(void **)((s8 *)(temp_r0_12) + (0x2D8)))) + (0xA))) = (s16) temp_r3_8;
+    }
+    return 1;
+}
 #endif
