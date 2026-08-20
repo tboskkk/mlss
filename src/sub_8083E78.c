ba7a17de1f1982afc8980e903d333a8c98f18d33
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 12:26:46 -0600

    Match sub_8083E78
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8083E78.c
+++ src/sub_8083E78.c
@@ -7,10 +7,6 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8083E78.s\"");
-#else
 s8 sub_8083E78(void *arg0) {
     return (s8) (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (0xE4)))) + (6)));
 }
-#endif
