049afd2ca4fc73ac043e7e133411db536462e399
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:59:36 -0600

    Extract sub_80DEC60
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p155.s
+++ asm/text08057568_p155.s
@@ -3,13 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80DEC60
-sub_80DEC60:
-	push {lr}
-	bl sub_807C298
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
 	thumb_func_start sub_80DEC6C
 sub_80DEC6C:
 	push {r4, r5, lr}
--- src/sub_80DEAC0.c
+++ src/sub_80DEAC0.c
@@ -53,3 +53,9 @@ asm_unified(".include \"asm/nonmatching/sub_80DEC44.s\"");
 #else
 #error "TODO: write sub_80DEC44 to match asm/nonmatching/sub_80DEC44.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80DEC60.s\"");
+#else
+#error "TODO: write sub_80DEC60 to match asm/nonmatching/sub_80DEC60.s, then delete this #error"
+#endif
