23574194ebc267bc58c5eee3176d7e38b97e3aec
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:34:34 -0600

    Draft m2c seeds for sub_80F7E80.c's #error placeholders
    Unblocks the whole translation unit (sub_80F7E84). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80F7E80.c
+++ src/sub_80F7E80.c
@@ -14,5 +14,13 @@ u32 sub_80F7E80(void) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F7E84.s\"");
 #else
-#error "TODO: write sub_80F7E84 to match asm/nonmatching/sub_80F7E84.s, then delete this #error"
+s32 sub_8082B00();                                  /* extern */
+
+s32 sub_80F7E84(s32 arg0, void *arg1) {
+    if ((sub_8082B00() << 0x18) == 0) {
+        return 1;
+    }
+    (*(s32 *)((s8 *)(arg1) + (0))) = (s32) (*(s32 *)((s8 *)(arg1) + (0x14)));
+    return 0;
+}
 #endif
