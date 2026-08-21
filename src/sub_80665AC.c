03f5c321476ca1898959ceb74157e1cc86a1fcb6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:20:25 -0600

    Extract sub_8066E5C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_80665AC.c
+++ src/sub_80665AC.c
@@ -102,3 +102,9 @@ asm_unified(".include \"asm/nonmatching/sub_8066E0C.s\"");
 #else
 #error "TODO: write sub_8066E0C to match asm/nonmatching/sub_8066E0C.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8066E5C.s\"");
+#else
+#error "TODO: write sub_8066E5C to match asm/nonmatching/sub_8066E5C.s, then delete this #error"
+#endif
