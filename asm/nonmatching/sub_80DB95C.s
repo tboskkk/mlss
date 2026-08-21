effd47d986859b51bac5b14139a84567a06bf1ee
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:43:38 -0600

    Extract sub_80DB95C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80DB95C.s
@@ -0,0 +1,66 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80DB95C
+sub_80DB95C:
+	push {r4, r5, lr}
+	adds r4, r0, #0x0
+	adds r0, #0x80
+	ldr r5, [r0, #0x00]
+	cmp r5, #0x00
+	bne _080DB9D2
+	adds r1, r4, #0x0
+	adds r1, #0xAC
+	ldrh r0, [r1, #0x00]
+	subs r0, #0x01
+	strh r0, [r1, #0x00]
+	lsls r0, r0, #0x10
+	cmp r0, #0x00
+	bgt _080DB9D2
+	adds r0, r4, #0x0
+	bl sub_807F47C
+	adds r1, r4, #0x0
+	adds r1, #0x84
+	ldr r2, [r4, #0x28]
+	adds r0, r2, #0x0
+	adds r0, #0xD8
+	ldr r0, [r0, #0x00]
+	str r0, [r1, #0x00]
+	adds r1, #0x04
+	adds r0, r2, #0x0
+	adds r0, #0xDC
+	ldr r0, [r0, #0x00]
+	str r0, [r1, #0x00]
+	adds r1, #0x04
+	adds r0, r2, #0x0
+	adds r0, #0xE0
+	ldr r0, [r0, #0x00]
+	str r0, [r1, #0x00]
+	adds r2, r4, #0x0
+	adds r2, #0x79
+	ldrb r0, [r2, #0x00]
+	movs r1, #0x20
+	orrs r0, r1
+	strb r0, [r2, #0x00]
+	adds r1, r4, #0x0
+	adds r1, #0x7C
+	movs r0, #0xC0
+	lsls r0, r0, #0x03
+	strh r0, [r1, #0x00]
+	adds r0, r4, #0x0
+	adds r0, #0x7A
+	strh r5, [r0, #0x00]
+	adds r0, r4, #0x0
+	bl sub_8085B38
+	adds r0, r4, #0x0
+	movs r1, #0x05
+	movs r2, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r0, _080DB9D8 @ =0x080DBC85
+	str r0, [r4, #0x4C]
+_080DB9D2:
+	pop {r4, r5}
+	pop {r0}
+	bx r0
+_080DB9D8: .4byte sub_80DBC84
