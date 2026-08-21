e7fa14afa56dbeb32976a015c72f976af1f9afb1
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:42:02 -0600

    Draft m2c seeds for sub_8143314.c's #error placeholders
    Unblocks the whole translation unit (sub_8143314, sub_8143374). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8143314.c
+++ src/sub_8143314.c
@@ -10,11 +10,36 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8143314.s\"");
 #else
-#error "TODO: write sub_8143314 to match asm/nonmatching/sub_8143314.s, then delete this #error"
+s32 sub_8047154(void *, s32, s32);              /* extern */
+s32 sub_81387C8(void *, s32);                   /* extern */
+extern s32 sub_8136D00;
+
+void sub_8143314(s32 arg0, void *arg1) {
+    sub_81387C8(arg1, 0x10);
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = (s32 (*)(s32, void *)) (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x19C)));
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_8136D00;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_8136D00;
+    (*(u8 *)((s8 *)(arg1) + (0x214))) = (u8) ((-8 & (*(u8 *)((s8 *)(arg1) + (0x214)))) | 1);
+    sub_8047154(arg1, 4, 0);
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198)))(arg0, arg1);
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8143374.s\"");
 #else
-#error "TODO: write sub_8143374 to match asm/nonmatching/sub_8143374.s, then delete this #error"
+s32 sub_81387C8(void *, s32);                   /* extern */
+s32 sub_8139030(void *, s32);                   /* extern */
+s32 sub_8143128(s32, void *);                   /* extern */
+extern s32 sub_8139420;
+
+void sub_8143374(s32 arg0, void *arg1) {
+    sub_81387C8(arg1, 0);
+    sub_8139030(arg1, 0);
+    (*(u8 *)((s8 *)(arg1) + (0x214))) = (u8) ((-8 & (*(u8 *)((s8 *)(arg1) + (0x214)))) | 3);
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_8143128;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_8139420;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_8139420;
+    sub_8143128(arg0, arg1);
+}
 #endif
