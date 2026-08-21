66cf8664ea6b50820a773761e03ae99574a28ba3
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:49:03 -0600

    Extract sub_80920A4
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80920A4.s
@@ -0,0 +1,78 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80920A4
+sub_80920A4:
+	push {r4, r5, lr}
+	adds r4, r0, #0x0
+	ldr r0, _08092108 @ =0x084FE9A4
+	str r0, [r4, #0x6C]
+	ldr r0, _0809210C @ =0x03000FF4
+	ldr r2, [r0, #0x00]
+	adds r2, #0x3C
+	adds r3, r4, #0x0
+	adds r3, #0x77
+	ldrb r1, [r3, #0x00]
+	movs r0, #0x41
+	negs r0, r0
+	ands r0, r1
+	movs r1, #0x21
+	negs r1, r1
+	ands r0, r1
+	strb r0, [r3, #0x00]
+	ldr r0, [r4, #0x28]
+	movs r1, #0x8A
+	lsls r1, r1, #0x01
+	adds r0, r0, r1
+	ldrh r1, [r2, #0x2A]
+	str r1, [r0, #0x00]
+	bl sub_8199F30
+	adds r1, r0, #0x0
+	movs r0, #0x16
+	bl sub_81DD77C
+	adds r5, r0, #0x0
+	adds r5, #0x0A
+	cmp r5, #0x00
+	beq _08092118
+	ldr r2, _08092110 @ =0x0000204D
+	adds r0, r4, #0x0
+	movs r1, #0x04
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r2, [r4, #0x08]
+	ldrb r1, [r2, #0x12]
+	movs r0, #0x07
+	negs r0, r0
+	ands r0, r1
+	strb r0, [r2, #0x12]
+	adds r0, r4, #0x0
+	adds r0, #0x9C
+	str r5, [r0, #0x00]
+	ldr r0, _08092114 @ =0x08092EA1
+	b _0809211A
+_08092108: .4byte 0x084FE9A4
+_0809210C: .4byte 0x03000FF4
+_08092110: .4byte 0x0000204D
+_08092114: .4byte sub_8092EA0
+_08092118:
+	ldr r0, _0809213C @ =0x08092E59
+_0809211A:
+	str r0, [r4, #0x4C]
+	ldr r0, _08092140 @ =0x03001038
+	ldr r1, [r0, #0x00]
+	ldr r0, _08092144 @ =0x005A1440
+	bl _call_via_r1
+	subs r0, #0x48
+	lsrs r1, r0, #0x1F
+	adds r0, r0, r1
+	asrs r0, r0, #0x01
+	adds r1, r4, #0x0
+	adds r1, #0xB2
+	strh r0, [r1, #0x00]
+	movs r0, #0x01
+	pop {r4, r5}
+	pop {r1}
+	bx r1
+_0809213C: .4byte sub_8092E58
+_08092140: .4byte 0x03001038
+_08092144: .4byte 0x005A1440
--- src/sub_8091C74.c
+++ src/sub_8091C74.c
@@ -99,3 +99,9 @@ asm_unified(".include \"asm/nonmatching/sub_8092004.s\"");
 #else
 #error "TODO: write sub_8092004 to match asm/nonmatching/sub_8092004.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80920A4.s\"");
+#else
+#error "TODO: write sub_80920A4 to match asm/nonmatching/sub_80920A4.s, then delete this #error"
+#endif
