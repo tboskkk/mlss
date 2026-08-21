16814067353b95a02d342e80e1eef51790fdc961
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:39:44 -0600

    Draft m2c seeds for sub_8132528.c's #error placeholders
    Unblocks the whole translation unit (sub_8132528). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8132528.c
+++ src/sub_8132528.c
@@ -10,5 +10,26 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132528.s\"");
 #else
-#error "TODO: write sub_8132528 to match asm/nonmatching/sub_8132528.s, then delete this #error"
+s32 sub_80880C4(void *, s32);                   /* extern */
+s32 sub_808843C(void *, s32, s32, s32, s32);    /* extern */
+extern s32 sub_8132570;
+
+void sub_8132528(void *arg0) {
+    s32 var_r0_10;
+    s32 var_r2_18;
+    void *temp_r2_9;
+
+    temp_r2_9 = (*(void **)((s8 *)(arg0) + (0x2C)));
+    var_r0_10 = (*(s32 *)((s8 *)(temp_r2_9) + (0x38)));
+    if (var_r0_10 < 0) {
+        var_r0_10 += 0xFF;
+    }
+    var_r2_18 = (*(s32 *)((s8 *)(temp_r2_9) + (0x3C)));
+    if (var_r2_18 < 0) {
+        var_r2_18 += 0xFF;
+    }
+    sub_808843C(arg0, (var_r0_10 >> 8) + 0x28, var_r2_18 >> 8, 0, 0x100);
+    sub_80880C4(arg0, 0x280);
+    (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8132570;
+}
 #endif
