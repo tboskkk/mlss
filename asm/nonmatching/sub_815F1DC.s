a6653b326b618e9d972e19d74261ce00a2d5a063
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:32:43 -0600

    Extract sub_815F1DC
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_815F1DC.s
@@ -0,0 +1,13 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_815F1DC
+sub_815F1DC:
+	push {lr}
+	ldr r2, _0815F1EC @ =0x08CDCCD0
+	str r2, [r0, #0x30]
+	bl sub_815FB14
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_0815F1EC: .4byte 0x08CDCCD0
--- asm/text08057568_p269.s
+++ asm/text08057568_p269.s
@@ -3,16 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_815F1DC
-sub_815F1DC:
-	push {lr}
-	ldr r2, _0815F1EC @ =0x08CDCCD0
-	str r2, [r0, #0x30]
-	bl sub_815FB14
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_0815F1EC: .4byte 0x08CDCCD0
 	thumb_func_start sub_815F1F0
 sub_815F1F0:
 	push {r4, r5, lr}
