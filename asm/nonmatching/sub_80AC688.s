087f7bf472db2081cc15a3f541d5f8ba0d87a100
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:54:14 -0600

    Extract sub_80AC688
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80AC688.s
@@ -0,0 +1,75 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80AC688
+sub_80AC688:
+	push {r4, r5, r6, lr}
+	mov r6, r9
+	mov r5, r8
+	push {r5, r6}
+	mov r9, r0
+	ldr r2, _080AC710 @ =0x03000FF4
+	ldr r3, [r0, #0x28]
+	adds r0, r3, #0x0
+	adds r0, #0xEC
+	movs r4, #0x00
+	ldsh r1, [r0, r4]
+	lsls r0, r1, #0x04
+	subs r0, r0, r1
+	lsls r0, r0, #0x02
+	adds r0, #0x3C
+	ldr r1, [r2, #0x00]
+	subs r1, r1, r0
+	movs r0, #0x8A
+	lsls r0, r0, #0x01
+	adds r2, r3, r0
+	ldrh r0, [r1, #0x2A]
+	str r0, [r2, #0x00]
+	ldr r0, _080AC714 @ =0x03000FD8
+	ldr r0, [r0, #0x00]
+	ldr r5, [r0, #0x70]
+	movs r1, #0x08
+	adds r1, r1, r5
+	mov r8, r1
+	ldr r4, [r0, #0x74]
+	adds r6, r4, #0x0
+	adds r6, #0x08
+	mov r2, r9
+	ldr r0, [r2, #0x2C]
+	str r0, [r6, #0x2C]
+	str r0, [r1, #0x2C]
+	ldr r1, [r2, #0x6C]
+	str r1, [r6, #0x6C]
+	mov r2, r8
+	str r1, [r2, #0x6C]
+	movs r3, #0x00
+	str r3, [r0, #0x50]
+	str r3, [r6, #0x50]
+	str r3, [r2, #0x50]
+	adds r2, r0, #0x0
+	adds r2, #0x7C
+	movs r1, #0x80
+	lsls r1, r1, #0x01
+	strh r1, [r2, #0x00]
+	adds r4, #0x84
+	strh r1, [r4, #0x00]
+	adds r5, #0x84
+	strh r1, [r5, #0x00]
+	ldr r1, _080AC718 @ =0x080AC71D
+	mov r4, r9
+	str r1, [r4, #0x4C]
+	strh r3, [r6, #0x24]
+	mov r1, r8
+	strh r3, [r1, #0x24]
+	bl sub_80870B4
+	movs r0, #0x01
+	pop {r3, r4}
+	mov r8, r3
+	mov r9, r4
+	pop {r4, r5, r6}
+	pop {r1}
+	bx r1
+	.byte 0x00, 0x00
+_080AC710: .4byte 0x03000FF4
+_080AC714: .4byte 0x03000FD8
+_080AC718: .4byte sub_80AC71C
--- src/sub_80AC4F8.c
+++ src/sub_80AC4F8.c
@@ -42,3 +42,9 @@ asm_unified(".include \"asm/nonmatching/sub_80AC610.s\"");
 #else
 #error "TODO: write sub_80AC610 to match asm/nonmatching/sub_80AC610.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80AC688.s\"");
+#else
+#error "TODO: write sub_80AC688 to match asm/nonmatching/sub_80AC688.s, then delete this #error"
+#endif
