8c2eb7dab6a237dd77111119f42edec490a89262
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 21:08:48 -0600

    Extract sub_81DD094
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p81.s
+++ asm/text08057568_p81.s
@@ -3,25 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_81DD094
-sub_81DD094:
-	push {r4, r5, lr}
-	add sp, #-0x00C
-	adds r4, r1, #0x0
-	adds r5, r2, #0x0
-	bl sub_81DCE9C
-	str r0, [sp, #0x000]
-	str r4, [sp, #0x004]
-	str r5, [sp, #0x008]
-	movs r3, #0x05
-	adds r0, r3, #0x0
-	mov r1, sp
-	swi #171
-	adds r2, r0, #0x0
-	adds r0, r2, #0x0
-	add sp, #0x00C
-	pop {r4, r5, pc}
-	.byte 0x00, 0x00
 	thumb_func_start sub_81DD0B8
 sub_81DD0B8:
 	push {r4, r5, r6, r7, lr}
--- src/sub_81DCF58.c
+++ src/sub_81DCF58.c
@@ -41,3 +41,9 @@ asm_unified(".include \"asm/nonmatching/sub_81DD004.s\"");
 #else
 #error "TODO: write sub_81DD004 to match asm/nonmatching/sub_81DD004.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_81DD094.s\"");
+#else
+#error "TODO: write sub_81DD094 to match asm/nonmatching/sub_81DD094.s, then delete this #error"
+#endif
