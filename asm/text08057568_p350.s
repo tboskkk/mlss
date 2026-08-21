4302be835027432bfeba65a80bc963d197921739
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:51:59 -0600

    Extract sub_809BA98
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p350.s
+++ asm/text08057568_p350.s
@@ -3,59 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_809BA98
-sub_809BA98:
-	push {r4, lr}
-	add sp, #-0x004
-	adds r4, r0, #0x0
-	adds r0, #0x9C
-	ldr r0, [r0, #0x00]
-	cmp r0, #0x00
-	beq _0809BAC2
-	ldr r2, _0809BAF4 @ =0x000040BF
-	adds r0, r4, #0x0
-	movs r1, #0x08
-	movs r3, #0x00
-	bl sub_8082E1C
-	ldr r2, [r4, #0x08]
-	ldrb r1, [r2, #0x12]
-	movs r0, #0x07
-	negs r0, r0
-	ands r0, r1
-	movs r1, #0x02
-	orrs r0, r1
-	strb r0, [r2, #0x12]
-_0809BAC2:
-	ldr r1, [r4, #0x10]
-	cmp r1, #0x00
-	bge _0809BACA
-	adds r1, #0xFF
-_0809BACA:
-	asrs r1, r1, #0x08
-	ldr r2, [r4, #0x14]
-	cmp r2, #0x00
-	bge _0809BAD4
-	adds r2, #0xFF
-_0809BAD4:
-	asrs r2, r2, #0x08
-	ldr r3, [r4, #0x18]
-	cmp r3, #0x00
-	bge _0809BADE
-	adds r3, #0xFF
-_0809BADE:
-	asrs r3, r3, #0x08
-	str r4, [sp, #0x000]
-	ldr r0, _0809BAF8 @ =0x000011E6
-	bl sub_80DF024
-	ldr r0, _0809BAFC @ =0x0809BB01
-	str r0, [r4, #0x4C]
-	add sp, #0x004
-	pop {r4}
-	pop {r0}
-	bx r0
-_0809BAF4: .4byte 0x000040BF
-_0809BAF8: .4byte 0x000011E6
-_0809BAFC: .4byte sub_809BB00
 	thumb_func_start sub_809BB00
 sub_809BB00:
 	push {r4, lr}
--- src/sub_809B960.c
+++ src/sub_809B960.c
@@ -52,3 +52,9 @@ asm_unified(".include \"asm/nonmatching/sub_809BA44.s\"");
 #else
 #error "TODO: write sub_809BA44 to match asm/nonmatching/sub_809BA44.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_809BA98.s\"");
+#else
+#error "TODO: write sub_809BA98 to match asm/nonmatching/sub_809BA98.s, then delete this #error"
+#endif
