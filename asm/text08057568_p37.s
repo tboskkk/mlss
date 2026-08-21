1643bbb85ff3bd4ba88a04f1b1727059c5388d98
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:51:28 -0600

    Extract sub_809A09C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p37.s
+++ asm/text08057568_p37.s
@@ -3,52 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_809A09C
-sub_809A09C:
-	push {r4, lr}
-	add sp, #-0x004
-	adds r4, r0, #0x0
-	ldr r0, [r4, #0x08]
-	ldrb r1, [r0, #0x12]
-	movs r0, #0x08
-	ands r0, r1
-	cmp r0, #0x00
-	beq _0809A0E4
-	adds r0, r4, #0x0
-	movs r1, #0x07
-	movs r2, #0x00
-	movs r3, #0x00
-	bl sub_8082E1C
-	ldr r1, [r4, #0x38]
-	cmp r1, #0x00
-	bge _0809A0C2
-	adds r1, #0xFF
-_0809A0C2:
-	asrs r1, r1, #0x08
-	ldr r2, [r4, #0x3C]
-	cmp r2, #0x00
-	bge _0809A0CC
-	adds r2, #0xFF
-_0809A0CC:
-	asrs r2, r2, #0x08
-	ldr r3, [r4, #0x40]
-	cmp r3, #0x00
-	bge _0809A0D6
-	adds r3, #0xFF
-_0809A0D6:
-	asrs r3, r3, #0x08
-	str r4, [sp, #0x000]
-	ldr r0, _0809A0EC @ =0x00000D26
-	bl sub_80DF024
-	ldr r0, _0809A0F0 @ =0x0809A0F5
-	str r0, [r4, #0x4C]
-_0809A0E4:
-	add sp, #0x004
-	pop {r4}
-	pop {r0}
-	bx r0
-_0809A0EC: .4byte 0x00000D26
-_0809A0F0: .4byte sub_809A0F4
 	thumb_func_start sub_809A0F4
 sub_809A0F4:
 	push {r4, lr}
--- src/sub_80993BC.c
+++ src/sub_80993BC.c
@@ -121,3 +121,9 @@ asm_unified(".include \"asm/nonmatching/sub_809A06C.s\"");
 #else
 #error "TODO: write sub_809A06C to match asm/nonmatching/sub_809A06C.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_809A09C.s\"");
+#else
+#error "TODO: write sub_809A09C to match asm/nonmatching/sub_809A09C.s, then delete this #error"
+#endif
