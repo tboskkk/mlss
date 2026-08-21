432bb9225637c685f19176cb3ef404757b5e74b6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:48:33 -0600

    Extract sub_808FE50
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p313.s
+++ asm/text08057568_p313.s
@@ -3,51 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_808FE50
-sub_808FE50:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	movs r1, #0x01
-	negs r1, r1
-	movs r0, #0x2B
-	bl play_sfx_80195B4
-	ldr r0, _0808FEA4 @ =0x03001038
-	ldr r1, [r0, #0x00]
-	ldr r0, _0808FEA8 @ =0x002E4559
-	bl _call_via_r1
-	subs r0, #0x25
-	lsrs r1, r0, #0x1F
-	adds r0, r0, r1
-	asrs r0, r0, #0x01
-	adds r1, r4, #0x0
-	adds r1, #0xB2
-	strh r0, [r1, #0x00]
-	subs r1, #0x1E
-	movs r0, #0x25
-	negs r0, r0
-	str r0, [r1, #0x00]
-	ldr r2, _0808FEAC @ =0x0000204D
-	adds r0, r4, #0x0
-	movs r1, #0x05
-	movs r3, #0x00
-	bl sub_8082E1C
-	ldr r2, [r4, #0x08]
-	ldrb r1, [r2, #0x12]
-	movs r0, #0x07
-	negs r0, r0
-	ands r0, r1
-	movs r1, #0x02
-	orrs r0, r1
-	strb r0, [r2, #0x12]
-	ldr r0, _0808FEB0 @ =0x0808FF19
-	str r0, [r4, #0x4C]
-	pop {r4}
-	pop {r0}
-	bx r0
-_0808FEA4: .4byte 0x03001038
-_0808FEA8: .4byte 0x002E4559
-_0808FEAC: .4byte 0x0000204D
-_0808FEB0: .4byte sub_808FF18
 	thumb_func_start sub_808FEB4
 sub_808FEB4:
 	push {r4, lr}
