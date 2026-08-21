db7770a6d5838b3bb7c6c12cbac6d6c0cb173d56
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:14:10 -0600

    Extract sub_801AD80
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text0801A548_p40.s
+++ asm/text0801A548_p40.s
@@ -3,34 +3,3 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_801AD80
-sub_801AD80:
-	push {lr}
-	ldr r0, _0801ADB0 @ =0x0300034C
-	ldr r1, _0801ADB4 @ =0x00000888
-	adds r0, r0, r1
-	ldrb r1, [r0, #0x00]
-	movs r0, #0x06
-	ands r0, r1
-	cmp r0, #0x02
-	bne _0801ADAA
-	ldr r2, _0801ADB8 @ =0x03000D18
-	ldrb r1, [r2, #0x00]
-	movs r0, #0x31
-	negs r0, r0
-	ands r0, r1
-	strb r0, [r2, #0x00]
-	ldr r2, _0801ADBC @ =0x03000D28
-	ldrb r1, [r2, #0x00]
-	movs r0, #0x04
-	negs r0, r0
-	ands r0, r1
-	strb r0, [r2, #0x00]
-_0801ADAA:
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_0801ADB0: .4byte 0x0300034C
-_0801ADB4: .4byte 0x00000888
-_0801ADB8: .4byte 0x03000D18
-_0801ADBC: .4byte 0x03000D28
--- src/sub_801AA70.c
+++ src/sub_801AA70.c
@@ -104,3 +104,9 @@ asm_unified(".include \"asm/nonmatching/sub_801AD3C.s\"");
 #else
 #error "TODO: write sub_801AD3C to match asm/nonmatching/sub_801AD3C.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_801AD80.s\"");
+#else
+#error "TODO: write sub_801AD80 to match asm/nonmatching/sub_801AD80.s, then delete this #error"
+#endif
