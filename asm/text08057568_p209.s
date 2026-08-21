aac618a0a39160ca4e4c39a51476616d796b5017
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:15:02 -0600

    Extract sub_807FA14
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p209.s
+++ asm/text08057568_p209.s
@@ -3,34 +3,3 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_807FA14
-sub_807FA14:
-	adds r3, r0, #0x0
-	ldr r0, _0807FA4C @ =0x03000FD8
-	ldr r0, [r0, #0x00]
-	mov r12, r0
-	movs r2, #0x8E
-	lsls r2, r2, #0x02
-	add r2, r12
-	ldr r0, [r2, #0x00]
-	ldr r1, [r0, #0x00]
-	str r1, [r2, #0x00]
-	movs r2, #0x8F
-	lsls r2, r2, #0x02
-	add r2, r12
-	ldr r1, [r2, #0x00]
-	str r1, [r0, #0x00]
-	str r0, [r2, #0x00]
-	ldrb r2, [r0, #0x0C]
-	movs r1, #0x20
-	negs r1, r1
-	ands r1, r2
-	strb r1, [r0, #0x0C]
-	str r0, [r3, #0x04]
-	ldr r1, [r3, #0x08]
-	str r1, [r0, #0x04]
-	adds r3, #0x0C
-	str r3, [r0, #0x08]
-	bx lr
-	.byte 0x00, 0x00
-_0807FA4C: .4byte 0x03000FD8
--- src/sub_807F834.c
+++ src/sub_807F834.c
@@ -110,3 +110,9 @@ asm_unified(".include \"asm/nonmatching/sub_807F9A8.s\"");
 #else
 #error "TODO: write sub_807F9A8 to match asm/nonmatching/sub_807F9A8.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_807FA14.s\"");
+#else
+#error "TODO: write sub_807FA14 to match asm/nonmatching/sub_807FA14.s, then delete this #error"
+#endif
