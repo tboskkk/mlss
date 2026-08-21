9a4d5e4866f3dbff13e5f0d5766c1094d356ebc1
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:58:51 -0600

    Extract sub_80F0420
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p545.s
+++ asm/text08057568_p545.s
@@ -3,61 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80F0420
-sub_80F0420:
-	push {r4, r5, r6, r7, lr}
-	mov r7, r8
-	push {r7}
-	add sp, #-0x010
-	adds r4, r0, #0x0
-	mov r8, r1
-	ldr r2, [r4, #0x00]
-	ldr r3, _080F0484 @ =0x08CDC308
-	str r3, [r2, #0x18]
-	cmp r1, #0x00
-	bne _080F0452
-	mov r1, sp
-	adds r0, r3, #0x0
-	ldm r0!, {r5, r6, r7}
-	stm r1!, {r5, r6, r7}
-	ldr r0, [r0, #0x00]
-	str r0, [r1, #0x00]
-	mov r0, sp
-	str r0, [r2, #0x18]
-	mov r1, sp
-	subs r0, r2, #0x4
-	subs r0, r4, r0
-	ldrh r3, [r3, #0x08]
-	adds r0, r0, r3
-	strh r0, [r1, #0x08]
-_080F0452:
-	movs r0, #0x02
-	mov r1, r8
-	ands r0, r1
-	cmp r0, #0x00
-	beq _080F0468
-	ldr r0, [r4, #0x00]
-	ldr r1, _080F0488 @ =0x08CDBD68
-	str r1, [r0, #0x18]
-	movs r1, #0x00
-	bl process_remove
-_080F0468:
-	movs r0, #0x01
-	mov r5, r8
-	ands r0, r5
-	cmp r0, #0x00
-	beq _080F0478
-	adds r0, r4, #0x0
-	bl free_heap_8018DA8
-_080F0478:
-	add sp, #0x010
-	pop {r3}
-	mov r8, r3
-	pop {r4, r5, r6, r7}
-	pop {r0}
-	bx r0
-_080F0484: .4byte 0x08CDC308
-_080F0488: .4byte 0x08CDBD68
 	thumb_func_start bevs_process_init_80F048C
 bevs_process_init_80F048C: @ 080F048C
 	push {r4, r5, r6, r7, lr}
--- src/sub_80F032C.c
+++ src/sub_80F032C.c
@@ -24,3 +24,9 @@ asm_unified(".include \"asm/nonmatching/sub_80F03C8.s\"");
 #else
 #error "TODO: write sub_80F03C8 to match asm/nonmatching/sub_80F03C8.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80F0420.s\"");
+#else
+#error "TODO: write sub_80F0420 to match asm/nonmatching/sub_80F0420.s, then delete this #error"
+#endif
