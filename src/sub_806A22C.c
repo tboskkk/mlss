03681c4765e045773f1086a290ab372256da304d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:22:30 -0600

    Match sub_806A24C
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_806A22C.c
+++ src/sub_806A22C.c
@@ -16,8 +16,17 @@ void sub_806A22C(void *arg0) {
     }
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_806A24C.s\"");
-#else
-#error "TODO: write sub_806A24C to match asm/nonmatching/sub_806A24C.s, then delete this #error"
-#endif
+s32 stop_sfx_80195A8(s32);                      /* extern */
+u32 sub_8199F30();                                  /* extern */
+extern s32 sub_806A348;
+
+void sub_806A24C(void *arg0) {
+    u32 temp_r4_20;
+
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        stop_sfx_80195A8(0x81);
+        temp_r4_20 = (*(s32 *)((s8 *)(arg0) + (0xA4))) - (*(s32 *)((s8 *)(arg0) + (0xA0)));
+        (*(s32 *)((s8 *)(arg0) + (0xA8))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0xA0))) + (sub_8199F30() % temp_r4_20));
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_806A348;
+    }
+}
