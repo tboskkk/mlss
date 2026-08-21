33d8ac8f9429b063bc098bb7f69775425847b7b7
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:33:13 -0600

    Extract sub_8160F7C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ src/sub_8160F7C.c
@@ -0,0 +1,14 @@
+#include "global.h"
+#include "common.h"
+
+// TODO: add other includes (a matching *.h, other subsystem headers) as
+// this file grows. Left minimal since split_func.py doesn't know yet what
+// sub_8160F7C needs.
+
+asm_unified(".include \"asm/macros.inc\"");
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8160F7C.s\"");
+#else
+#error "TODO: write sub_8160F7C to match asm/nonmatching/sub_8160F7C.s, then delete this #error"
+#endif
