ae12e94fdf56610f439da0805aaaf5d5ced71a2e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:49:31 -0600

    Extract sub_81127B8
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p567.s
+++ asm/text08057568_p567.s
@@ -3,38 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_81127B8
-sub_81127B8:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	bl sub_8087CE4
-	lsls r0, r0, #0x18
-	cmp r0, #0x00
-	bne _081127EC
-	adds r0, r4, #0x0
-	movs r1, #0x0B
-	movs r2, #0x00
-	movs r3, #0x00
-	bl sub_8082E1C
-	ldr r2, [r4, #0x08]
-	ldrb r1, [r2, #0x12]
-	movs r0, #0x07
-	negs r0, r0
-	ands r0, r1
-	movs r1, #0x02
-	orrs r0, r1
-	strb r0, [r2, #0x12]
-	ldr r0, _081127F4 @ =0x08111B39
-	str r0, [r4, #0x4C]
-	ldr r0, _081127F8 @ =0x0000011B
-	bl stop_sfx_80195A8
-_081127EC:
-	pop {r4}
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_081127F4: .4byte sub_8111B38
-_081127F8: .4byte 0x0000011B
 	thumb_func_start sub_81127FC
 sub_81127FC:
 	push {r4, lr}
