8d41a79a8fae6d1e02267cbfa377da32a1c13c2b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 18:04:53 -0600

    Draft m2c seeds for sub_80F8C60.c's #error placeholders
    Unblocks the whole translation unit (sub_80F8C60, sub_80F8C8C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80F8C60.c
+++ src/sub_80F8C60.c
@@ -10,11 +10,19 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F8C60.s\"");
 #else
-#error "TODO: write sub_80F8C60 to match asm/nonmatching/sub_80F8C60.s, then delete this #error"
+void sub_80F8C60(void *arg0, void *arg1) {
+    if ((*(s32 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x2C)))) + (0x1788))) & 0x4000) {
+        (*(u16 *)((s8 *)(arg1) + (0xA0))) = (u16) (0xFDFF & (*(u16 *)((s8 *)(arg1) + (0xA0))));
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F8C8C.s\"");
 #else
-#error "TODO: write sub_80F8C8C to match asm/nonmatching/sub_80F8C8C.s, then delete this #error"
+void sub_80F8C8C(void *arg0, void *arg1) {
+    if (!(0x20 & (*(u16 *)((s8 *)(((((u8) (*(u8 *)((s8 *)(arg1) + (0xFD))) >> 4) << 8) + (*(s32 *)((s8 *)(arg0) + (0x2C))))) + (0x7A0))))) {
+        (*(u16 *)((s8 *)(arg1) + (0xA0))) = (u16) (0xF7FF & (*(u16 *)((s8 *)(arg1) + (0xA0))));
+    }
+}
 #endif
