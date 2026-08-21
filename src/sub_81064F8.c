025deb3bf09d8696f62040fb0feed27e3d2933a6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:36:44 -0600

    Draft m2c seeds for sub_81064F8.c's #error placeholders
    Unblocks the whole translation unit (sub_81064F8). 1 left empty (seed didn't compile): sub_8106564. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_81064F8.c
+++ src/sub_81064F8.c
@@ -10,11 +10,41 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81064F8.s\"");
 #else
-#error "TODO: write sub_81064F8 to match asm/nonmatching/sub_81064F8.s, then delete this #error"
+extern s32 sub_8106650;
+
+void sub_81064F8(void *arg0) {
+    s32 var_r0_16;
+    s32 var_r2_43;
+    void *temp_r0_10;
+    void *temp_r3_9;
+
+    temp_r3_9 = *(void **)0x03000FD8;
+    temp_r0_10 = (*(void **)((s8 *)(temp_r3_9) + (0)));
+    var_r0_16 = M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */);
+    if (var_r0_16 < 0) {
+        var_r0_16 += 0xFF;
+    }
+    (*(u16 *)((s8 *)(temp_r0_10) + (0x40))) = (u16) (((*(u16 *)((s8 *)(temp_r0_10) + (0x40))) - 1) - (var_r0_16 >> 8));
+    (*(u16 *)((s8 *)(arg0) + (0x18))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0x18))) + 0x19);
+    if (M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) != 0) {
+        (*(u16 *)((s8 *)(arg0) + (0x10))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0x10))) - 1);
+        return;
+    }
+    var_r2_43 = 0xE8;
+    if (4 & (*(u8 *)((s8 *)(temp_r3_9) + (0x2C0)))) {
+        var_r2_43 = 0xFA;
+    }
+    play_sfx_80195B4(var_r2_43, -1);
+    (*(u16 *)((s8 *)(arg0) + (0x10))) = 0x28U;
+    (*(s32 **)((s8 *)(arg0) + (4))) = &sub_8106650;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8106564.s\"");
 #else
-#error "TODO: write sub_8106564 to match asm/nonmatching/sub_8106564.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
