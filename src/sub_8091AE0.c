9d4179fae0eb3af5db60e39f1ab45636c76a7a65
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:23:22 -0600

    Draft m2c seeds for sub_8091AE0.c's #error placeholders
    Unblocks the whole translation unit (sub_8091AE0, sub_8091B30). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8091AE0.c
+++ src/sub_8091AE0.c
@@ -10,11 +10,55 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8091AE0.s\"");
 #else
-#error "TODO: write sub_8091AE0 to match asm/nonmatching/sub_8091AE0.s, then delete this #error"
+s32 sub_808DD2C(void *);                        /* extern */
+
+void sub_8091AE0(void *arg0) {
+    u16 temp_r1_34;
+    void *temp_r1_8;
+
+    temp_r1_8 = (*(void **)((s8 *)(arg0) + (0x28)));
+    if (temp_r1_8 != NULL) {
+        (*(u8 *)((s8 *)(temp_r1_8) + (0x111))) = (u8) (-5 & (*(u8 *)((s8 *)(temp_r1_8) + (0x111))));
+    }
+    if (0x38 & (*(u8 *)((s8 *)(arg0) + (0x76)))) {
+        sub_808DD2C(arg0);
+        return;
+    }
+    temp_r1_34 = (*(u16 *)((s8 *)(arg0) + (0xAC)));
+    (*(u16 *)((s8 *)(arg0) + (0xAC))) = (u16) (temp_r1_34 + 1);
+    if ((s32) (s16) temp_r1_34 > 0x17) {
+        sub_808DD2C(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8091B30.s\"");
 #else
-#error "TODO: write sub_8091B30 to match asm/nonmatching/sub_8091B30.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+s32 sub_808DD2C(void *);                        /* extern */
+extern s32 sub_8091938;
+
+void sub_8091B30(void *arg0) {
+    s32 temp_r0_20;
+    void *temp_r1_27;
+    void *temp_r2_42;
+
+    if (0x38 & (*(u8 *)((s8 *)(arg0) + (0x76)))) {
+        sub_808DD2C(arg0);
+        return;
+    }
+    temp_r0_20 = (*(s32 *)((s8 *)(arg0) + (0x9C)));
+    if (temp_r0_20 > 0) {
+        (*(s32 *)((s8 *)(arg0) + (0x9C))) = (s32) (temp_r0_20 - 1);
+        return;
+    }
+    temp_r1_27 = (*(void **)((s8 *)(arg0) + (0x30)));
+    (*(s32 *)((s8 *)(temp_r1_27) + (0xA0))) = 0xB400;
+    (*(s32 *)((s8 *)(temp_r1_27) + (0xA4))) = 0xA00;
+    sub_8082E1C(arg0, 2, 0, 0);
+    temp_r2_42 = (*(void **)((s8 *)(arg0) + (8)));
+    (*(u8 *)((s8 *)(temp_r2_42) + (0x12))) = (u8) ((*(u8 *)((s8 *)(temp_r2_42) + (0x12))) | 0x10);
+    (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8091938;
+}
 #endif
