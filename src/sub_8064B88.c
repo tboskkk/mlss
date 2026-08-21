4669ae0acfd5f6e0166f732730c63e34a92a02a0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:18:41 -0600

    Draft m2c seeds for sub_8064B88.c's #error placeholders
    Unblocks the whole translation unit (sub_8064C80, sub_8064CB0, sub_8064D20, sub_8064D64). 1 left empty (seed didn't compile): sub_8064C00. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8064B88.c
+++ src/sub_8064B88.c
@@ -41,29 +41,95 @@ void sub_8064B88(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8064C00.s\"");
 #else
-#error "TODO: write sub_8064C00 to match asm/nonmatching/sub_8064C00.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8064C80.s\"");
 #else
-#error "TODO: write sub_8064C80 to match asm/nonmatching/sub_8064C80.s, then delete this #error"
+s32 sub_807C298(void *);                        /* extern */
+
+void sub_8064C80(void *arg0) {
+    void *temp_r1_8;
+
+    temp_r1_8 = (*(void **)((s8 *)(arg0) + (8)));
+    (*(u16 *)((s8 *)(temp_r1_8) + (0xC))) = (u16) ((*(u16 *)((s8 *)(temp_r1_8) + (0xC))) + 0x1000);
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x28)))) + (0xB4))) = 1;
+        sub_807C298(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8064CB0.s\"");
 #else
-#error "TODO: write sub_8064CB0 to match asm/nonmatching/sub_8064CB0.s, then delete this #error"
+s32 stop_sfx_80195A8(s32);                      /* extern */
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_8064DDC;
+
+void sub_8064CB0(void *arg0) {
+    s32 temp_r0_10;
+    s32 var_r1_23;
+
+    temp_r0_10 = M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */);
+    if (temp_r0_10 != 0) {
+        if (temp_r0_10 == 1) {
+            if ((*(s32 *)((s8 *)(arg0) + (0xA0))) == 0) {
+                stop_sfx_80195A8(0x117);
+                var_r1_23 = 7;
+            } else {
+                stop_sfx_80195A8(0x117);
+                var_r1_23 = 0xB;
+            }
+            goto block_7;
+        }
+        if ((*(s32 *)((s8 *)(arg0) + (0xA0))) == 0) {
+            var_r1_23 = 8;
+block_7:
+            sub_8082E1C(arg0, var_r1_23, 0, 0);
+        } else {
+            sub_8082E1C(arg0, 0xC, 0, 0);
+        }
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8064DDC;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8064D20.s\"");
 #else
-#error "TODO: write sub_8064D20 to match asm/nonmatching/sub_8064D20.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_8064614;
+
+void sub_8064D20(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        if ((*(s32 *)((s8 *)(arg0) + (0xA0))) == 0) {
+            sub_8082E1C(arg0, 5, 0, 0);
+        } else {
+            sub_8082E1C(arg0, 9, 0, 0);
+        }
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8064614;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8064D64.s\"");
 #else
-#error "TODO: write sub_8064D64 to match asm/nonmatching/sub_8064D64.s, then delete this #error"
+extern s32 sub_8064E08;
+
+s32 sub_8064D64(void *arg0) {
+    (*(s32 *)((s8 *)(arg0) + (0xA0))) = 0;
+    if (M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */) == -1) {
+        (*(s32 *)((s8 *)(arg0) + (0x9C))) = 0;
+    } else {
+        (*(s32 *)((s8 *)(arg0) + (0x9C))) = 1;
+    }
+    (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8064E08;
+    return 1;
+}
 #endif
