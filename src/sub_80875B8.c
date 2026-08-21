8972cdb051fe0567126ef0015bea8694d5be415a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:22:05 -0600

    Draft m2c seeds for sub_80875B8.c's #error placeholders
    Unblocks the whole translation unit (sub_80875E0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80875B8.c
+++ src/sub_80875B8.c
@@ -24,5 +24,32 @@ void sub_80875B8(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80875E0.s\"");
 #else
-#error "TODO: write sub_80875E0 to match asm/nonmatching/sub_80875E0.s, then delete this #error"
+s32 sub_8083A8C(s32, s32);                          /* extern */
+s32 sub_8085ED0(s32, s32);                          /* extern */
+
+s32 sub_80875E0(s32 arg0, s32 arg1) {
+    s32 var_r0_23;
+    s32 var_r4_15;
+    s32 var_r7_9;
+
+    var_r7_9 = 0;
+loop_1:
+    var_r7_9 = sub_8083A8C(arg0, var_r7_9);
+    var_r4_15 = 0;
+loop_2:
+    var_r4_15 = sub_8083A8C(arg1, var_r4_15);
+    var_r0_23 = sub_8085ED0(arg0, arg1);
+    if (var_r0_23 == 0) {
+        if (var_r4_15 < 0) {
+            if (var_r7_9 < 0) {
+                var_r0_23 = 0;
+            } else {
+                goto loop_1;
+            }
+        } else {
+            goto loop_2;
+        }
+    }
+    return var_r0_23;
+}
 #endif
