431e9c6d4dd83b42eeec2998aefd135b6cf21c52
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:22:05 -0600

    Extract sub_806A180
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p190.s
+++ asm/text08057568_p190.s
@@ -3,22 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_806A180
-sub_806A180:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	bl sub_8086D80
-	cmp r0, #0x00
-	bne _0806A196
-	adds r1, r4, #0x0
-	adds r1, #0x9C
-	movs r0, #0x01
-	str r0, [r1, #0x00]
-	movs r0, #0x00
-_0806A196:
-	pop {r4}
-	pop {r1}
-	bx r1
 	thumb_func_start sub_806A19C
 sub_806A19C:
 	push {r4, lr}
--- src/sub_8068818.c
+++ src/sub_8068818.c
@@ -280,3 +280,9 @@ asm_unified(".include \"asm/nonmatching/sub_806A118.s\"");
 #else
 #error "TODO: write sub_806A118 to match asm/nonmatching/sub_806A118.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_806A180.s\"");
+#else
+#error "TODO: write sub_806A180 to match asm/nonmatching/sub_806A180.s, then delete this #error"
+#endif
