7209d8447b6a41b5f7828005c8137d67a26a843b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:10:32 -0600

    Draft m2c seeds for sub_801AA0C.c's #error placeholders
    Unblocks the whole translation unit (sub_801AA0C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_801AA0C.c
+++ src/sub_801AA0C.c
@@ -10,5 +10,40 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_801AA0C.s\"");
 #else
-#error "TODO: write sub_801AA0C to match asm/nonmatching/sub_801AA0C.s, then delete this #error"
+s32 sub_801AEDC();                                  /* extern */
+
+s32 sub_801AA0C(u8 arg0) {
+    u32 temp_r5_12;
+    u8 temp_r4_8;
+
+    temp_r4_8 = arg0;
+    temp_r5_12 = (u32) (*(u32 *)((s8 *)((void *)0x03000D18) + (8))) >> 0x1C;
+    if ((sub_801AEDC() << 0x18) == 0) {
+        switch (temp_r4_8) {                        /* irregular */
+        case 2:
+            (*(s32 *)((s8 *)((void *)0x03000D18) + (0xC))) = (s32) (((u32) ((*(u32 *)((s8 *)((void *)0x03000D18) + (8))) * 0x10) >> 8) & 1);
+            if (temp_r5_12 != 1) {
+                goto block_8;
+            }
+block_13:
+            return 0;
+        case 3:
+            if (temp_r5_12 == 2) {
+                if ((*(s32 *)((s8 *)((void *)0x03000D18) + (0xC))) != ((u32) ((*(u32 *)((s8 *)((void *)0x03000D18) + (8))) * 0x10) >> 8)) {
+                    goto block_8;
+                }
+                goto block_13;
+            }
+            goto block_8;
+        case 4:
+            if (temp_r5_12 == 3) {
+                goto block_13;
+            }
+            goto block_8;
+        }
+    } else {
+block_8:
+        return 1;
+    }
+}
 #endif
