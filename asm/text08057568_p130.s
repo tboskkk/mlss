1d1f897599d3a264ea210c8910febfdeaaf6f15a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:16:11 -0600

    Extract sub_8060AF8
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p130.s
+++ asm/text08057568_p130.s
@@ -3,62 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_8060AF8
-sub_8060AF8:
-	push {r4, r5, lr}
-	adds r4, r0, #0x0
-	adds r0, #0x80
-	ldr r5, [r0, #0x00]
-	cmp r5, #0x00
-	bne _08060B5E
-	adds r0, r4, #0x0
-	movs r1, #0x07
-	movs r2, #0x00
-	movs r3, #0x00
-	bl sub_8082E1C
-	adds r2, r4, #0x0
-	adds r2, #0x84
-	ldr r0, [r4, #0x2C]
-	ldr r1, [r0, #0x28]
-	adds r0, r1, #0x0
-	adds r0, #0xD8
-	ldr r0, [r0, #0x00]
-	str r0, [r2, #0x00]
-	adds r2, #0x04
-	adds r1, #0xDC
-	ldr r0, [r1, #0x00]
-	str r0, [r2, #0x00]
-	adds r1, r4, #0x0
-	adds r1, #0x8C
-	adds r0, r4, #0x0
-	adds r0, #0xB2
-	movs r2, #0x00
-	ldsh r0, [r0, r2]
-	lsls r0, r0, #0x08
-	str r0, [r1, #0x00]
-	adds r2, r4, #0x0
-	adds r2, #0x79
-	ldrb r0, [r2, #0x00]
-	movs r1, #0x20
-	orrs r0, r1
-	strb r0, [r2, #0x00]
-	adds r1, r4, #0x0
-	adds r1, #0x7C
-	movs r0, #0x80
-	lsls r0, r0, #0x01
-	strh r0, [r1, #0x00]
-	adds r0, r4, #0x0
-	adds r0, #0x7A
-	strh r5, [r0, #0x00]
-	adds r0, r4, #0x0
-	bl sub_8085B38
-	ldr r0, _08060B64 @ =0x08060C45
-	str r0, [r4, #0x4C]
-_08060B5E:
-	pop {r4, r5}
-	pop {r0}
-	bx r0
-_08060B64: .4byte sub_8060C44
 	thumb_func_start sub_8060B68
 sub_8060B68:
 	push {r4, r5, r6, r7, lr}
--- src/sub_80604E0.c
+++ src/sub_80604E0.c
@@ -88,3 +88,9 @@ asm_unified(".include \"asm/nonmatching/sub_8060A58.s\"");
 #else
 #error "TODO: write sub_8060A58 to match asm/nonmatching/sub_8060A58.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8060AF8.s\"");
+#else
+#error "TODO: write sub_8060AF8 to match asm/nonmatching/sub_8060AF8.s, then delete this #error"
+#endif
