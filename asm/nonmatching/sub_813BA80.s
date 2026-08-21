ff4934a3b7c5d10842456325f5a885b3186f016a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 21:07:49 -0600

    Extract sub_813BA80
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/nonmatching/sub_813BA80.s
+++ asm/nonmatching/sub_813BA80.s
@@ -1,35 +1,33 @@
-	.include "asm/macros.inc"
-
 	.syntax unified
 	.text
 
-	thumb_func_start sub_813BAF0
-sub_813BAF0:
+	thumb_func_start sub_813BA80
+sub_813BA80:
 	push {r4, r5, lr}
 	adds r4, r1, #0x0
 	adds r5, r2, #0x0
 	movs r2, #0x00
-	bl sub_8139E88
+	bl sub_8139F08
 	lsls r0, r0, #0x18
 	asrs r0, r0, #0x18
 	movs r1, #0x01
 	negs r1, r1
 	cmp r0, r1
-	beq _0813BB0C
+	beq _0813BA9C
 	ldr r0, [r5, #0x08]
-	b _0813BB52
-_0813BB0C:
-	ldr r0, _0813BB1C @ =0x000002DE
+	b _0813BAE2
+_0813BA9C:
+	ldr r0, _0813BAAC @ =0x000002DE
 	adds r2, r4, r0
 	ldrh r0, [r2, #0x00]
 	cmp r0, #0x00
-	bne _0813BB20
+	bne _0813BAB0
 	movs r0, #0x00
-	b _0813BB4C
+	b _0813BADC
 	.byte 0x00, 0x00
-_0813BB1C: .4byte 0x000002DE
-_0813BB20:
-	ldr r1, _0813BB5C @ =0x00000242
+_0813BAAC: .4byte 0x000002DE
+_0813BAB0:
+	ldr r1, _0813BAEC @ =0x00000242
 	adds r0, r4, r1
 	movs r3, #0x00
 	ldsh r1, [r0, r3]
@@ -51,15 +49,15 @@ _0813BB20:
 	lsrs r0, r0, #0x10
 	negs r0, r0
 	lsrs r0, r0, #0x1F
-_0813BB4C:
+_0813BADC:
 	cmp r0, #0x00
-	bne _0813BB54
+	bne _0813BAE4
 	ldr r0, [r5, #0x04]
-_0813BB52:
+_0813BAE2:
 	str r0, [r5, #0x00]
-_0813BB54:
+_0813BAE4:
 	pop {r4, r5}
 	pop {r0}
 	bx r0
 	.byte 0x00, 0x00
-_0813BB5C: .4byte 0x00000242
+_0813BAEC: .4byte 0x00000242
--- src/sub_813B9B4.c
+++ src/sub_813B9B4.c
@@ -48,3 +48,9 @@ asm_unified(".include \"asm/nonmatching/sub_813BA0C.s\"");
 #else
 #error "TODO: write sub_813BA0C to match asm/nonmatching/sub_813BA0C.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_813BA80.s\"");
+#else
+#error "TODO: write sub_813BA80 to match asm/nonmatching/sub_813BA80.s, then delete this #error"
+#endif
