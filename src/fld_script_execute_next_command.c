527a3574a6ebb29b0745dd61b317057156f2654f
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:58:38 -0600

    Extract sub_80EC970
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/fld_script_execute_next_command.c
+++ src/fld_script_execute_next_command.c
@@ -12,3 +12,9 @@ asm_unified(".include \"asm/nonmatching/fld_script_execute_next_command.s\"");
 #else
 #error "TODO: write fld_script_execute_next_command to match asm/nonmatching/fld_script_execute_next_command.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80EC970.s\"");
+#else
+#error "TODO: write sub_80EC970 to match asm/nonmatching/sub_80EC970.s, then delete this #error"
+#endif
