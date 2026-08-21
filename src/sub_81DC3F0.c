17123d8d3c36dfce19edb4017c90738d634fafec
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:48:37 -0600

    Draft m2c seeds for sub_81DC3F0.c's #error placeholders
    Unblocks the whole translation unit (sub_81DC3F0). 1 left empty (seed didn't compile): sub_81DC434. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_81DC3F0.c
+++ src/sub_81DC3F0.c
@@ -10,11 +10,27 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81DC3F0.s\"");
 #else
-#error "TODO: write sub_81DC3F0 to match asm/nonmatching/sub_81DC3F0.s, then delete this #error"
+s32 sub_81DBAE8(s32 *);                         /* extern */
+
+void sub_81DC3F0(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    s32 sp0;
+    s32 sp4;
+    s32 sp8;
+    s32 spC;
+
+    sp0 = arg0;
+    sp4 = arg1;
+    sp8 = arg2;
+    spC = arg3;
+    sub_81DBAE8(&sp0);
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81DC434.s\"");
 #else
-#error "TODO: write sub_81DC434 to match asm/nonmatching/sub_81DC434.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
