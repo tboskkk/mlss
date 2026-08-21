00615ef2fab2eeb357d3cf082dc668cfe2528800
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:29:24 -0600

    Draft m2c seeds for sub_80DB0FC.c's #error placeholders
    Unblocks the whole translation unit (sub_80DB0FC, sub_80DB12C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80DB0FC.c
+++ src/sub_80DB0FC.c
@@ -10,11 +10,38 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DB0FC.s\"");
 #else
-#error "TODO: write sub_80DB0FC to match asm/nonmatching/sub_80DB0FC.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_80DABFC;
+
+void sub_80DB0FC(void *arg0) {
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        sub_8082E1C(arg0, 3, 0, 0);
+        (*(s16 *)((s8 *)(arg0) + (0xAC))) = 0x18;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DABFC;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DB12C.s\"");
 #else
-#error "TODO: write sub_80DB12C to match asm/nonmatching/sub_80DB12C.s, then delete this #error"
+s32 sub_807F4FC(void *);                        /* extern */
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_80DAD00;
+extern s32 sub_80DB184;
+extern s32 sub_80DB1A4;
+extern s32 sub_80DB1C4;
+
+void sub_80DB12C(void *arg0) {
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        play_sfx_80195B4(0x2E, -1);
+        sub_807F4FC(arg0);
+        (*(s32 **)((s8 *)(arg0) + (0x58))) = &sub_80DB1C4;
+        (*(s32 **)((s8 *)(arg0) + (0x5C))) = &sub_80DB1A4;
+        (*(s32 **)((s8 *)(arg0) + (0x60))) = &sub_80DB184;
+        sub_8082E1C(arg0, 7, 0, 0);
+        (*(s16 *)((s8 *)(arg0) + (0xAC))) = 0x10;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DAD00;
+    }
+}
 #endif
