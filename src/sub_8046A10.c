303aa8509215644bbed1d72a4e31283bfafa6736
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:15:56 -0600

    Draft m2c seeds for sub_8046A10.c's #error placeholders
    Unblocks the whole translation unit (sub_8046A90, sub_8046B30). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8046A10.c
+++ src/sub_8046A10.c
@@ -29,11 +29,73 @@ void sub_8046A10(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8046A90.s\"");
 #else
-#error "TODO: write sub_8046A90 to match asm/nonmatching/sub_8046A90.s, then delete this #error"
+s32 sub_8046A90(void *arg0, s32 arg1, s32 arg2, u16 arg3, s32 arg4) {
+    s16 temp_r0_42;
+    s32 temp_r2_40;
+    u16 temp_r5_13;
+    u16 var_r1_50;
+    u16 var_r3_11;
+
+    var_r3_11 = arg3;
+    temp_r5_13 = (u16) arg4;
+    if (arg2 == 1) {
+        var_r3_11 = (s16) var_r3_11 + (*(u16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x294)))) + (4)));
+    }
+    (*(u16 *)((s8 *)(arg0) + (0x2FA))) = (u16) (*(u16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x294)))) + (4)));
+    (*(u16 *)((s8 *)(arg0) + (0x302))) = var_r3_11;
+    temp_r2_40 = M2C_ERROR(/* unknown instruction: ldsh $r2, ($mem_loc_fictive_) */);
+    temp_r0_42 = (s16) var_r3_11;
+    if (temp_r2_40 == temp_r0_42) {
+        return 0;
+    }
+    if (arg1 == 1) {
+        var_r1_50 = temp_r5_13;
+        if (temp_r2_40 >= (s32) temp_r0_42) {
+            var_r1_50 = 0 - temp_r5_13;
+        }
+        (*(u16 *)((s8 *)(arg0) + (0x30A))) = var_r1_50;
+    } else {
+        (*(u16 *)((s8 *)(arg0) + (0x31A))) = temp_r5_13;
+    }
+    (*(s16 *)((s8 *)(arg0) + (0x312))) = 0;
+    (*(u8 *)((s8 *)(arg0) + (0x2B5))) = (u8) ((-0x31 & (*(u8 *)((s8 *)(arg0) + (0x2B5)))) | ((arg1 & 3) * 0x10));
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8046B30.s\"");
 #else
-#error "TODO: write sub_8046B30 to match asm/nonmatching/sub_8046B30.s, then delete this #error"
+s32 sub_8046B30(void *arg0, s32 arg1, s32 arg2, u16 arg3, s32 arg4) {
+    s16 temp_r0_44;
+    s32 temp_r2_42;
+    u16 temp_r4_13;
+    u16 var_r1_52;
+    u16 var_r3_11;
+
+    var_r3_11 = arg3;
+    temp_r4_13 = (u16) arg4;
+    if (arg2 == 1) {
+        var_r3_11 = (s16) var_r3_11 + (*(u16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x294)))) + (6)));
+    }
+    (*(u16 *)((s8 *)(arg0) + (0x2FC))) = (u16) (*(u16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x294)))) + (6)));
+    (*(u16 *)((s8 *)(arg0) + (0x304))) = var_r3_11;
+    temp_r2_42 = M2C_ERROR(/* unknown instruction: ldsh $r2, ($mem_loc_fictive_) */);
+    temp_r0_44 = (s16) var_r3_11;
+    if (temp_r2_42 == temp_r0_44) {
+        return 0;
+    }
+    if (arg1 == 1) {
+        var_r1_52 = temp_r4_13;
+        if (temp_r2_42 >= (s32) temp_r0_44) {
+            var_r1_52 = 0 - temp_r4_13;
+        }
+        (*(u16 *)((s8 *)(arg0) + (0x30C))) = var_r1_52;
+    } else {
+        (*(u16 *)((s8 *)(arg0) + (0x31C))) = temp_r4_13;
+    }
+    (*(s16 *)((s8 *)(arg0) + (0x314))) = 0;
+    (*(u8 *)((s8 *)(arg0) + (0x2B5))) = (u8) ((0x3F & (*(u8 *)((s8 *)(arg0) + (0x2B5)))) | (arg1 << 6));
+    return 1;
+}
 #endif
