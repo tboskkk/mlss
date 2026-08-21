7e318e9039cc0a667209edaa5e2fd00c5d178f9e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:47:57 -0600

    Draft m2c seeds for sub_819AF88.c's #error placeholders
    Unblocks the whole translation unit (sub_819AF88). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_819AF88.c
+++ src/sub_819AF88.c
@@ -10,5 +10,13 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_819AF88.s\"");
 #else
-#error "TODO: write sub_819AF88 to match asm/nonmatching/sub_819AF88.s, then delete this #error"
+s32 sub_819AC84(s32);                           /* extern */
+s32 sub_819AF18();                              /* extern */
+
+void sub_819AF88(void) {
+    sub_819AC84(0);
+    sub_819AC84(1);
+    sub_819AF18();
+    sub_819AC84(3);
+}
 #endif
