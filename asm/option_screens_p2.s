1ac0aa9021199a8364867bbaf82943fead897e6e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 19:58:05 -0600

    Extract sub_805420C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/option_screens_p2.s
+++ asm/option_screens_p2.s
@@ -3,42 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_805420C
-sub_805420C:
-	push {r4, r5, r6, lr}
-	add sp, #-0x00C
-	adds r4, r0, #0x0
-	ldr r1, _08054250 @ =0x083A2918
-	ldrb r0, [r1, #0x00]
-	lsls r0, r0, #0x18
-	lsrs r6, r0, #0x18
-	ldrb r1, [r1, #0x02]
-	lsls r1, r1, #0x18
-	lsrs r5, r1, #0x18
-	lsrs r0, r0, #0x19
-	movs r2, #0x0F
-	subs r2, r2, r0
-	lsls r2, r2, #0x18
-	lsrs r2, r2, #0x18
-	lsrs r1, r1, #0x19
-	movs r3, #0x0A
-	subs r3, r3, r1
-	lsls r3, r3, #0x18
-	lsrs r3, r3, #0x18
-	adds r0, r4, #0x0
-	adds r0, #0x48
-	ldr r1, [r4, #0x40]
-	str r6, [sp, #0x000]
-	str r5, [sp, #0x004]
-	movs r4, #0xC8
-	lsls r4, r4, #0x08
-	str r4, [sp, #0x008]
-	bl generate_window_bg_8051B98
-	add sp, #0x00C
-	pop {r4, r5, r6}
-	pop {r0}
-	bx r0
-_08054250: .4byte 0x083A2918
 	thumb_func_start option_screen_set_ok_button
 option_screen_set_ok_button:
 	push {r4, r5, r6, r7, lr}
--- src/sub_8052EFC.c
+++ src/sub_8052EFC.c
@@ -66,3 +66,9 @@ asm_unified(".include \"asm/nonmatching/sub_80541B0.s\"");
 #else
 #error "TODO: write sub_80541B0 to match asm/nonmatching/sub_80541B0.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_805420C.s\"");
+#else
+#error "TODO: write sub_805420C to match asm/nonmatching/sub_805420C.s, then delete this #error"
+#endif
