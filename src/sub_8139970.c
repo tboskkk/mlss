a72603ad25ecd011c0e66ec2283ec90b59fec88a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:40:55 -0600

    Draft m2c seeds for sub_8139970.c's #error placeholders
    Unblocks the whole translation unit (sub_8139988, sub_81399C4, sub_8139A04, sub_8139A38, sub_8139A6C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8139970.c
+++ src/sub_8139970.c
@@ -21,29 +21,72 @@ void sub_8139970(void *arg1) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8139988.s\"");
 #else
-#error "TODO: write sub_8139988 to match asm/nonmatching/sub_8139988.s, then delete this #error"
+s32 sub_81396A8(s32, void *);               /* extern */
+extern s32 sub_813967C;
+
+void sub_8139988(s32 arg0, void *arg1) {
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_81396A8;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_813967C;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_813967C;
+    (*(s32 **)((s8 *)(arg1) + (0x1AC))) = &sub_813967C;
+    sub_81396A8(arg0, arg1);
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81399C4.s\"");
 #else
-#error "TODO: write sub_81399C4 to match asm/nonmatching/sub_81399C4.s, then delete this #error"
+s32 sub_813B380(s32, void *);               /* extern */
+extern s32 sub_81396D8;
+extern s32 sub_8139708;
+
+void sub_81399C4(s32 arg0, void *arg1) {
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_813B380;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_8139708;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_8139708;
+    (*(s32 **)((s8 *)(arg1) + (0x1AC))) = &sub_81396D8;
+    sub_813B380(arg0, arg1);
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8139A04.s\"");
 #else
-#error "TODO: write sub_8139A04 to match asm/nonmatching/sub_8139A04.s, then delete this #error"
+s32 sub_813B380(s32, void *);               /* extern */
+extern s32 sub_81368AC;
+
+void sub_8139A04(s32 arg0, void *arg1) {
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_813B380;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_81368AC;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_81368AC;
+    sub_813B380(arg0, arg1);
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8139A38.s\"");
 #else
-#error "TODO: write sub_8139A38 to match asm/nonmatching/sub_8139A38.s, then delete this #error"
+s32 sub_813B380(s32, void *);               /* extern */
+extern s32 sub_81397CC;
+
+void sub_8139A38(s32 arg0, void *arg1) {
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_813B380;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_81397CC;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_81397CC;
+    sub_813B380(arg0, arg1);
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8139A6C.s\"");
 #else
-#error "TODO: write sub_8139A6C to match asm/nonmatching/sub_8139A6C.s, then delete this #error"
+s32 sub_81398C4(s32, void *);               /* extern */
+extern s32 sub_8139880;
+
+void sub_8139A6C(s32 arg0, void *arg1) {
+    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_81398C4;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_8139880;
+    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_8139880;
+    sub_81398C4(arg0, arg1);
+}
 #endif
