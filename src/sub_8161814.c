a39d069fb36b50ad45d1d0f14ed209340bca6536
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:45:03 -0600

    Draft m2c seeds for sub_8161814.c's #error placeholders
    Unblocks the whole translation unit (sub_8161888, sub_8161924). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8161814.c
+++ src/sub_8161814.c
@@ -47,11 +47,43 @@ block_9:
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8161888.s\"");
 #else
-#error "TODO: write sub_8161888 to match asm/nonmatching/sub_8161888.s, then delete this #error"
+void sub_8161888(void *arg0) {
+    void *temp_r0_22;
+    void *temp_r0_48;
+
+    temp_r0_22 = sub_8020DD0(4, 0x5005, 1, -1, -1, -1, -1);
+    (*(void **)((s8 *)(arg0) + (8))) = temp_r0_22;
+    sub_801E150(temp_r0_22, 0x2D, -1, 0, 0);
+    sprite_show_8020CBC((*(void **)((s8 *)(arg0) + (8))));
+    (*(s32 *)((s8 *)(arg0) + (0x10))) = 0;
+    (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0))) = 0;
+    (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (2))) = 0x50;
+    temp_r0_48 = sub_8020DD0(4, 0x5005, 1, -1, -1, -1, -1);
+    (*(void **)((s8 *)(arg0) + (0xC))) = temp_r0_48;
+    sub_801E150(temp_r0_48, 0x33, -1, 0, 0);
+    sprite_show_8020CBC((*(void **)((s8 *)(arg0) + (0xC))));
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = 0xF000;
+    (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0xC)))) + (0))) = 0xF0;
+    (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0xC)))) + (2))) = 0x50;
+    (*(s16 *)((s8 *)(arg0) + (0))) = 0x1E;
+    (*(s16 *)((s8 *)(arg0) + (2))) = 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8161924.s\"");
 #else
-#error "TODO: write sub_8161924 to match asm/nonmatching/sub_8161924.s, then delete this #error"
+s32 sub_8021308();                              /* extern */
+
+void sub_8161924(void *arg0, s32 arg1) {
+    if ((*(s32 *)((s8 *)(arg0) + (8))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0xC))) != 0) {
+        sub_8021308();
+    }
+    if (1 & arg1) {
+        free_heap_8018DA8(arg0);
+    }
+}
 #endif
