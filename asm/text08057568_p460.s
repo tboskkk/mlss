38e28f146ac70a4c5ae959997a5034e36d163159
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:33:43 -0600

    Extract sub_80EAACC
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p460.s
+++ asm/text08057568_p460.s
@@ -3,27 +3,3 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80EAACC
-sub_80EAACC:
-	push {lr}
-	ldr r3, _080EAAF4 @ =0x0300034C
-	ldr r0, [r2, #0x00]
-	ldr r1, _080EAAF8 @ =0x000003FF
-	adds r2, r1, #0x0
-	adds r1, r2, #0x0
-	bics r1, r0
-	adds r3, #0x78
-	ands r1, r2
-	ldrh r2, [r3, #0x00]
-	ldr r0, _080EAAFC @ =0xFFFFFC00
-	ands r0, r2
-	orrs r0, r1
-	strh r0, [r3, #0x00]
-	bl sub_8018818
-	movs r0, #0x01
-	pop {r1}
-	bx r1
-	.byte 0x00, 0x00
-_080EAAF4: .4byte 0x0300034C
-_080EAAF8: .4byte 0x000003FF
-_080EAAFC: .4byte 0xFFFFFC00
--- src/sub_80EAA5C.c
+++ src/sub_80EAA5C.c
@@ -18,3 +18,9 @@ asm_unified(".include \"asm/nonmatching/sub_80EAA84.s\"");
 #else
 #error "TODO: write sub_80EAA84 to match asm/nonmatching/sub_80EAA84.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80EAACC.s\"");
+#else
+#error "TODO: write sub_80EAACC to match asm/nonmatching/sub_80EAACC.s, then delete this #error"
+#endif
