54e5c5ab2f474135b8cb0f547b617e99012c9c11
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:38:21 -0600

    Extract sub_8090528
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/nonmatching/sub_8090528.s
+++ asm/nonmatching/sub_8090528.s
@@ -1,10 +1,8 @@
-	.include "asm/macros.inc"
-
 	.syntax unified
 	.text
 
-	thumb_func_start sub_8090598
-sub_8090598:
+	thumb_func_start sub_8090528
+sub_8090528:
 	push {r4, lr}
 	adds r4, r0, #0x0
 	adds r2, r4, #0x0
@@ -22,16 +20,15 @@ sub_8090598:
 	movs r1, #0x00
 	ldsh r0, [r2, r1]
 	cmp r0, #0x00
-	bgt _080905FC
-	ldr r0, _08090604 @ =0x08090661
+	bgt _0809058A
+	ldr r0, _08090590 @ =0x08090609
 	str r0, [r4, #0x4C]
 	adds r0, r4, #0x0
 	bl sub_808CAC4
 	lsls r0, r0, #0x18
 	cmp r0, #0x00
-	bne _080905FC
-	movs r2, #0x80
-	lsls r2, r2, #0x06
+	bne _0809058A
+	ldr r2, _08090594 @ =0x0000204D
 	adds r0, r4, #0x0
 	movs r1, #0x06
 	movs r3, #0x00
@@ -53,9 +50,9 @@ sub_8090598:
 	movs r1, #0x01
 	orrs r0, r1
 	strb r0, [r2, #0x00]
-_080905FC:
+_0809058A:
 	pop {r4}
 	pop {r0}
 	bx r0
-	.byte 0x00, 0x00
-_08090604: .4byte sub_8090660
+_08090590: .4byte sub_8090608
+_08090594: .4byte 0x0000204D
