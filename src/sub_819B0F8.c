449b402040139045dff1ce17fe220c91282ea021
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:24:37 -0600

    Extract sub_819B110
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_819B0F8.c
+++ src/sub_819B0F8.c
@@ -18,3 +18,9 @@ u32* sub_819B0F8(u32 index, u32 value) {
     return ptr;
 }
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_819B110.s\"");
+#else
+#error "TODO: write sub_819B110 to match asm/nonmatching/sub_819B110.s, then delete this #error"
+#endif
