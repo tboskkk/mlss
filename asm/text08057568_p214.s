d4fd83c9905a769dd08a40a26577f711d390654d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:25:20 -0600

    Extract sub_819BA00
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p214.s
+++ asm/text08057568_p214.s
@@ -3,15 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_819BA00
-sub_819BA00:
-	push {lr}
-	movs r0, #0xE0
-	lsls r0, r0, #0x07
-	bl sub_819B418
-	pop {r1}
-	bx r1
-	.byte 0x00, 0x00
 	thumb_func_start sub_819BA10
 sub_819BA10:
 	push {lr}
--- src/sub_819B418.c
+++ src/sub_819B418.c
@@ -66,3 +66,9 @@ asm_unified(".include \"asm/nonmatching/sub_819B9E8.s\"");
 #else
 #error "TODO: write sub_819B9E8 to match asm/nonmatching/sub_819B9E8.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_819BA00.s\"");
+#else
+#error "TODO: write sub_819BA00 to match asm/nonmatching/sub_819BA00.s, then delete this #error"
+#endif
