2340eb070b997251f7e8c81932983f1bfbd6307d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:20:12 -0600

    Draft m2c seeds for sub_8070AE8.c's #error placeholders
    Unblocks the whole translation unit (sub_8070B70, sub_8070C2C, sub_8070C5C). 1 left empty (seed didn't compile): sub_8070BC8. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8070AE8.c
+++ src/sub_8070AE8.c
@@ -32,23 +32,69 @@ s32 sub_8070AE8(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8070B70.s\"");
 #else
-#error "TODO: write sub_8070B70 to match asm/nonmatching/sub_8070B70.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+s32 sub_80DF024(s32, s32, s32, s32, void *);    /* extern */
+extern s32 sub_8070EDC;
+
+void sub_8070B70(void *arg0) {
+    s32 var_r1_20;
+    s32 var_r2_26;
+    s32 var_r3_32;
+
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8082E1C(arg0, 0x2B, 0, 0);
+        var_r1_20 = (*(s32 *)((s8 *)(arg0) + (0x38)));
+        if (var_r1_20 < 0) {
+            var_r1_20 += 0xFF;
+        }
+        var_r2_26 = (*(s32 *)((s8 *)(arg0) + (0x3C)));
+        if (var_r2_26 < 0) {
+            var_r2_26 += 0xFF;
+        }
+        var_r3_32 = (*(s32 *)((s8 *)(arg0) + (0x40)));
+        if (var_r3_32 < 0) {
+            var_r3_32 += 0xFF;
+        }
+        sub_80DF024(0x201C, var_r1_20 >> 8, var_r2_26 >> 8, var_r3_32 >> 8, arg0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8070EDC;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8070BC8.s\"");
 #else
-#error "TODO: write sub_8070BC8 to match asm/nonmatching/sub_8070BC8.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8070C2C.s\"");
 #else
-#error "TODO: write sub_8070C2C to match asm/nonmatching/sub_8070C2C.s, then delete this #error"
+s32 stop_sfx_80195A8(s32);                      /* extern */
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_8070F64;
+
+void sub_8070C2C(void *arg0) {
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        stop_sfx_80195A8(0x11C);
+        sub_8082E1C(arg0, 0x1C, 0, 0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8070F64;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8070C5C.s\"");
 #else
-#error "TODO: write sub_8070C5C to match asm/nonmatching/sub_8070C5C.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+
+void sub_8070C5C(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8082E1C(arg0, 0, 0, 0);
+        (*(s32 *)((s8 *)(arg0) + (0x4C))) = 0;
+    }
+}
 #endif
