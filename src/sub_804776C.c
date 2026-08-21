0e257af192240978cb06c3d67dc2c87cc5eb1d5b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:16:09 -0600

    Draft m2c seeds for sub_804776C.c's #error placeholders
    Unblocks the whole translation unit (sub_80477A8). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_804776C.c
+++ src/sub_804776C.c
@@ -22,5 +22,15 @@ void sub_8047784(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80477A8.s\"");
 #else
-#error "TODO: write sub_80477A8 to match asm/nonmatching/sub_80477A8.s, then delete this #error"
+void sub_80477A8(void *arg0, s32 arg1, s8 arg2) {
+    void *temp_r5_19;
+    void *temp_r5_30;
+
+    (*(u8 *)((s8 *)(arg0) + (0x20D))) = (u8) (-0x11 & (*(u8 *)((s8 *)(arg0) + (0x20D))));
+    temp_r5_19 = (*(void **)((s8 *)(arg0) + (0x294)));
+    (*(u8 *)((s8 *)(temp_r5_19) + (0x1F))) = (u8) ((-4 & (*(u8 *)((s8 *)(temp_r5_19) + (0x1F)))) | (arg1 & 3));
+    temp_r5_30 = (*(void **)((s8 *)(arg0) + (0x294)));
+    (*(u8 *)((s8 *)(temp_r5_30) + (0x1F))) = (u8) ((-0xD & (*(u8 *)((s8 *)(temp_r5_30) + (0x1F)))) | ((arg2 & 3) * 4));
+    (*(s8 *)((s8 *)(arg0) + (0x23C))) = arg2;
+}
 #endif
