e33212752a19f3c1667b693a95c4bfe0d5daae3d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:33:15 -0600

    Extract sub_80DAF84
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p525.s
+++ asm/text08057568_p525.s
@@ -3,24 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80DAF84
-sub_80DAF84:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	bl sub_8082B00
-	cmp r0, #0x00
-	bne _080DAF9E
-	movs r1, #0x01
-	negs r1, r1
-	movs r0, #0x8D
-	bl play_sfx_80195B4
-	ldr r0, _080DAFA4 @ =0x080DB015
-	str r0, [r4, #0x4C]
-_080DAF9E:
-	pop {r4}
-	pop {r0}
-	bx r0
-_080DAFA4: .4byte sub_80DB014
 	thumb_func_start sub_80DAFA8
 sub_80DAFA8:
 	push {r4, lr}
