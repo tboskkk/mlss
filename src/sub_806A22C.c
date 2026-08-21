7df3d9a91c408f152568b9437940d849cc20c5f4
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:26:55 -0600

    Draft m2c seeds for sub_806A22C.c's #error placeholders
    Unblocks the whole translation unit (sub_806A290). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_806A22C.c
+++ src/sub_806A22C.c
@@ -34,5 +34,21 @@ void sub_806A24C(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_806A290.s\"");
 #else
-#error "TODO: write sub_806A290 to match asm/nonmatching/sub_806A290.s, then delete this #error"
+extern s32 sub_80695E4;
+
+s32 sub_806A290(void *arg0) {
+    s32 *var_r1_18;
+    s32 var_r0_19;
+
+    if (M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */) == -1) {
+        var_r1_18 = arg0 + 0x9C;
+        var_r0_19 = 0;
+    } else {
+        var_r1_18 = arg0 + 0x9C;
+        var_r0_19 = 1;
+    }
+    *var_r1_18 = var_r0_19;
+    (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80695E4;
+    return 1;
+}
 #endif
