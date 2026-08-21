d1d13c704e8d86592bb062c9ae642a75f835edf2
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:14:17 -0600

    Draft m2c seeds for sub_801AA70.c's #error placeholders
    Unblocks the whole translation unit (sub_801ABE8, sub_801AC7C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_801AA70.c
+++ src/sub_801AA70.c
@@ -29,11 +29,72 @@ void sub_801AA70(s32 r0, u32 r1, u32 r2, u32 r3)
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_801ABE8.s\"");
 #else
-#error "TODO: write sub_801ABE8 to match asm/nonmatching/sub_801ABE8.s, then delete this #error"
+void sub_801ABE8(u16 arg0, u16 arg1, s32 arg2) {
+    u16 temp_r4_9;
+    u16 temp_r5_11;
+
+    temp_r4_9 = arg0;
+    temp_r5_11 = arg1;
+    if ((6 & *(u8 *)0x03000BD4) == 2) {
+        if (temp_r4_9 != 0) {
+            (*(u8 *)((s8 *)((void *)0x03000D28) + (0))) = (u8) ((((-4 & (*(u8 *)((s8 *)((void *)0x03000D28) + (0)))) | 1) & ~4) | ((arg2 & 1) * 4));
+            (*(u16 *)((s8 *)((void *)0x03000D28) + (2))) = temp_r5_11;
+            (*(u16 *)((s8 *)((void *)0x03000D28) + (4))) = temp_r4_9;
+            return;
+        }
+        if (1 & *(u8 *)0x03000BD7) {
+            *(u8 *)0x03000D18 = (-0x31 & *(u8 *)0x03000D18) | 0x20;
+        }
+        (*(u8 *)((s8 *)((void *)0x03000D28) + (0))) = (u8) ((-4 & (*(u8 *)((s8 *)((void *)0x03000D28) + (0)))) | 2);
+        (*(u16 *)((s8 *)((void *)0x03000D28) + (4))) = temp_r5_11;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_801AC7C.s\"");
 #else
-#error "TODO: write sub_801AC7C to match asm/nonmatching/sub_801AC7C.s, then delete this #error"
+void sub_801AC7C(void) {
+    u16 temp_r0_29;
+    u16 temp_r0_62;
+    u16 temp_r0_65;
+    u32 temp_r1_18;
+    u8 temp_r4_16;
+    u8 var_r0_78;
+
+    if ((6 & *(u8 *)0x03000BD4) == 2) {
+        temp_r4_16 = (*(u8 *)((s8 *)((void *)0x03000D28) + (0)));
+        temp_r1_18 = (u32) (temp_r4_16 << 0x1E) >> 0x1E;
+        switch (temp_r1_18) {                       /* irregular */
+        case 1:
+            temp_r0_29 = (*(u16 *)((s8 *)((void *)0x03000D28) + (4))) - 1;
+            (*(u16 *)((s8 *)((void *)0x03000D28) + (4))) = temp_r0_29;
+            if ((temp_r0_29 << 0x10) == 0) {
+                if (temp_r1_18 & *(u8 *)0x03000BD7) {
+                    *(u32 *)0x03000D18 = (u8) ((-0x31 & *(u32 *)0x03000D18) | 0x20);
+                }
+                (*(u8 *)((s8 *)((void *)0x03000D28) + (0))) = (u8) ((-4 & (*(u8 *)((s8 *)((void *)0x03000D28) + (0)))) | 2);
+                (*(u16 *)((s8 *)((void *)0x03000D28) + (4))) = (u16) (*(u16 *)((s8 *)((void *)0x03000D28) + (2)));
+                return;
+            }
+            break;
+        case 2:
+            temp_r0_62 = (*(u16 *)((s8 *)((void *)0x03000D28) + (4)));
+            if (temp_r0_62 != 0) {
+                temp_r0_65 = temp_r0_62 - 1;
+                (*(u16 *)((s8 *)((void *)0x03000D28) + (4))) = temp_r0_65;
+                if ((temp_r0_65 << 0x10) == 0) {
+                    if (!(4 & temp_r4_16)) {
+                        var_r0_78 = -0x31 & *(u8 *)0x03000D18;
+                    } else {
+                        var_r0_78 = (-0x31 & *(u32 *)0x03000D18) | 0x10;
+                    }
+                    *(u8 *)0x03000D18 = var_r0_78;
+                    (*(u8 *)((s8 *)((void *)0x03000D28) + (0))) = (u8) (-4 & temp_r4_16);
+                }
+            }
+            break;
+        }
+    }
+}
 #endif
