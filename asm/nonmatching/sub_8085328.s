45675798774337af56779d062b1f09f452398d4c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:57:34 -0600

    Extract sub_8085328
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8085328.s
@@ -0,0 +1,61 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8085328
+sub_8085328:
+	push {r4, r5, r6, r7, lr}
+	mov r7, r8
+	push {r7}
+	adds r1, r0, #0x0
+	movs r2, #0xFA
+	lsls r2, r2, #0x01
+	adds r0, r1, r2
+	ldr r4, [r0, #0x00]
+	cmp r4, #0x00
+	beq _08085380
+	ldr r3, _0808538C @ =0x0000027F
+	mov r8, r3
+	adds r6, r0, #0x0
+	movs r0, #0xA2
+	lsls r0, r0, #0x02
+	adds r7, r1, r0
+	subs r2, #0x04
+	adds r5, r1, r2
+_0808534C:
+	movs r3, #0x04
+	ldsh r0, [r4, r3]
+	ldr r2, _08085390 @ =0x0600F300
+	cmp r0, r8
+	bgt _08085358
+	ldr r2, _08085394 @ =0x0600F000
+_08085358:
+	movs r0, #0x04
+	ldsh r1, [r4, r0]
+	lsls r1, r1, #0x01
+	ldr r0, [r7, #0x00]
+	adds r0, r0, r1
+	adds r1, r2, r1
+	movs r3, #0x06
+	ldsh r2, [r4, r3]
+	ldr r3, _08085398 @ =0x001FFFFF
+	ands r2, r3
+	bl CpuSet
+	ldr r0, [r4, #0x00]
+	str r0, [r6, #0x00]
+	ldr r0, [r5, #0x00]
+	str r0, [r4, #0x00]
+	str r4, [r5, #0x00]
+	ldr r4, [r6, #0x00]
+	cmp r4, #0x00
+	bne _0808534C
+_08085380:
+	pop {r3}
+	mov r8, r3
+	pop {r4, r5, r6, r7}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_0808538C: .4byte 0x0000027F
+_08085390: .4byte 0x0600F300
+_08085394: .4byte 0x0600F000
+_08085398: .4byte 0x001FFFFF
--- src/sub_808520C.c
+++ src/sub_808520C.c
@@ -18,3 +18,9 @@ asm_unified(".include \"asm/nonmatching/sub_8085260.s\"");
 #else
 #error "TODO: write sub_8085260 to match asm/nonmatching/sub_8085260.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8085328.s\"");
+#else
+#error "TODO: write sub_8085328 to match asm/nonmatching/sub_8085328.s, then delete this #error"
+#endif
