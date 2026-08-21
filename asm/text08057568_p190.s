49afb9d47157e466e784ae2d52899b0080061069
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:21:36 -0600

    Extract sub_8068EF4
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p190.s
+++ asm/text08057568_p190.s
@@ -3,20 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_8068EF4
-sub_8068EF4:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	movs r0, #0x8D
-	lsls r0, r0, #0x01
-	bl stop_sfx_80195A8
-	ldr r0, _08068F0C @ =0x0808750D
-	str r0, [r4, #0x4C]
-	pop {r4}
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_08068F0C: .4byte sub_808750C
 	thumb_func_start sub_8068F10
 sub_8068F10:
 	push {r4, lr}
--- src/sub_8068818.c
+++ src/sub_8068818.c
@@ -130,3 +130,9 @@ asm_unified(".include \"asm/nonmatching/sub_8068E98.s\"");
 #else
 #error "TODO: write sub_8068E98 to match asm/nonmatching/sub_8068E98.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8068EF4.s\"");
+#else
+#error "TODO: write sub_8068EF4 to match asm/nonmatching/sub_8068EF4.s, then delete this #error"
+#endif
