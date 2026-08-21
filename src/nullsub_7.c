4cc879df1dedadc930b7368b06ee1b43c2af807b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:24:36 -0600

    Extract sub_819AFA8
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/nullsub_7.c
+++ src/nullsub_7.c
@@ -1,6 +1,8 @@
 #include "global.h"
 #include "common.h"
 
+asm_unified(".include \"asm/macros.inc\"");
+
 // First function extracted from the middle of a raw blob rather than off
 // the front of one (see split_func.py's mid-file path). It's deliberately
 // the most trivial possible target -- retail is a bare `bx lr` plus two
@@ -9,3 +11,9 @@
 // on something with no other moving parts to blame.
 void nullsub_7(void) {
 }
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_819AFA8.s\"");
+#else
+#error "TODO: write sub_819AFA8 to match asm/nonmatching/sub_819AFA8.s, then delete this #error"
+#endif
