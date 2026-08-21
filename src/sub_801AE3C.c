26b4dad2510f0474777b510db5d07c4d785e35b6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:14:18 -0600

    Draft m2c seeds for sub_801AE3C.c's #error placeholders
    Unblocks the whole translation unit (sub_801AEA0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_801AE3C.c
+++ src/sub_801AE3C.c
@@ -26,5 +26,26 @@ void sub_801AE3C(void) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_801AEA0.s\"");
 #else
-#error "TODO: write sub_801AEA0 to match asm/nonmatching/sub_801AEA0.s, then delete this #error"
+s32 sub_801AEA0(s32 arg0, s32 arg2) {
+    s32 var_r2_0;
+    u32 temp_r0_10;
+
+    var_r2_0 = arg2;
+    temp_r0_10 = (u8) *(u8 *)0x03000D18 >> 6;
+    switch (temp_r0_10) {                           /* irregular */
+    case 0:
+        var_r2_0 = arg0;
+        break;
+    case 1:
+        var_r2_0 = arg0 * 4;
+        break;
+    case 2:
+        var_r2_0 = arg0 * 0x10;
+        break;
+    case 3:
+        var_r2_0 = arg0 << 6;
+        break;
+    }
+    return var_r2_0;
+}
 #endif
