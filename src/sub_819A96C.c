b7e92fe8a9988f5da4d8d25f92cbd2e6f684f004
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:46:59 -0600

    Draft m2c seeds for sub_819A96C.c's #error placeholders
    Unblocks the whole translation unit (sub_819A96C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_819A96C.c
+++ src/sub_819A96C.c
@@ -10,5 +10,17 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_819A96C.s\"");
 #else
-#error "TODO: write sub_819A96C to match asm/nonmatching/sub_819A96C.s, then delete this #error"
+u16 sub_819A96C(s32 arg0) {
+    s32 temp_r3_13;
+    u16 temp_r1_19;
+    u32 temp_r0_7;
+
+    temp_r0_7 = arg0 << 0x10;
+    temp_r3_13 = (temp_r0_7 >> 0x18) - 0x24;
+    if (temp_r3_13 >= 0) {
+        temp_r1_19 = *(s32 *)(0x0821C9A8 + (temp_r3_13 * 2));
+        return (u16) (0x800 - (temp_r1_19 + ((u32) ((*(s32 *)(0x0821C9A8 + ((temp_r3_13 + 1) * 2)) - temp_r1_19) * ((u32) (0xFF0000 & temp_r0_7) >> 0x10)) >> 8)));
+    }
+    return 0U;
+}
 #endif
