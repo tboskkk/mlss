ea9a36fa39afcb00f501753eeb98aa76b9797a06
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:42:00 -0600

    Draft m2c seeds for sub_8142CF0.c's #error placeholders
    Unblocks the whole translation unit (sub_8142CF0, sub_8142D10, sub_8142D6C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8142CF0.c
+++ src/sub_8142CF0.c
@@ -10,17 +10,58 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8142CF0.s\"");
 #else
-#error "TODO: write sub_8142CF0 to match asm/nonmatching/sub_8142CF0.s, then delete this #error"
+s32 sub_813B5B4(s32);                               /* extern */
+extern s32 sub_813E974;
+
+void sub_8142CF0(s32 arg0, s32 arg1, s32 **arg2) {
+    if ((sub_813B5B4(arg1) << 0x18) == 0) {
+        *arg2 = &sub_813E974;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8142D10.s\"");
 #else
-#error "TODO: write sub_8142D10 to match asm/nonmatching/sub_8142D10.s, then delete this #error"
+s32 sub_80FBC14(s32 *, s32, s32, s32, s32); /* extern */
+s32 sub_80FBD44(s32, u8 *);                         /* extern */
+s32 sub_8139D44(s32 *, s32, s32, s32);      /* extern */
+
+s32 sub_8142D10(s32 arg0, s32 arg1, s32 *arg2) {
+    s32 sp4;
+    u8 sp14;
+    s32 temp_r4_20;
+
+    if ((sub_80FBD44(arg0, &sp14) << 0x18) != 0) {
+        temp_r4_20 = (*(s32 *)((s8 *)(((sp14 * 4) + arg0)) + (0x28)));
+        if (arg2 != NULL) {
+            *arg2 = temp_r4_20;
+        }
+        sub_8139D44(&sp4, arg1, 0x11, 0x1C);
+        if ((sub_80FBC14(&sp4, arg1, temp_r4_20, M2C_ERROR(/* unknown instruction: ldsh $r3, ($mem_loc_fictive_) */), M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */)) << 0x18) == 0) {
+            goto block_4;
+        }
+        return 1;
+    }
+block_4:
+    return 0;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8142D6C.s\"");
 #else
-#error "TODO: write sub_8142D6C to match asm/nonmatching/sub_8142D6C.s, then delete this #error"
+s32 sub_80FBDE0(s32 *, s32, s32, s32);  /* extern */
+s32 sub_8139BB0(s32 *, s32, s32, s32);      /* extern */
+s32 sub_813B650(s32);                               /* extern */
+extern s32 sub_813E05C;
+
+void sub_8142D6C(s32 arg0, s32 arg1, s32 **arg2) {
+    s32 sp0;
+
+    sub_8139BB0(&sp0, arg1, 0x22, 0);
+    if (((sub_80FBDE0(&sp0, arg1, M2C_ERROR(/* unknown instruction: ldsh $r2, ($mem_loc_fictive_) */), M2C_ERROR(/* unknown instruction: ldsh $r3, ($mem_loc_fictive_) */)) << 0x18) != 0) || ((sub_813B650(arg1) << 0x18) == 0)) {
+        *arg2 = &sub_813E05C;
+    }
+}
 #endif
