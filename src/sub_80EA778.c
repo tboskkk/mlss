74ebfc52d66ced7e254ba96d39a3d369d0ba4fc4
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:31:50 -0600

    Draft m2c seeds for sub_80EA778.c's #error placeholders
    Unblocks the whole translation unit (sub_80EA778, sub_80EA88C, sub_80EA8A4, sub_80EA8DC). 2 left empty (seed didn't compile): script_test_condition, sub_80EA854. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80EA778.c
+++ src/sub_80EA778.c
@@ -10,35 +10,88 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA778.s\"");
 #else
-#error "TODO: write sub_80EA778 to match asm/nonmatching/sub_80EA778.s, then delete this #error"
+void sub_80EA778(s32 arg0, void *arg1) {
+    s32 temp_r0_10;
+
+    temp_r0_10 = (*(s32 *)((s8 *)(arg1) + (0x9C))) - 1;
+    (*(s32 *)((s8 *)(arg1) + (0x9C))) = temp_r0_10;
+    if (temp_r0_10 == 0) {
+        (*(u16 *)((s8 *)(arg1) + (0xA0))) = (u16) (0xFFEF & (*(u16 *)((s8 *)(arg1) + (0xA0))));
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/script_test_condition.s\"");
 #else
-#error "TODO: write script_test_condition to match asm/nonmatching/script_test_condition.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA854.s\"");
 #else
-#error "TODO: write sub_80EA854 to match asm/nonmatching/sub_80EA854.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA88C.s\"");
 #else
-#error "TODO: write sub_80EA88C to match asm/nonmatching/sub_80EA88C.s, then delete this #error"
+s32 sub_80EA88C(s32 arg0, s32 arg1, void *arg2) {
+    void *temp_r2_7;
+
+    temp_r2_7 = arg2 + 4;
+    if ((*(s32 *)((s8 *)(arg2) + (0))) == 0) {
+        sub_801ABE8((*(u16 *)((s8 *)(arg2) + (4))), (*(u16 *)((s8 *)(temp_r2_7) + (4))), (*(s32 *)((s8 *)(temp_r2_7) + (8))));
+    }
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA8A4.s\"");
 #else
-#error "TODO: write sub_80EA8A4 to match asm/nonmatching/sub_80EA8A4.s, then delete this #error"
+s32 reset_game_80189C4();                       /* extern */
+s32 sub_801AD3C();                              /* extern */
+s32 sub_801AD80();                              /* extern */
+s32 sub_801ADC0();                              /* extern */
+
+s32 sub_80EA8A4(s32 arg0, s32 arg1, s32 *arg2) {
+    s32 temp_r2_7;
+
+    temp_r2_7 = *arg2;
+    switch (temp_r2_7) {                            /* irregular */
+    case 0:
+        reset_game_80189C4();
+        break;
+    case 1:
+        sub_801ADC0();
+        break;
+    case 2:
+        sub_801AD80();
+        break;
+    case 3:
+        sub_801AD3C();
+        break;
+    }
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA8DC.s\"");
 #else
-#error "TODO: write sub_80EA8DC to match asm/nonmatching/sub_80EA8DC.s, then delete this #error"
+s32 script_test_condition(s32, u8, s32, s32);   /* extern */
+
+s32 sub_80EA8DC(s32 arg0, void *arg1, void *arg2) {
+    if ((script_test_condition(arg0, (*(u8 *)((s8 *)(arg2) + (0))), (*(s32 *)((s8 *)(arg2) + (4))), (*(s32 *)((s8 *)(arg1) + (0x98)))) << 0x18) != 0) {
+        (*(s32 *)((s8 *)(arg1) + (0))) = (s32) (*(s32 *)((s8 *)(arg2) + (8)));
+    }
+    return 1;
+}
 #endif
