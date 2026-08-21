1d5197ab76ea4362809e6462eddc3d8f13993551
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:45:33 -0600

    Draft m2c seeds for sub_81639D4.c's #error placeholders
    Unblocks the whole translation unit (sub_81639E0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_81639D4.c
+++ src/sub_81639D4.c
@@ -15,5 +15,12 @@ void sub_81639D4(void *arg0, s32 arg1) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81639E0.s\"");
 #else
-#error "TODO: write sub_81639E0 to match asm/nonmatching/sub_81639E0.s, then delete this #error"
+s32 sub_8161E38();                              /* extern */
+
+void sub_81639E0(void *arg0, s32 arg1) {
+    (*(s32 *)((s8 *)(arg0) + (0x44))) = arg1;
+    (*(s16 *)((s8 *)(arg0) + (0x48))) = 0;
+    (*(s32 *)((s8 *)(arg0) + (0x40))) = 0;
+    sub_8161E38();
+}
 #endif
