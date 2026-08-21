67c73c32f14c8815f158a24c81ab9317ae8c9e84
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:35:38 -0600

    Draft m2c seeds for sub_80FA6BC.c's #error placeholders
    Unblocks the whole translation unit (sub_80FA6BC, sub_80FA6DC, sub_80FA734). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80FA6BC.c
+++ src/sub_80FA6BC.c
@@ -10,17 +10,49 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80FA6BC.s\"");
 #else
-#error "TODO: write sub_80FA6BC to match asm/nonmatching/sub_80FA6BC.s, then delete this #error"
+s32 sub_81219C4(s32);                               /* extern */
+
+s32 sub_80FA6BC(void *arg0, void *arg1) {
+    if ((sub_81219C4((*(s32 *)((s8 *)(arg0) + (0x14)))) << 0x18) == 0) {
+        return 1;
+    }
+    (*(s32 *)((s8 *)(arg1) + (0))) = (s32) (*(s32 *)((s8 *)(arg1) + (0x14)));
+    return 0;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80FA6DC.s\"");
 #else
-#error "TODO: write sub_80FA6DC to match asm/nonmatching/sub_80FA6DC.s, then delete this #error"
+s32 sub_81219EC(s32);                           /* extern */
+s32 sub_8121A20(s32);                           /* extern */
+
+s32 sub_80FA6DC(void *arg0, s32 *arg2) {
+    if (*arg2 & 1) {
+        sub_81219EC((*(s32 *)((s8 *)(arg0) + (0x14))));
+    } else {
+        sub_8121A20((*(s32 *)((s8 *)(arg0) + (0x14))));
+    }
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80FA734.s\"");
 #else
-#error "TODO: write sub_80FA734 to match asm/nonmatching/sub_80FA734.s, then delete this #error"
+s32 sub_80EA6D4(void *, void *, s32 *, s32 *, s32 *); /* extern */
+s32 sub_8120C54(s32, u8, s32, s32, s32, s32, s32); /* extern */
+
+s32 sub_80FA734(void *arg0, void *arg1, void *arg2) {
+    s32 spC;
+    s32 sp10;
+    s32 sp14;
+    u8 temp_r4_20;
+
+    temp_r4_20 = (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x24)))) + (0x9A0))) - 1;
+    sub_80EA6D4(arg0 + 0x1C, arg2, &spC, &sp10, &sp14);
+    sub_8120C54((*(s32 *)((s8 *)(arg0) + (0x14))), temp_r4_20, (*(s32 *)((s8 *)(arg2) + (0xC))) & 3, 1, spC, sp10, M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    (*(u16 *)((s8 *)(arg1) + (0xA0))) = (u16) (2 | (*(u16 *)((s8 *)(arg1) + (0xA0))));
+    return 0;
+}
 #endif
