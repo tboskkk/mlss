7153b93c3a0b9255a1edddfe84a8ba80dcb28aa3
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:24:24 -0600

    Extract sub_806D9F8
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_806D9F8.s
@@ -0,0 +1,116 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_806D9F8
+sub_806D9F8:
+	push {r4, lr}
+	adds r4, r0, #0x0
+	ldr r0, [r4, #0x08]
+	ldrb r1, [r0, #0x12]
+	movs r0, #0x08
+	ands r0, r1
+	cmp r0, #0x00
+	beq _0806DADC
+	ldr r0, [r4, #0x2C]
+	ldr r0, [r0, #0x08]
+	bl sub_807BC90
+	ldr r1, [r4, #0x2C]
+	movs r0, #0x00
+	str r0, [r1, #0x4C]
+	adds r0, r4, #0x0
+	movs r2, #0xFF
+	bl sub_810DD7C
+	adds r0, r4, #0x0
+	adds r0, #0x9C
+	ldr r0, [r0, #0x00]
+	cmp r0, #0x00
+	bne _0806DA64
+	ldr r2, _0806DA5C @ =0x00004112
+	adds r0, r4, #0x0
+	movs r1, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r0, [r4, #0x30]
+	ldr r2, _0806DA60 @ =0x00004113
+	movs r1, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	adds r0, r4, #0x0
+	movs r1, #0x09
+	movs r2, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r0, [r4, #0x30]
+	movs r1, #0x09
+	movs r2, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	b _0806DA94
+	.byte 0x00, 0x00
+_0806DA5C: .4byte 0x00004112
+_0806DA60: .4byte 0x00004113
+_0806DA64:
+	ldr r2, _0806DAE4 @ =0x00004114
+	adds r0, r4, #0x0
+	movs r1, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r0, [r4, #0x30]
+	ldr r2, _0806DAE8 @ =0x00004115
+	movs r1, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	adds r0, r4, #0x0
+	movs r1, #0x09
+	movs r2, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r0, [r4, #0x30]
+	movs r1, #0x09
+	movs r2, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+_0806DA94:
+	adds r1, r4, #0x0
+	adds r1, #0x84
+	ldr r2, [r4, #0x28]
+	adds r0, r2, #0x0
+	adds r0, #0xD8
+	ldr r0, [r0, #0x00]
+	str r0, [r1, #0x00]
+	adds r1, #0x04
+	adds r0, r2, #0x0
+	adds r0, #0xDC
+	ldr r0, [r0, #0x00]
+	str r0, [r1, #0x00]
+	adds r1, #0x04
+	adds r0, r2, #0x0
+	adds r0, #0xE0
+	ldr r0, [r0, #0x00]
+	str r0, [r1, #0x00]
+	subs r1, #0x10
+	movs r2, #0x00
+	movs r0, #0xC0
+	lsls r0, r0, #0x02
+	strh r0, [r1, #0x00]
+	adds r3, r4, #0x0
+	adds r3, #0x79
+	ldrb r0, [r3, #0x00]
+	movs r1, #0x20
+	orrs r0, r1
+	strb r0, [r3, #0x00]
+	adds r0, r4, #0x0
+	adds r0, #0x7A
+	strh r2, [r0, #0x00]
+	adds r0, r4, #0x0
+	bl sub_8085B38
+	ldr r0, _0806DAEC @ =0x0806E779
+	str r0, [r4, #0x4C]
+_0806DADC:
+	pop {r4}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_0806DAE4: .4byte 0x00004114
+_0806DAE8: .4byte 0x00004115
+_0806DAEC: .4byte sub_806E778
