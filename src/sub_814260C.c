b8299a7751d9398e6fb46fcba96ac6a03990de06
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:41:57 -0600

    Draft m2c seeds for sub_814260C.c's #error placeholders
    Unblocks the whole translation unit (sub_814260C, sub_8142658). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_814260C.c
+++ src/sub_814260C.c
@@ -10,11 +10,35 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_814260C.s\"");
 #else
-#error "TODO: write sub_814260C to match asm/nonmatching/sub_814260C.s, then delete this #error"
+s32 sub_813B650(void *);                            /* extern */
+extern s32 sub_813B1E8;
+extern s32 sub_813B360;
+extern s32 sub_8141D54;
+
+void sub_814260C(s32 arg0, void *arg1, void *arg2) {
+    s32 temp_r6_12;
+
+    temp_r6_12 = (*(s32 *)((s8 *)(arg1) + (0x1B0)));
+    if ((sub_813B650(arg1) << 0x18) == 0) {
+        (*(s16 *)((s8 *)(arg1) + (0x1D2))) = (s16) (*(u8 *)((s8 *)(((*(u16 *)((s8 *)(arg1) + (0x1D0))) + temp_r6_12)) + (0x58)));
+        (*(s32 **)((s8 *)(arg2) + (0))) = &sub_813B1E8;
+        (*(s32 **)((s8 *)(arg2) + (0xC))) = &sub_813B360;
+        (*(s32 **)((s8 *)(arg2) + (4))) = &sub_8141D54;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8142658.s\"");
 #else
-#error "TODO: write sub_8142658 to match asm/nonmatching/sub_8142658.s, then delete this #error"
+s32 sub_8046A90(void *, s32, s32, s32, s32); /* extern */
+s32 sub_8046D18(void *);                        /* extern */
+extern s32 sub_8142EFC;
+
+void sub_8142658(s32 arg0, void *arg1, s32 **arg2) {
+    sub_8046A90(arg1, 2, 0, M2C_ERROR(/* unknown instruction: ldsh $r3, ($mem_loc_fictive_) */), (s32) (*(u8 *)((s8 *)((*(void **)((s8 *)(arg1) + (0x1B0)))) + (0x26))));
+    sub_8046D18(arg1);
+    (*(u8 *)((s8 *)(arg1) + (0x1D6))) = (u8) (-2 & (*(u8 *)((s8 *)(arg1) + (0x1D6))));
+    *arg2 = &sub_8142EFC;
+}
 #endif
