6d92b7ae5da5d940380224a7948bc8f137c4c7f6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:59:38 -0600

    Extract sub_80DED30
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p155.s
+++ asm/text08057568_p155.s
@@ -3,21 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80DED30
-sub_80DED30:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	bl sub_8082B00
-	cmp r0, #0x00
-	bne _080DED40
-	ldr r0, _080DED48 @ =0x080DDAE5
-	str r0, [r4, #0x4C]
-_080DED40:
-	pop {r4}
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_080DED48: .4byte sub_80DDAE4
 	thumb_func_start sub_80DED4C
 sub_80DED4C:
 	push {r4, lr}
--- src/sub_80DEAC0.c
+++ src/sub_80DEAC0.c
@@ -71,3 +71,9 @@ asm_unified(".include \"asm/nonmatching/sub_80DECC8.s\"");
 #else
 #error "TODO: write sub_80DECC8 to match asm/nonmatching/sub_80DECC8.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80DED30.s\"");
+#else
+#error "TODO: write sub_80DED30 to match asm/nonmatching/sub_80DED30.s, then delete this #error"
+#endif
