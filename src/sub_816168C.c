c016725bf06078f7c3bd20fa7df9dc6a16fc5e20
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:45:02 -0600

    Draft m2c seeds for sub_816168C.c's #error placeholders
    Unblocks the whole translation unit (sub_8161780). 1 left empty (seed didn't compile): sub_8161698. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_816168C.c
+++ src/sub_816168C.c
@@ -15,11 +15,46 @@ u32* sub_816168C(u32* param_1) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8161698.s\"");
 #else
-#error "TODO: write sub_8161698 to match asm/nonmatching/sub_8161698.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8161780.s\"");
 #else
-#error "TODO: write sub_8161780 to match asm/nonmatching/sub_8161780.s, then delete this #error"
+s32 sub_8161780(void *arg0) {
+    s32 temp_r0_26;
+    s32 temp_r0_50;
+    s32 temp_r0_8;
+    s32 temp_r1_20;
+    s32 temp_r2_17;
+
+    temp_r0_8 = (*(s32 *)((s8 *)(arg0) + (0x30)));
+    switch (temp_r0_8) {                            /* irregular */
+    case 0:
+        temp_r2_17 = (*(s32 *)((s8 *)(arg0) + (0x10))) + 0x14;
+        (*(s32 *)((s8 *)(arg0) + (0x10))) = temp_r2_17;
+        temp_r1_20 = (*(s32 *)((s8 *)(arg0) + (8))) + temp_r2_17;
+        (*(s32 *)((s8 *)(arg0) + (8))) = temp_r1_20;
+        if ((temp_r1_20 <= 0x4FFF) || (temp_r0_26 = (0 - temp_r2_17) * 2, (*(s32 *)((s8 *)(arg0) + (0x10))) = (s32) (temp_r0_26 / 3), ((temp_r0_26 / 768) != 0))) {
+            (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0)))) + (0))) = (s16) ((s32) ((*(s32 *)((s8 *)(arg0) + (4))) + (*(s32 *)((s8 *)(arg0) + (0x14)))) >> 8);
+            (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0)))) + (2))) = (s16) ((s32) ((*(s32 *)((s8 *)(arg0) + (8))) + (*(s32 *)((s8 *)(arg0) + (0x18)))) >> 8);
+        } else {
+            (*(s32 *)((s8 *)(arg0) + (0x2C))) = 0x50;
+            (*(s32 *)((s8 *)(arg0) + (0x30))) = 1;
+        }
+block_9:
+    default:
+        return 0;
+    case 1:
+        temp_r0_50 = (*(s32 *)((s8 *)(arg0) + (0x2C))) - 1;
+        (*(s32 *)((s8 *)(arg0) + (0x2C))) = temp_r0_50;
+        if (temp_r0_50 <= 0) {
+            return 1;
+        }
+        goto block_9;
+    }
+}
 #endif
