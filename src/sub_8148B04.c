806323ce830c6c34b9fbdafa861040b6db59cbc6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:42:15 -0600

    Draft m2c seeds for sub_8148B04.c's #error placeholders
    Unblocks the whole translation unit (sub_8148B04, sub_8148B60, sub_8148BBC, sub_8148C20). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8148B04.c
+++ src/sub_8148B04.c
@@ -10,23 +10,71 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8148B04.s\"");
 #else
-#error "TODO: write sub_8148B04 to match asm/nonmatching/sub_8148B04.s, then delete this #error"
+u8 sub_813B5B4(void *);                             /* extern */
+extern s32 sub_813B1E8;
+extern s32 sub_814765C;
+
+void sub_8148B04(s32 arg0, void *arg1, void *arg2) {
+    s32 temp_r5_12;
+    u8 temp_r3_16;
+
+    temp_r5_12 = (*(s32 *)((s8 *)(arg1) + (0x1B0)));
+    temp_r3_16 = sub_813B5B4(arg1);
+    if (temp_r3_16 == 0) {
+        (*(s32 *)((s8 *)(arg1) + (0x2EC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x14))) + (*(s32 *)((s8 *)(arg1) + (0x18))));
+        (*(s32 *)((s8 *)(arg1) + (0x2F4))) = (s32) temp_r3_16;
+        (*(s16 *)((s8 *)(arg1) + (0x1D2))) = (s16) (*(u8 *)((s8 *)(((*(u16 *)((s8 *)(arg1) + (0x1D0))) + temp_r5_12)) + (0x52)));
+        (*(s32 **)((s8 *)(arg2) + (0))) = &sub_813B1E8;
+        (*(s32 **)((s8 *)(arg2) + (0xC))) = &sub_814765C;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8148B60.s\"");
 #else
-#error "TODO: write sub_8148B60 to match asm/nonmatching/sub_8148B60.s, then delete this #error"
+u8 sub_813ADF0(void *);                             /* extern */
+extern s32 sub_8147AD4;
+
+void sub_8148B60(s32 arg0, void *arg1, s32 **arg2) {
+    u8 temp_r3_27;
+
+    (*(s32 *)((s8 *)(arg1) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0xC))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    (*(s32 *)((s8 *)(arg1) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    temp_r3_27 = sub_813ADF0(arg1);
+    if (temp_r3_27 == 0) {
+        (*(s32 *)((s8 *)(arg1) + (0x2EC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x14))) + (*(s32 *)((s8 *)(arg1) + (0x18))));
+        (*(s32 *)((s8 *)(arg1) + (0x2F4))) = (s32) temp_r3_27;
+        *arg2 = &sub_8147AD4;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8148BBC.s\"");
 #else
-#error "TODO: write sub_8148BBC to match asm/nonmatching/sub_8148BBC.s, then delete this #error"
+s32 sub_8139AA0(void *);                        /* extern */
+extern s32 sub_8147590;
+extern s32 sub_814765C;
+
+void sub_8148BBC(s32 arg0, void *arg1) {
+    (*(u8 *)((s8 *)(arg1) + (0x33E))) = (u8) ((*(u8 *)((s8 *)(arg1) + (0x33E))) | 0x10);
+    sub_8139AA0(arg1);
+    (*(s32 *)((s8 *)(arg1) + (0x2EC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x14))) + (*(s32 *)((s8 *)(arg1) + (0x18))));
+    (*(s32 *)((s8 *)(arg1) + (0x2F4))) = 0;
+    (*(s16 *)((s8 *)(arg1) + (0x2F8))) = 0x10;
+    (*(s16 *)((s8 *)(arg1) + (0x1D4))) = 0;
+    (*(s32 **)((s8 *)(arg1) + (0x198))) = &sub_8147590;
+    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_814765C;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8148C20.s\"");
 #else
-#error "TODO: write sub_8148C20 to match asm/nonmatching/sub_8148C20.s, then delete this #error"
+void sub_8148C20(void *arg0) {
+    (*(u8 *)((s8 *)(arg0) + (0x20D))) = (u8) (-9 & (*(u8 *)((s8 *)(arg0) + (0x20D))));
+    (*(u8 *)((s8 *)(arg0) + (0x20C))) = (u8) (-0x41 & (*(u8 *)((s8 *)(arg0) + (0x20C))) & 0x7F & ~0x20);
+    (*(u8 *)((s8 *)(arg0) + (0x20F))) = (u8) (-5 & (*(u8 *)((s8 *)(arg0) + (0x20F))));
+}
 #endif
