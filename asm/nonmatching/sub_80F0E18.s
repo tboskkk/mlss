158f41fafd3da74191cedf800b764f8ad976b8d6
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 21:01:17 -0600

    Extract sub_80F0E18
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/nonmatching/sub_80F0E18.s
+++ asm/nonmatching/sub_80F0E18.s
@@ -1,34 +1,30 @@
-	.include "asm/macros.inc"
-
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80F0E80
-sub_80F0E80:
+	thumb_func_start sub_80F0E18
+sub_80F0E18:
 	push {r4, r5, r6, r7, lr}
 	add sp, #-0x01C
-	adds r4, r0, #0x0
+	adds r3, r0, #0x0
 	adds r7, r2, #0x0
 	ldr r0, [r7, #0x0C]
-	lsls r0, r0, #0x0C
+	lsls r0, r0, #0x08
 	str r0, [r7, #0x0C]
 	ldr r0, [r7, #0x10]
-	lsls r0, r0, #0x0C
+	lsls r0, r0, #0x08
 	str r0, [r7, #0x10]
 	ldr r0, [r7, #0x14]
-	lsls r0, r0, #0x0B
-	movs r1, #0x05
-	bl __divsi3
+	lsls r0, r0, #0x08
 	str r0, [r7, #0x14]
 	ldr r1, [r7, #0x1C]
 	movs r0, #0x01
 	negs r0, r0
 	cmp r1, r0
-	bne _080F0EAE
+	bne _080F0E40
 	movs r0, #0x00
 	str r0, [r7, #0x1C]
-_080F0EAE:
-	ldr r0, [r4, #0x14]
+_080F0E40:
+	ldr r0, [r3, #0x14]
 	ldrh r1, [r7, #0x00]
 	ldr r5, [r7, #0x04]
 	movs r6, #0x01
@@ -58,3 +54,4 @@ _080F0EAE:
 	pop {r4, r5, r6, r7}
 	pop {r1}
 	bx r1
+	.byte 0x00, 0x00
