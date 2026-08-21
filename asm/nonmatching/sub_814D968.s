b8aa5532b90808ea88a56663b44716ef2eaa31b2
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:01:15 -0600

    Extract sub_814D968
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_814D968.s
@@ -0,0 +1,91 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_814D968
+sub_814D968:
+	push {r4, r5, r6, lr}
+	adds r6, r0, #0x0
+	movs r0, #0x80
+	lsls r0, r0, #0x01
+	adds r5, r6, r0
+	movs r1, #0xD8
+	lsls r1, r1, #0x01
+	adds r0, r6, r1
+	ldr r4, [r0, #0x00]
+	adds r0, r4, #0x0
+	adds r1, r5, #0x0
+	movs r2, #0x04
+	bl CpuSet
+	adds r4, #0x08
+	movs r0, #0x84
+	lsls r0, r0, #0x01
+	adds r1, r6, r0
+	adds r0, r4, #0x0
+	movs r2, #0x04
+	bl CpuSet
+	adds r4, #0x08
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x10]
+	movs r0, #0x01
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x12]
+	adds r4, #0x02
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x14]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x16]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x18]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x1A]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x1C]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x1E]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x20]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x22]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x24]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x26]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x28]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x2A]
+	adds r4, #0x01
+	movs r0, #0x00
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x2C]
+	movs r0, #0x01
+	ldsb r0, [r4, r0]
+	strh r0, [r5, #0x2E]
+	pop {r4, r5, r6}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
--- src/sub_814D838.c
+++ src/sub_814D838.c
@@ -12,3 +12,9 @@ asm_unified(".include \"asm/nonmatching/sub_814D838.s\"");
 #else
 #error "TODO: write sub_814D838 to match asm/nonmatching/sub_814D838.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_814D968.s\"");
+#else
+#error "TODO: write sub_814D968 to match asm/nonmatching/sub_814D968.s, then delete this #error"
+#endif
