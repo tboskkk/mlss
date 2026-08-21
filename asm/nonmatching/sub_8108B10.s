eba1d90e18bfb268b5e29ca003b7b7506f90ab0d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:34:06 -0600

    Extract sub_8108B10
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8108B10.s
@@ -0,0 +1,37 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8108B10
+sub_8108B10:
+	push {r4, r5, lr}
+	adds r5, r0, #0x0
+	ldr r4, [r5, #0x08]
+	adds r3, r4, #0x0
+	adds r3, #0x08
+	ldr r0, [r3, #0x08]
+	ldrb r1, [r0, #0x12]
+	movs r0, #0x08
+	ands r0, r1
+	cmp r0, #0x00
+	beq _08108B48
+	movs r2, #0x01
+	negs r2, r2
+	adds r0, r3, #0x0
+	adds r1, r2, #0x0
+	movs r3, #0x00
+	bl sub_8082E1C
+	movs r0, #0x91
+	lsls r0, r0, #0x01
+	adds r2, r4, r0
+	ldrb r1, [r2, #0x00]
+	movs r0, #0x21
+	negs r0, r0
+	ands r0, r1
+	strb r0, [r2, #0x00]
+	movs r0, #0x00
+	str r0, [r5, #0x04]
+_08108B48:
+	pop {r4, r5}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
