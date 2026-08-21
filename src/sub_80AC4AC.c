e14a4b25233ab3b722efa053f8b23c2478fdac32
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:26:42 -0600

    Draft m2c seeds for sub_80AC4AC.c's #error placeholders
    Unblocks the whole translation unit (sub_80AC4AC). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80AC4AC.c
+++ src/sub_80AC4AC.c
@@ -10,5 +10,29 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80AC4AC.s\"");
 #else
-#error "TODO: write sub_80AC4AC to match asm/nonmatching/sub_80AC4AC.s, then delete this #error"
+s32 sub_80884AC(void *);                        /* extern */
+extern s32 sub_80A99A8;
+
+void sub_80AC4AC(void) {
+    s32 temp_r1_27;
+    void *temp_r0_8;
+    void *temp_r4_12;
+    void *temp_r5_9;
+    void *temp_r6_14;
+    void *temp_r7_11;
+
+    temp_r0_8 = *(void **)0x03000FD8;
+    temp_r5_9 = (*(void **)((s8 *)(temp_r0_8) + (0x70)));
+    temp_r7_11 = temp_r5_9 + 8;
+    temp_r4_12 = (*(void **)((s8 *)(temp_r0_8) + (0x74)));
+    temp_r6_14 = temp_r4_12 + 8;
+    sub_80884AC(temp_r6_14);
+    if (0x20 & (*(u8 *)((s8 *)(temp_r4_12) + (0x81)))) {
+        temp_r1_27 = 6 & (*(u8 *)((s8 *)(temp_r5_9) + (0x7E)));
+        if ((temp_r1_27 == 2) || (temp_r1_27 == 4)) {
+            (*(s32 **)((s8 *)(temp_r7_11) + (0x4C))) = &sub_80A99A8;
+        }
+        (*(s32 *)((s8 *)(temp_r6_14) + (0x4C))) = 0;
+    }
+}
 #endif
