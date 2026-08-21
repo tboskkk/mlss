e6b5580b68d09c42c64c56a5d082eea33932a3f2
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:44:38 -0600

    Draft m2c seeds for sub_815EC3C.c's #error placeholders
    Unblocks the whole translation unit (sub_815EC4C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_815EC3C.c
+++ src/sub_815EC3C.c
@@ -21,5 +21,30 @@ u32 sub_815EC3C(u32 param_1) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_815EC4C.s\"");
 #else
-#error "TODO: write sub_815EC4C to match asm/nonmatching/sub_815EC4C.s, then delete this #error"
+void sub_815EC4C(void *arg0, u16 arg1) {
+    s16 temp_r0_24;
+    s16 temp_r1_12;
+    s8 var_r0_29;
+    u16 var_r0_16;
+
+    temp_r1_12 = (s16) arg1;
+    if ((s32) (M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) + temp_r1_12) < 0) {
+        var_r0_16 = 0;
+    } else {
+        var_r0_16 = temp_r1_12 + (*(u16 *)((s8 *)(arg0) + (0x18)));
+    }
+    (*(u16 *)((s8 *)(arg0) + (0x18))) = var_r0_16;
+    temp_r0_24 = (s16) arg1;
+    if ((s32) temp_r0_24 < 0) {
+        (*(s8 *)((s8 *)(arg0) + (0x1B))) = 0x64;
+        var_r0_29 = 1;
+        goto block_7;
+    }
+    if ((s32) temp_r0_24 > 0) {
+        (*(s8 *)((s8 *)(arg0) + (0x1B))) = 0x64;
+        var_r0_29 = 2;
+block_7:
+        (*(s8 *)((s8 *)(arg0) + (0x1A))) = var_r0_29;
+    }
+}
 #endif
