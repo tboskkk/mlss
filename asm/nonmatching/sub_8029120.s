a15d90e6b79622f1d94b1d4ccca844082a8c3840
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:54:43 -0600

    Extract sub_8029120
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8029120.s
@@ -0,0 +1,46 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8029120
+sub_8029120:
+	push {r4, r5, r6, r7, lr}
+	add sp, #-0x004
+	lsls r2, r2, #0x10
+	lsrs r4, r2, #0x10
+	lsls r1, r1, #0x10
+	orrs r4, r1
+	ldr r3, _0802916C @ =0x02000080
+	ldr r5, [r0, #0x24]
+	movs r7, #0x01
+	cmp r4, #0x00
+	beq _08029162
+	adds r6, r0, #0x0
+	adds r6, #0xF0
+_0802913A:
+	movs r0, #0x01
+	ands r0, r4
+	cmp r0, #0x00
+	beq _08029156
+	adds r0, r3, #0x0
+	adds r1, r5, #0x0
+	movs r2, #0x08
+	str r3, [sp, #0x000]
+	bl CpuFastSet
+	ldr r0, [r6, #0x00]
+	orrs r0, r7
+	str r0, [r6, #0x00]
+	ldr r3, [sp, #0x000]
+_08029156:
+	adds r3, #0x20
+	adds r5, #0x20
+	lsls r7, r7, #0x01
+	lsrs r4, r4, #0x01
+	cmp r4, #0x00
+	bne _0802913A
+_08029162:
+	add sp, #0x004
+	pop {r4, r5, r6, r7}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_0802916C: .4byte 0x02000080
--- src/sub_80290CC.c
+++ src/sub_80290CC.c
@@ -21,3 +21,9 @@ asm_unified(".include \"asm/nonmatching/sub_80290E0.s\"");
 #else
 #error "TODO: write sub_80290E0 to match asm/nonmatching/sub_80290E0.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8029120.s\"");
+#else
+#error "TODO: write sub_8029120 to match asm/nonmatching/sub_8029120.s, then delete this #error"
+#endif
