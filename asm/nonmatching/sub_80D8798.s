8c298d52151863f9d00173e904ed9741980ce994
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:57:37 -0600

    Extract sub_80D8798
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80D8798.s
@@ -0,0 +1,103 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80D8798
+sub_80D8798:
+	push {r4, r5, r6, r7, lr}
+	add sp, #-0x004
+	adds r7, r0, #0x0
+	ldr r0, _080D881C @ =0x03000FD8
+	ldr r0, [r0, #0x00]
+	ldr r5, [r0, #0x74]
+	adds r4, r5, #0x0
+	adds r4, #0x08
+	adds r0, r4, #0x0
+	bl sub_8087CE4
+	adds r0, r5, #0x0
+	adds r0, #0x81
+	ldrb r1, [r0, #0x00]
+	movs r0, #0x20
+	ands r0, r1
+	cmp r0, #0x00
+	beq _080D884A
+	movs r6, #0x01
+	negs r6, r6
+	movs r0, #0x2E
+	adds r1, r6, #0x0
+	bl play_sfx_80195B4
+	ldr r1, [r4, #0x10]
+	cmp r1, #0x00
+	bge _080D87D0
+	adds r1, #0xFF
+_080D87D0:
+	asrs r1, r1, #0x08
+	ldr r2, [r4, #0x14]
+	cmp r2, #0x00
+	bge _080D87DA
+	adds r2, #0xFF
+_080D87DA:
+	asrs r2, r2, #0x08
+	ldr r3, [r4, #0x18]
+	cmp r3, #0x00
+	bge _080D87E4
+	adds r3, #0xFF
+_080D87E4:
+	asrs r3, r3, #0x08
+	str r4, [sp, #0x000]
+	movs r0, #0x1E
+	bl sub_80DF024
+	adds r0, r5, #0x0
+	adds r0, #0x7E
+	ldrb r0, [r0, #0x00]
+	movs r1, #0x06
+	ands r1, r0
+	cmp r1, #0x02
+	beq _080D8800
+	cmp r1, #0x04
+	bne _080D883C
+_080D8800:
+	ldr r0, [r4, #0x28]
+	adds r0, #0xEC
+	movs r1, #0x00
+	ldsh r0, [r0, r1]
+	cmp r0, r6
+	bne _080D8820
+	movs r2, #0x80
+	lsls r2, r2, #0x06
+	adds r0, r4, #0x0
+	movs r1, #0x08
+	movs r3, #0x00
+	bl sub_8082E1C
+	b _080D882C
+_080D881C: .4byte 0x03000FD8
+_080D8820:
+	ldr r2, _080D8854 @ =0x0000204D
+	adds r0, r4, #0x0
+	movs r1, #0x08
+	movs r3, #0x00
+	bl sub_8082E1C
+_080D882C:
+	ldr r2, [r4, #0x08]
+	ldrb r1, [r2, #0x12]
+	movs r0, #0x07
+	negs r0, r0
+	ands r0, r1
+	movs r1, #0x02
+	orrs r0, r1
+	strb r0, [r2, #0x12]
+_080D883C:
+	ldr r2, [r4, #0x08]
+	ldrb r0, [r2, #0x11]
+	movs r1, #0x40
+	orrs r0, r1
+	strb r0, [r2, #0x11]
+	ldr r0, _080D8858 @ =0x080D885D
+	str r0, [r7, #0x4C]
+_080D884A:
+	add sp, #0x004
+	pop {r4, r5, r6, r7}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_080D8854: .4byte 0x0000204D
+_080D8858: .4byte sub_80D885C
--- src/sub_80D7410.c
+++ src/sub_80D7410.c
@@ -134,3 +134,9 @@ asm_unified(".include \"asm/nonmatching/sub_80D86B0.s\"");
 #else
 #error "TODO: write sub_80D86B0 to match asm/nonmatching/sub_80D86B0.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80D8798.s\"");
+#else
+#error "TODO: write sub_80D8798 to match asm/nonmatching/sub_80D8798.s, then delete this #error"
+#endif
