b97ef85ce1ff9f23d8cc26c4349ad662637b1cc0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:15:35 -0600

    Extract sub_80601D4
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p386.s
+++ asm/text08057568_p386.s
@@ -3,43 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80601D4
-sub_80601D4:
-	push {r4, lr}
-	add sp, #-0x004
-	adds r4, r0, #0x0
-	movs r2, #0xFF
-	bl sub_810DD7C
-	ldr r1, [r4, #0x38]
-	cmp r1, #0x00
-	bge _080601E8
-	adds r1, #0xFF
-_080601E8:
-	asrs r1, r1, #0x08
-	ldr r2, [r4, #0x3C]
-	cmp r2, #0x00
-	bge _080601F2
-	adds r2, #0xFF
-_080601F2:
-	asrs r2, r2, #0x08
-	ldr r3, [r4, #0x40]
-	cmp r3, #0x00
-	bge _080601FC
-	adds r3, #0xFF
-_080601FC:
-	asrs r3, r3, #0x08
-	str r4, [sp, #0x000]
-	ldr r0, _08060218 @ =0x00001118
-	bl sub_80DF024
-	adds r0, r4, #0x0
-	bl sub_807C298
-	movs r0, #0x00
-	add sp, #0x004
-	pop {r4}
-	pop {r1}
-	bx r1
-	.byte 0x00, 0x00
-_08060218: .4byte 0x00001118
 	thumb_func_start sub_806021C
 sub_806021C:
 	push {r4, r5, lr}
