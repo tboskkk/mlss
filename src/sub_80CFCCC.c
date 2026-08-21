4e31c669208a3b90a158fb5d5b0450e7a28f1aa5
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:42:06 -0600

    Extract sub_80D08F8
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_80CFCCC.c
+++ src/sub_80CFCCC.c
@@ -42,3 +42,9 @@ asm_unified(".include \"asm/nonmatching/sub_80D086C.s\"");
 #else
 #error "TODO: write sub_80D086C to match asm/nonmatching/sub_80D086C.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80D08F8.s\"");
+#else
+#error "TODO: write sub_80D08F8 to match asm/nonmatching/sub_80D08F8.s, then delete this #error"
+#endif
