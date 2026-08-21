cffb3a7051090bda214af008b178a556be3e833d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:31:51 -0600

    Draft m2c seeds for sub_80EA91C.c's #error placeholders
    Unblocks the whole translation unit (sub_80EA940, sub_80EA968, sub_80EA9A8). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80EA91C.c
+++ src/sub_80EA91C.c
@@ -21,17 +21,45 @@ s32 sub_80EA928(s32 arg0, void *arg1, s32 *arg2) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA940.s\"");
 #else
-#error "TODO: write sub_80EA940 to match asm/nonmatching/sub_80EA940.s, then delete this #error"
+s32 sub_8199F30();                                  /* extern */
+s32 sub_81DD77C(s32, s32);                          /* extern */
+
+s32 sub_80EA940(s32 arg0, s32 *arg1, s32 *arg2) {
+    s32 temp_r0_16;
+    s32 temp_r4_9;
+
+    temp_r4_9 = *arg2;
+    temp_r0_16 = (*(s32 *)((s8 *)(((sub_81DD77C(temp_r4_9, sub_8199F30()) * 4) + arg2)) + (4)));
+    if (temp_r0_16 != 0) {
+        *arg1 = temp_r0_16;
+    }
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA968.s\"");
 #else
-#error "TODO: write sub_80EA968 to match asm/nonmatching/sub_80EA968.s, then delete this #error"
+s32 sub_80E9C4C(s32, s32, s32, s32, s32, s32); /* extern */
+s32 sub_8199F30();                                  /* extern */
+s32 sub_81DD77C(s32, s32);                          /* extern */
+
+s32 sub_80EA968(s32 arg0, s32 arg1, void *arg2, s32 arg3) {
+    s32 temp_r4_13;
+
+    temp_r4_13 = (*(s32 *)((s8 *)(arg2) + (4)));
+    sub_80E9C4C(arg3, arg1 + 0x18, 0, 0, M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */), sub_81DD77C(temp_r4_13, sub_8199F30()));
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EA9A8.s\"");
 #else
-#error "TODO: write sub_80EA9A8 to match asm/nonmatching/sub_80EA9A8.s, then delete this #error"
+s32 sub_801B0AC(u16);                           /* extern */
+
+s32 sub_80EA9A8(s32 arg0, s32 arg1, u16 *arg2) {
+    sub_801B0AC(*arg2);
+    return 1;
+}
 #endif
