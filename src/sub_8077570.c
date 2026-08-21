2477459462b1ac047a6413492e40f67ef2764d4e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:29:34 -0600

    Extract sub_8078D2C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_8077570.c
+++ src/sub_8077570.c
@@ -141,3 +141,9 @@ asm_unified(".include \"asm/nonmatching/sub_8078B94.s\"");
 #else
 #error "TODO: write sub_8078B94 to match asm/nonmatching/sub_8078B94.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8078D2C.s\"");
+#else
+#error "TODO: write sub_8078D2C to match asm/nonmatching/sub_8078D2C.s, then delete this #error"
+#endif
