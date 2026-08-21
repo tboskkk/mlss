e3adfa7eae17f07f5a4df5b64bba39cafa046fb0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 00:09:54 -0600

    Match sub_81DD690
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_81DD670.c
+++ src/sub_81DD670.c
@@ -14,12 +14,8 @@ void sub_81DD670(void* p0, s32 p1)
         free_heap_8018DA8(p0);
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81DD690.s\"");
-#else
 s32 sub_8161330();                              /* extern */
 
 void sub_81DD690(void) {
     sub_8161330();
 }
-#endif
