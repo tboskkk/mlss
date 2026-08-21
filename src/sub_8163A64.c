b4ac0ba9cb8872f82c720d75b880657e87a9a618
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:45:34 -0600

    Draft m2c seeds for sub_8163A64.c's #error placeholders
    Unblocks the whole translation unit (sub_8163A80, sub_8163ADC). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8163A64.c
+++ src/sub_8163A64.c
@@ -35,11 +35,36 @@ u32 sub_8163A6C(u32 param_1) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8163A80.s\"");
 #else
-#error "TODO: write sub_8163A80 to match asm/nonmatching/sub_8163A80.s, then delete this #error"
+void sub_8163A80(void *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5) {
+    void *temp_r0_20;
+    void *temp_r2_30;
+
+    temp_r0_20 = sub_8020DD0(arg1, arg2, arg3, -1, -1, -1, -1);
+    (*(void **)((s8 *)(arg0) + (0x14))) = temp_r0_20;
+    sub_801E150(temp_r0_20, arg4, -1, 0, 0);
+    sprite_show_8020CBC((*(void **)((s8 *)(arg0) + (0x14))));
+    temp_r2_30 = (*(void **)((s8 *)(arg0) + (0x14)));
+    (*(u8 *)((s8 *)(temp_r2_30) + (0x1F))) = (u8) ((-4 & (*(u8 *)((s8 *)(temp_r2_30) + (0x1F)))) | (arg5 & 3));
+    (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x14)))) + (0xE))) = 0xFFFF;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8163ADC.s\"");
 #else
-#error "TODO: write sub_8163ADC to match asm/nonmatching/sub_8163ADC.s, then delete this #error"
+void sub_8163ADC(void *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5) {
+    s32 temp_r5_42;
+    void *temp_r0_22;
+    void *temp_r3_35;
+
+    temp_r0_22 = sub_8020DD0(arg1, arg2, arg3, -1, -1, -1, -1);
+    (*(void **)((s8 *)(arg0) + (0x10))) = temp_r0_22;
+    sub_801E150(temp_r0_22, arg4, -1, 0, 0);
+    sprite_show_8020CBC((*(void **)((s8 *)(arg0) + (0x10))));
+    temp_r3_35 = (*(void **)((s8 *)(arg0) + (0x10)));
+    temp_r5_42 = arg5 & 3;
+    (*(u8 *)((s8 *)(temp_r3_35) + (0x1F))) = (u8) ((((((((0x3F & (*(u8 *)((s8 *)(temp_r3_35) + (0x1F)))) | (arg5 << 6)) & ~0x30) | (temp_r5_42 * 0x10)) & ~0xC) | (temp_r5_42 * 4)) & ~3) | temp_r5_42);
+    (*(s16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x10)))) + (0xE))) = 0;
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = 0;
+}
 #endif
