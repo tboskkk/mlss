0e7e8e8c9b4628575bb7921263f10c85f3ec1984
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:22:36 -0600

    Extract sub_806A83C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p150.s
+++ asm/text08057568_p150.s
@@ -3,41 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_806A83C
-sub_806A83C:
-	push {r4, r5, lr}
-	adds r5, r0, #0x0
-	adds r0, #0x80
-	ldr r0, [r0, #0x00]
-	cmp r0, #0x00
-	bne _0806A878
-	movs r0, #0x81
-	bl stop_sfx_80195A8
-	adds r0, r5, #0x0
-	movs r1, #0x04
-	movs r2, #0x00
-	movs r3, #0x00
-	bl sub_8082E1C
-	bl sub_8199F30
-	adds r1, r5, #0x0
-	adds r1, #0x9C
-	ldr r4, [r1, #0x00]
-	adds r1, #0x04
-	ldr r1, [r1, #0x00]
-	bl __umodsi3
-	adds r4, r4, r0
-	adds r0, r5, #0x0
-	adds r0, #0xAC
-	strh r4, [r0, #0x00]
-	ldr r0, _0806A880 @ =0x0806A47D
-	str r0, [r5, #0x4C]
-_0806A878:
-	pop {r4, r5}
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_0806A880: .4byte sub_806A47C
-	.byte 0x01, 0x20, 0x70, 0x47, 0x01, 0x49, 0xC1, 0x64, 0x01, 0x20, 0x70, 0x47, 0xE1, 0xA8, 0x06, 0x08
 	thumb_func_start sub_806A894
 sub_806A894:
 	push {r4, lr}
--- src/sub_806A77C.c
+++ src/sub_806A77C.c
@@ -47,3 +47,9 @@ asm_unified(".include \"asm/nonmatching/sub_806A814.s\"");
 #else
 #error "TODO: write sub_806A814 to match asm/nonmatching/sub_806A814.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_806A83C.s\"");
+#else
+#error "TODO: write sub_806A83C to match asm/nonmatching/sub_806A83C.s, then delete this #error"
+#endif
