cc73900bf59cf329a7f3e78c42dc9a77935fd375
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:57:25 -0600

    Extract sub_8083E1C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8083E1C.s
@@ -0,0 +1,41 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8083E1C
+sub_8083E1C:
+	push {lr}
+	ldr r2, _08083E54 @ =0x03001034
+	ldr r0, _08083E58 @ =0x08198154
+	ldr r1, _08083E5C @ =0x081980D8
+	subs r0, r0, r1
+	ldr r3, [r2, #0x00]
+	adds r3, r3, r0
+	ldr r0, _08083E60 @ =0x02000420
+	ldr r1, _08083E64 @ =0x03000FDC
+	ldr r1, [r1, #0x00]
+	ldr r2, _08083E68 @ =0x00003C90
+	adds r1, r1, r2
+	movs r2, #0x20
+	bl _call_via_r3
+	ldr r0, _08083E6C @ =0x03000D74
+	ldr r1, [r0, #0x00]
+	ldr r0, _08083E70 @ =0x0000FFFF
+	strh r0, [r1, #0x26]
+	ldr r0, _08083E74 @ =0x03000FD8
+	ldr r2, [r0, #0x00]
+	ldrb r0, [r2, #0x0C]
+	movs r1, #0x01
+	orrs r0, r1
+	strb r0, [r2, #0x0C]
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_08083E54: .4byte 0x03001034
+_08083E58: .4byte 0x08198154
+_08083E5C: .4byte 0x081980D8
+_08083E60: .4byte 0x02000420
+_08083E64: .4byte 0x03000FDC
+_08083E68: .4byte 0x00003C90
+_08083E6C: .4byte 0x03000D74
+_08083E70: .4byte 0x0000FFFF
+_08083E74: .4byte 0x03000FD8
--- src/sub_8083860.c
+++ src/sub_8083860.c
@@ -30,3 +30,9 @@ asm_unified(".include \"asm/nonmatching/sub_8083C74.s\"");
 #else
 #error "TODO: write sub_8083C74 to match asm/nonmatching/sub_8083C74.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8083E1C.s\"");
+#else
+#error "TODO: write sub_8083E1C to match asm/nonmatching/sub_8083E1C.s, then delete this #error"
+#endif
