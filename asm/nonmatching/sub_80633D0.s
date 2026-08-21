27f695c5cfd169959478871a57119d6cbde88fc9
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:17:48 -0600

    Extract sub_80633D0
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80633D0.s
@@ -0,0 +1,43 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80633D0
+sub_80633D0:
+	push {r4, lr}
+	add sp, #-0x004
+	adds r4, r0, #0x0
+	ldr r1, [r4, #0x38]
+	cmp r1, #0x00
+	bge _080633DE
+	adds r1, #0xFF
+_080633DE:
+	asrs r1, r1, #0x08
+	ldr r2, [r4, #0x3C]
+	cmp r2, #0x00
+	bge _080633E8
+	adds r2, #0xFF
+_080633E8:
+	asrs r2, r2, #0x08
+	ldr r3, [r4, #0x40]
+	cmp r3, #0x00
+	bge _080633F2
+	adds r3, #0xFF
+_080633F2:
+	asrs r3, r3, #0x08
+	str r4, [sp, #0x000]
+	ldr r0, _08063418 @ =0x000012CD
+	bl sub_80DF024
+	movs r0, #0x00
+	str r0, [r4, #0x4C]
+	ldr r0, _0806341C @ =0x03000E18
+	ldr r0, [r0, #0x00]
+	bl free_heap_8018DA8
+	adds r0, r4, #0x0
+	bl sub_807C298
+	add sp, #0x004
+	pop {r4}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_08063418: .4byte 0x000012CD
+_0806341C: .4byte 0x03000E18
--- src/sub_8063118.c
+++ src/sub_8063118.c
@@ -121,3 +121,9 @@ asm_unified(".include \"asm/nonmatching/sub_8063384.s\"");
 #else
 #error "TODO: write sub_8063384 to match asm/nonmatching/sub_8063384.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80633D0.s\"");
+#else
+#error "TODO: write sub_80633D0 to match asm/nonmatching/sub_80633D0.s, then delete this #error"
+#endif
