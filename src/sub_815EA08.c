43dd2eabde9ac0790c8f72e71aff009b0eb365f9
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:44:38 -0600

    Draft m2c seeds for sub_815EA08.c's #error placeholders
    Unblocks the whole translation unit (sub_815EACC, sub_815EB70). 2 left empty (seed didn't compile): sub_815EA18, sub_815EA64. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_815EA08.c
+++ src/sub_815EA08.c
@@ -19,23 +19,83 @@ void sub_815EA08(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_815EA18.s\"");
 #else
-#error "TODO: write sub_815EA18 to match asm/nonmatching/sub_815EA18.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_815EA64.s\"");
 #else
-#error "TODO: write sub_815EA64 to match asm/nonmatching/sub_815EA64.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_815EACC.s\"");
 #else
-#error "TODO: write sub_815EACC to match asm/nonmatching/sub_815EACC.s, then delete this #error"
+s32 sub_8021308();                              /* extern */
+
+void sub_815EACC(void *arg0, s32 arg1) {
+    if ((*(s32 *)((s8 *)(arg0) + (4))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)((arg0 + 4)) + (4))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0xC))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x10))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x14))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x18))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x1C))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x20))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x24))) != 0) {
+        sub_8021308();
+    }
+    if (1 & arg1) {
+        free_heap_8018DA8(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_815EB70.s\"");
 #else
-#error "TODO: write sub_815EB70 to match asm/nonmatching/sub_815EB70.s, then delete this #error"
+s32 sub_8021308();                              /* extern */
+
+void sub_815EB70(void *arg0, s32 arg1) {
+    if ((*(s32 *)((s8 *)(arg0) + (4))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)((arg0 + 4)) + (4))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0xC))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x10))) != 0) {
+        sub_8021308();
+    }
+    if ((*(s32 *)((s8 *)(arg0) + (0x14))) != 0) {
+        sub_8021308();
+    }
+    if (1 & arg1) {
+        free_heap_8018DA8(arg0);
+    }
+}
 #endif
