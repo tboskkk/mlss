e11b46854afcae210744c6d3ee204871a8740d48
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:29:21 -0600

    Draft m2c seeds for sub_80DA97C.c's #error placeholders
    Unblocks the whole translation unit (sub_80DA97C, sub_80DA9E0, sub_80DA9FC, sub_80DAA18). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80DA97C.c
+++ src/sub_80DA97C.c
@@ -10,23 +10,72 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA97C.s\"");
 #else
-#error "TODO: write sub_80DA97C to match asm/nonmatching/sub_80DA97C.s, then delete this #error"
+s32 sub_807C298(void *);                        /* extern */
+
+void sub_80DA97C(void *arg0) {
+    s32 temp_r0_46;
+    s32 temp_r1_38;
+
+    (*(u16 *)((s8 *)(arg0) + (0xAE))) = (u16) (*(u16 *)((s8 *)(arg0) + (0xAE)));
+    (*(u16 *)((s8 *)(arg0) + (0xB2))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0xB2))) + (*(s32 *)((s8 *)(arg0) + (0x94))));
+    (*(s32 *)((s8 *)(arg0) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x38))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x3C))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    temp_r1_38 = (*(s32 *)((s8 *)(arg0) + (0x40))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */);
+    (*(s32 *)((s8 *)(arg0) + (0x18))) = temp_r1_38;
+    if ((s32) M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) <= 0) {
+        temp_r0_46 = (*(s32 *)((s8 *)(arg0) + (0x98)));
+        if (temp_r1_38 <= temp_r0_46) {
+            (*(s32 *)((s8 *)(arg0) + (0x18))) = temp_r0_46;
+            sub_807C298(arg0);
+        }
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA9E0.s\"");
 #else
-#error "TODO: write sub_80DA9E0 to match asm/nonmatching/sub_80DA9E0.s, then delete this #error"
+s32 sub_8087540(void *);                        /* extern */
+
+void sub_80DA9E0(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8087540(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA9FC.s\"");
 #else
-#error "TODO: write sub_80DA9FC to match asm/nonmatching/sub_80DA9FC.s, then delete this #error"
+s32 sub_8087540(void *);                        /* extern */
+
+void sub_80DA9FC(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8087540(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DAA18.s\"");
 #else
-#error "TODO: write sub_80DAA18 to match asm/nonmatching/sub_80DAA18.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+s16 sub_8086D80();                                  /* extern */
+extern s32 sub_80DAAEC;
+
+s16 sub_80DAA18(void *arg0) {
+    s16 temp_r0_8;
+    u32 temp_r0_16;
+
+    temp_r0_8 = sub_8086D80();
+    if (temp_r0_8 == 0) {
+        temp_r0_16 = 0 - M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */);
+        (*(s16 *)((s8 *)(arg0) + (0xAE))) = (s16) ((s32) (temp_r0_16 + (temp_r0_16 >> 0x1F)) >> 1);
+        (*(s16 *)((s8 *)(arg0) + (0xB0))) = temp_r0_8;
+        (*(s16 *)((s8 *)((arg0 + 0xB0)) + (2))) = temp_r0_8;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DAAEC;
+        sub_8082E1C(arg0, -1, 0, 0);
+    }
+    return temp_r0_8;
+}
 #endif
