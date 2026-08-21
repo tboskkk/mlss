d6626ec929dfa528960d59581eac1cfea21f25c8
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:00:53 -0600

    Extract sub_8028EF4
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_8028EE8.c
+++ src/sub_8028EE8.c
@@ -10,3 +10,9 @@ asm_unified(".include \"asm/macros.inc\"");
 void sub_8028EE8(void *arg0) {
     (*(u8 *)((s8 *)(arg0) + (0xF8))) = (u8) ((*(u8 *)((s8 *)(arg0) + (0xF8))) | 0x40);
 }
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8028EF4.s\"");
+#else
+#error "TODO: write sub_8028EF4 to match asm/nonmatching/sub_8028EF4.s, then delete this #error"
+#endif
