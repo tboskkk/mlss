dc445355ffe1ee0811fe9c2937597d457bb8262e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 21:01:36 -0600

    Extract sub_80F1934
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p156.s
+++ asm/text08057568_p156.s
@@ -3,54 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80F1934
-sub_80F1934:
-	push {r4, r5, lr}
-	adds r4, r1, #0x0
-	adds r5, r3, #0x0
-	adds r0, r4, #0x0
-	movs r1, #0x00
-	bl sub_8047364
-	adds r0, r4, #0x0
-	movs r1, #0x00
-	bl sub_8047258
-	movs r1, #0xCE
-	lsls r1, r1, #0x02
-	adds r0, r4, r1
-	ldr r2, [r0, #0x00]
-	adds r2, #0x90
-	movs r1, #0x00
-	ldsh r0, [r2, r1]
-	adds r0, r4, r0
-	ldrb r1, [r5, #0x00]
-	ldr r2, [r2, #0x04]
-	bl _call_via_r2
-	ldr r1, [r5, #0x04]
-	ldr r2, [r5, #0x08]
-	adds r0, r4, #0x0
-	bl sub_8047154
-	movs r0, #0xA5
-	lsls r0, r0, #0x02
-	adds r4, r4, r0
-	ldr r3, [r4, #0x00]
-	ldrb r2, [r3, #0x11]
-	lsls r1, r2, #0x19
-	lsrs r1, r1, #0x1F
-	ldr r0, [r5, #0x0C]
-	eors r1, r0
-	movs r0, #0x01
-	ands r1, r0
-	lsls r1, r1, #0x06
-	movs r0, #0x41
-	negs r0, r0
-	ands r0, r2
-	orrs r0, r1
-	strb r0, [r3, #0x11]
-	movs r0, #0x01
-	pop {r4, r5}
-	pop {r1}
-	bx r1
-	.byte 0x00, 0x00
 	thumb_func_start sub_80F1998
 sub_80F1998:
 	push {r4, r5, r6, r7, lr}
--- src/sub_80F110C.c
+++ src/sub_80F110C.c
@@ -149,3 +149,9 @@ asm_unified(".include \"asm/nonmatching/sub_80F18C4.s\"");
 #else
 #error "TODO: write sub_80F18C4 to match asm/nonmatching/sub_80F18C4.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80F1934.s\"");
+#else
+#error "TODO: write sub_80F1934 to match asm/nonmatching/sub_80F1934.s, then delete this #error"
+#endif
