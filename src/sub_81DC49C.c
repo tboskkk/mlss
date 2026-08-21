248e3b09b671f7dac6a3c1184a80e17e09ca3f47
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:47:44 -0600

    Draft m2c seeds for sub_81DC49C.c's #error placeholders
    Unblocks the whole translation unit (_lshrdi3, _muldi3). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_81DC49C.c
+++ src/sub_81DC49C.c
@@ -50,11 +50,47 @@ asm_unified(".include \"asm/nonmatching/sub_81DC50C.s\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/_lshrdi3.s\"");
 #else
-#error "TODO: write _lshrdi3 to match asm/nonmatching/_lshrdi3.s, then delete this #error"
+u32 _lshrdi3(u32 arg0, u32 arg1, s32 arg2) {
+    s32 temp_r0_12;
+    u32 var_r0_0;
+    u32 var_r3_18;
+
+    var_r0_0 = arg0;
+    if (arg2 != 0) {
+        temp_r0_12 = 0x20 - arg2;
+        if (temp_r0_12 <= 0) {
+            var_r3_18 = arg1 >> (0 - temp_r0_12);
+        } else {
+            var_r3_18 = (var_r0_0 >> arg2) | (arg1 << temp_r0_12);
+        }
+        var_r0_0 = var_r3_18;
+    }
+    return var_r0_0;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/_muldi3.s\"");
 #else
-#error "TODO: write _muldi3 to match asm/nonmatching/_muldi3.s, then delete this #error"
+s32 _muldi3(u32 arg0, s32 arg1, u32 arg2, s32 arg3) {
+    u32 sp0;
+    s32 sp4;
+    u32 sp8;
+    s32 spC;
+    u16 temp_r5_24;
+    u32 temp_r2_28;
+    u32 temp_r4_32;
+
+    sp0 = arg0;
+    sp4 = arg1;
+    sp8 = arg2;
+    spC = arg3;
+    temp_r5_24 = (u16) sp0 * (u16) sp8;
+    temp_r2_28 = (sp0 >> 0x10) * (u16) sp8;
+    temp_r4_32 = ((u16) sp0 * (sp8 >> 0x10)) + (temp_r5_24 >> 0x10) + temp_r2_28;
+    if (temp_r4_32 < temp_r2_28) {
+
+    }
+    return ((u16) temp_r4_32 << 0x10) | temp_r5_24;
+}
 #endif
