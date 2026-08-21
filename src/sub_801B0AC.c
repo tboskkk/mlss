bab1d3b70960c28c07cd95e5e159cda3ec7ba227
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:14:20 -0600

    Draft m2c seeds for sub_801B0AC.c's #error placeholders
    Unblocks the whole translation unit (sub_801B390, sub_801B5A0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_801B0AC.c
+++ src/sub_801B0AC.c
@@ -100,11 +100,117 @@ s32 sub_801B1C8(void *arg0, s32 *arg1, s32 *arg2, s32 arg3) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_801B390.s\"");
 #else
-#error "TODO: write sub_801B390 to match asm/nonmatching/sub_801B390.s, then delete this #error"
+void sub_801B390(void *arg0, s32 arg1, s32 *arg2, s32 *arg3, s32 *arg4) {
+    s32 *sp0;
+    s32 *sp4;
+    s32 sp8;
+    s32 spC;
+    s32 sp10;
+    s32 sp14;
+    s32 temp_r0_101;
+    s32 temp_r0_92;
+    s32 temp_r1_21;
+    s32 temp_r2_111;
+    s32 temp_r2_27;
+    s32 temp_r2_80;
+    s32 temp_r3_65;
+    s32 temp_r3_72;
+    s32 temp_r4_76;
+    s32 temp_r5_70;
+    s32 temp_r6_68;
+    s32 temp_r7_63;
+    s32 var_r0_126;
+    s32 var_r0_147;
+    s32 var_r0_167;
+    s32 var_r0_177;
+    s32 var_r0_188;
+    s32 var_r0_199;
+    s32 var_r0_22;
+    s32 var_r0_30;
+    s32 var_r0_39;
+    s32 var_r0_48;
+    s32 var_r0_58;
+
+    sp0 = arg2;
+    sp4 = arg3;
+    temp_r1_21 = M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */);
+    var_r0_22 = temp_r1_21;
+    if (temp_r1_21 < 0) {
+        var_r0_22 += 0x3F;
+    }
+    temp_r2_27 = var_r0_22 >> 6;
+    var_r0_30 = (*(s32 *)((s8 *)(arg0) + (0xC))) * temp_r2_27;
+    if (var_r0_30 < 0) {
+        var_r0_30 += 0xFF;
+    }
+    sp8 = var_r0_30 >> 8;
+    var_r0_39 = (*(s32 *)((s8 *)(arg0) + (0x10))) * temp_r2_27;
+    if (var_r0_39 < 0) {
+        var_r0_39 += 0xFF;
+    }
+    spC = var_r0_39 >> 8;
+    var_r0_48 = (*(s32 *)((s8 *)(arg0) + (0x14))) * temp_r2_27;
+    if (var_r0_48 < 0) {
+        var_r0_48 += 0xFF;
+    }
+    sp10 = var_r0_48 >> 8;
+    var_r0_58 = M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */);
+    if (var_r0_58 < 0) {
+        var_r0_58 += 0x3F;
+    }
+    temp_r7_63 = var_r0_58 >> 6;
+    temp_r3_65 = 0 - sp8;
+    temp_r6_68 = 0 - spC;
+    temp_r5_70 = 0 - sp10;
+    temp_r3_72 = (*(s32 *)((s8 *)(arg0) + (0)));
+    temp_r4_76 = (*(s32 *)((s8 *)(arg0) + (8)));
+    temp_r2_80 = (*(s32 *)((s8 *)(arg0) + (4)));
+    sp14 = ((temp_r7_63 * temp_r3_72) + (temp_r6_68 * temp_r4_76)) - (temp_r5_70 * temp_r2_80);
+    temp_r0_92 = ((temp_r7_63 * temp_r2_80) - (temp_r3_65 * temp_r4_76)) + (temp_r5_70 * temp_r3_72);
+    temp_r0_101 = ((temp_r7_63 * temp_r4_76) + (temp_r3_65 * temp_r2_80)) - (temp_r6_68 * temp_r3_72);
+    temp_r2_111 = ((sp8 * temp_r3_72) - (temp_r6_68 * temp_r2_80)) - (temp_r5_70 * temp_r4_76);
+    var_r0_126 = ((temp_r2_111 * sp8) + (sp14 * temp_r7_63) + (temp_r0_92 * sp10)) - (temp_r0_101 * spC);
+    if (var_r0_126 < 0) {
+        var_r0_126 += 0xFF;
+    }
+    var_r0_147 = ((temp_r2_111 * spC) - (sp14 * sp10)) + (temp_r0_92 * temp_r7_63) + (temp_r0_101 * sp8);
+    if (var_r0_147 < 0) {
+        var_r0_147 += 0xFF;
+    }
+    var_r0_167 = (((temp_r2_111 * sp10) + (sp14 * spC)) - (temp_r0_92 * sp8)) + (temp_r0_101 * temp_r7_63);
+    if (var_r0_167 < 0) {
+        var_r0_167 += 0xFF;
+    }
+    var_r0_177 = (var_r0_126 >> 8) * (*(u16 *)((s8 *)(arg0) + (0x18)));
+    if (var_r0_177 < 0) {
+        var_r0_177 += 0xFFFF;
+    }
+    *sp0 = var_r0_177 >> 0x10;
+    var_r0_188 = (*(u16 *)((s8 *)(arg0) + (0x1A))) * (var_r0_147 >> 8);
+    if (var_r0_188 < 0) {
+        var_r0_188 += 0xFFFF;
+    }
+    *sp4 = var_r0_188 >> 0x10;
+    var_r0_199 = (*(u16 *)((s8 *)(arg0) + (0x1C))) * (var_r0_167 >> 8);
+    if (var_r0_199 < 0) {
+        var_r0_199 += 0xFFFF;
+    }
+    *arg4 = var_r0_199 >> 0x10;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_801B5A0.s\"");
 #else
-#error "TODO: write sub_801B5A0 to match asm/nonmatching/sub_801B5A0.s, then delete this #error"
+void sub_801B5A0(void *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8, s32 arg9) {
+    (*(s32 *)((s8 *)(arg0) + (0))) = arg1;
+    (*(s32 *)((s8 *)(arg0) + (4))) = arg2;
+    (*(s32 *)((s8 *)(arg0) + (8))) = arg3;
+    (*(s32 *)((s8 *)(arg0) + (0xC))) = arg4;
+    (*(s32 *)((s8 *)(arg0) + (0x10))) = arg5;
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = arg6;
+    (*(s16 *)((s8 *)(arg0) + (0x18))) = (s16) arg7;
+    (*(s16 *)((s8 *)(arg0) + (0x1A))) = (s16) arg8;
+    (*(s16 *)((s8 *)(arg0) + (0x1C))) = (s16) arg9;
+}
 #endif
