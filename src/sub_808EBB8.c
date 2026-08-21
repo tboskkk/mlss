5d8c96068b347de52098e3e2d379275fbd848fea
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:23:10 -0600

    Draft m2c seeds for sub_808EBB8.c's #error placeholders
    Unblocks the whole translation unit (sub_808EBB8, sub_808EC08, sub_808EC58). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_808EBB8.c
+++ src/sub_808EBB8.c
@@ -10,17 +10,65 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_808EBB8.s\"");
 #else
-#error "TODO: write sub_808EBB8 to match asm/nonmatching/sub_808EBB8.s, then delete this #error"
+s32 sub_808DD2C(void *);                        /* extern */
+
+void sub_808EBB8(void *arg0) {
+    u16 temp_r1_34;
+    void *temp_r1_8;
+
+    temp_r1_8 = (*(void **)((s8 *)(arg0) + (0x28)));
+    if (temp_r1_8 != NULL) {
+        (*(u8 *)((s8 *)(temp_r1_8) + (0x111))) = (u8) (-5 & (*(u8 *)((s8 *)(temp_r1_8) + (0x111))));
+    }
+    if (0x38 & (*(u8 *)((s8 *)(arg0) + (0x76)))) {
+        sub_808DD2C(arg0);
+        return;
+    }
+    temp_r1_34 = (*(u16 *)((s8 *)(arg0) + (0xAC)));
+    (*(u16 *)((s8 *)(arg0) + (0xAC))) = (u16) (temp_r1_34 + 1);
+    if ((s32) (s16) temp_r1_34 > 0x17) {
+        sub_808DD2C(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_808EC08.s\"");
 #else
-#error "TODO: write sub_808EC08 to match asm/nonmatching/sub_808EC08.s, then delete this #error"
+s32 sub_808DD2C(void *);                        /* extern */
+
+void sub_808EC08(void *arg0) {
+    u16 temp_r1_34;
+    void *temp_r1_8;
+
+    temp_r1_8 = (*(void **)((s8 *)(arg0) + (0x28)));
+    if (temp_r1_8 != NULL) {
+        (*(u8 *)((s8 *)(temp_r1_8) + (0x111))) = (u8) (-5 & (*(u8 *)((s8 *)(temp_r1_8) + (0x111))));
+    }
+    if (0x38 & (*(u8 *)((s8 *)(arg0) + (0x76)))) {
+        sub_808DD2C(arg0);
+        return;
+    }
+    temp_r1_34 = (*(u16 *)((s8 *)(arg0) + (0xAC)));
+    (*(u16 *)((s8 *)(arg0) + (0xAC))) = (u16) (temp_r1_34 + 1);
+    if ((s32) (s16) temp_r1_34 > 0x13) {
+        sub_808DD2C(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_808EC58.s\"");
 #else
-#error "TODO: write sub_808EC58 to match asm/nonmatching/sub_808EC58.s, then delete this #error"
+s32 sub_808DD2C(void *);                        /* extern */
+
+void sub_808EC58(void *arg0) {
+    if (0x38 & (*(u8 *)((s8 *)(arg0) + (0x76)))) {
+        sub_808DD2C(arg0);
+        return;
+    }
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_808DD2C(arg0);
+    }
+}
 #endif
