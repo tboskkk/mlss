f27c5d7ca8f6eda02b6cc56d99f7c2a641dc5fcb
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 13:38:51 -0600

    Match sub_8143374
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8143314.c
+++ src/sub_8143314.c
@@ -25,21 +25,19 @@ void sub_8143314(s32 arg0, void *arg1) {
 }
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8143374.s\"");
-#else
 s32 sub_81387C8(void *, s32);                   /* extern */
 s32 sub_8139030(void *, s32);                   /* extern */
 s32 sub_8143128(s32, void *);                   /* extern */
 extern s32 sub_8139420;
-
-void sub_8143374(s32 arg0, void *arg1) {
-    sub_81387C8(arg1, 0);
-    sub_8139030(arg1, 0);
-    (*(u8 *)((s8 *)(arg1) + (0x214))) = (u8) ((-8 & (*(u8 *)((s8 *)(arg1) + (0x214)))) | 3);
-    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_8143128;
-    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_8139420;
-    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_8139420;
-    sub_8143128(arg0, arg1);
+void sub_8143374(s32 arg0, void *arg1)
+{
+  s32 *new_var;
+  sub_81387C8(arg1, 0);
+  new_var = &arg0;
+  sub_8139030(arg1, 0);
+  *((u8 *) (((s8 *) arg1) + 0x214)) = (u8) (((-8) & (*(0x214 + ((s8 *) arg1)))) | 3);
+  *((s32 (**)(s32, void *)) (((s8 *) arg1) + 0x198)) = sub_8143128;
+  *((s32 **) (((s8 *) arg1) + 0x1A0)) = &sub_8139420;
+  *((s32 **) (((s8 *) arg1) + 0x1A4)) = &sub_8139420;
+  sub_8143128(*new_var, arg1);
 }
-#endif
