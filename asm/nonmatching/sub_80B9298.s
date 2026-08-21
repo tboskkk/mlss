4fd4f7b18ff782772e0d3c7862f70b46d224bf23
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:54:49 -0600

    Extract sub_80B9298
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80B9298.s
@@ -0,0 +1,147 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80B9298
+sub_80B9298:
+	push {r4, r5, r6, lr}
+	add sp, #-0x004
+	ldr r0, _080B9348 @ =0x03000FD8
+	ldr r0, [r0, #0x00]
+	ldr r4, [r0, #0x70]
+	adds r5, r4, #0x0
+	adds r5, #0x08
+	adds r0, r5, #0x0
+	bl sub_8087DE4
+	adds r0, r4, #0x0
+	adds r0, #0x81
+	ldrb r1, [r0, #0x00]
+	movs r0, #0x20
+	ands r0, r1
+	cmp r0, #0x00
+	beq _080B93A4
+	movs r6, #0x01
+	negs r6, r6
+	movs r0, #0x53
+	adds r1, r6, #0x0
+	bl play_sfx_80195B4
+	adds r0, r4, #0x0
+	adds r0, #0x7E
+	ldrb r0, [r0, #0x00]
+	movs r1, #0x06
+	ands r1, r0
+	cmp r1, #0x02
+	beq _080B92D8
+	cmp r1, #0x04
+	bne _080B9316
+_080B92D8:
+	ldr r3, [r5, #0x28]
+	adds r0, r3, #0x0
+	adds r0, #0xD8
+	ldr r0, [r0, #0x00]
+	cmp r0, #0x00
+	bge _080B92E6
+	adds r0, #0xFF
+_080B92E6:
+	asrs r1, r0, #0x08
+	adds r0, r3, #0x0
+	adds r0, #0xDC
+	ldr r0, [r0, #0x00]
+	cmp r0, #0x00
+	bge _080B92F4
+	adds r0, #0xFF
+_080B92F4:
+	asrs r2, r0, #0x08
+	adds r0, r3, #0x0
+	adds r0, #0xE0
+	ldr r0, [r0, #0x00]
+	cmp r0, #0x00
+	bge _080B9302
+	adds r0, #0xFF
+_080B9302:
+	asrs r3, r0, #0x08
+	str r6, [sp, #0x000]
+	adds r0, r5, #0x0
+	bl sub_808843C
+	movs r1, #0x80
+	lsls r1, r1, #0x03
+	adds r0, r5, #0x0
+	bl sub_80880C4
+_080B9316:
+	adds r1, r5, #0x0
+	adds r1, #0x76
+	ldrb r0, [r1, #0x00]
+	movs r2, #0x06
+	ands r2, r0
+	adds r4, r1, #0x0
+	cmp r2, #0x02
+	beq _080B932A
+	cmp r2, #0x04
+	bne _080B938C
+_080B932A:
+	ldr r0, [r5, #0x28]
+	adds r0, #0xEC
+	movs r2, #0x00
+	ldsh r1, [r0, r2]
+	movs r0, #0x01
+	negs r0, r0
+	cmp r1, r0
+	bne _080B9350
+	ldr r2, _080B934C @ =0x00002002
+	adds r0, r5, #0x0
+	movs r1, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	b _080B935C
+_080B9348: .4byte 0x03000FD8
+_080B934C: .4byte 0x00002002
+_080B9350:
+	ldr r2, _080B93AC @ =0x0000204F
+	adds r0, r5, #0x0
+	movs r1, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+_080B935C:
+	ldr r3, [r5, #0x08]
+	movs r2, #0x00
+	adds r0, r5, #0x0
+	adds r0, #0x84
+	movs r6, #0x00
+	ldsh r1, [r0, r6]
+	movs r6, #0x02
+	ldsh r0, [r0, r6]
+	cmp r1, r0
+	blt _080B9372
+	movs r2, #0x01
+_080B9372:
+	lsls r2, r2, #0x06
+	ldrb r1, [r3, #0x11]
+	movs r0, #0x41
+	negs r0, r0
+	ands r0, r1
+	orrs r0, r2
+	strb r0, [r3, #0x11]
+	ldr r2, [r5, #0x08]
+	ldrb r1, [r2, #0x12]
+	movs r0, #0x07
+	negs r0, r0
+	ands r0, r1
+	strb r0, [r2, #0x12]
+_080B938C:
+	ldrb r0, [r4, #0x00]
+	movs r2, #0x06
+	ands r2, r0
+	cmp r2, #0x02
+	beq _080B939A
+	cmp r2, #0x04
+	bne _080B93A4
+_080B939A:
+	movs r0, #0x00
+	str r0, [r5, #0x4C]
+	adds r0, r5, #0x0
+	bl sub_808863C
+_080B93A4:
+	add sp, #0x004
+	pop {r4, r5, r6}
+	pop {r0}
+	bx r0
+_080B93AC: .4byte 0x0000204F
