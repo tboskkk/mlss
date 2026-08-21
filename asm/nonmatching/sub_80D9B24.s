da7850931815a5dd513bbf8458e84125b614b413
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:58:03 -0600

    Extract sub_80D9B24
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80D9B24.s
@@ -0,0 +1,68 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80D9B24
+sub_80D9B24:
+	push {r4, lr}
+	add sp, #-0x00C
+	adds r4, r0, #0x0
+	adds r0, #0xAE
+	movs r2, #0x00
+	ldsh r1, [r0, r2]
+	ldr r0, [r4, #0x38]
+	adds r0, r0, r1
+	str r0, [r4, #0x10]
+	adds r2, r4, #0x0
+	adds r2, #0xB2
+	movs r0, #0x00
+	ldsh r1, [r2, r0]
+	ldr r0, [r4, #0x40]
+	adds r0, r0, r1
+	str r0, [r4, #0x18]
+	ldrh r0, [r2, #0x00]
+	subs r0, #0x5E
+	strh r0, [r2, #0x00]
+	ldr r0, [r4, #0x10]
+	cmp r0, #0x00
+	bge _080D9B52
+	adds r0, #0xFF
+_080D9B52:
+	asrs r0, r0, #0x08
+	str r0, [sp, #0x000]
+	ldr r0, [r4, #0x14]
+	cmp r0, #0x00
+	bge _080D9B5E
+	adds r0, #0xFF
+_080D9B5E:
+	asrs r0, r0, #0x08
+	str r0, [sp, #0x004]
+	ldr r0, [r4, #0x18]
+	cmp r0, #0x00
+	bge _080D9B6A
+	adds r0, #0xFF
+_080D9B6A:
+	asrs r0, r0, #0x08
+	str r0, [sp, #0x008]
+	add r1, sp, #0x004
+	add r2, sp, #0x008
+	mov r0, sp
+	movs r3, #0x00
+	bl sub_807FC08
+	ldr r1, [sp, #0x000]
+	movs r0, #0x10
+	negs r0, r0
+	cmp r1, r0
+	blt _080D9B8E
+	ldr r1, [sp, #0x004]
+	movs r0, #0x88
+	lsls r0, r0, #0x01
+	cmp r1, r0
+	ble _080D9B94
+_080D9B8E:
+	adds r0, r4, #0x0
+	bl sub_807C298
+_080D9B94:
+	add sp, #0x00C
+	pop {r4}
+	pop {r0}
+	bx r0
--- src/sub_80D7410.c
+++ src/sub_80D7410.c
@@ -326,3 +326,9 @@ asm_unified(".include \"asm/nonmatching/sub_80D9B08.s\"");
 #else
 #error "TODO: write sub_80D9B08 to match asm/nonmatching/sub_80D9B08.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80D9B24.s\"");
+#else
+#error "TODO: write sub_80D9B24 to match asm/nonmatching/sub_80D9B24.s, then delete this #error"
+#endif
