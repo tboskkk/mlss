7e16c650eb661171b28a363e40f4b4e240ded721
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:25:32 -0600

    Extract sub_81DAD4C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/__udivsi3.c
+++ src/__udivsi3.c
@@ -18,3 +18,9 @@ asm_unified(".include \"asm/nonmatching/__umodsi3.s\"");
 #else
 #error "TODO: write __umodsi3 to match asm/nonmatching/__umodsi3.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_81DAD4C.s\"");
+#else
+#error "TODO: write sub_81DAD4C to match asm/nonmatching/sub_81DAD4C.s, then delete this #error"
+#endif
