fda0af12ccc51581413c2738b72afc36e74a8374
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:33:03 -0600

    Extract sub_815FA40
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p12.s
+++ asm/text08057568_p12.s
@@ -3,16 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_815FA40
-sub_815FA40:
-	push {lr}
-	ldr r0, [r0, #0x00]
-	cmp r0, #0x00
-	beq _0815FA4C
-	bl sprite_hide_8021F20
-_0815FA4C:
-	pop {r0}
-	bx r0
 	thumb_func_start sub_815FA50
 sub_815FA50:
 	push {lr}
--- src/sub_815FA3C.c
+++ src/sub_815FA3C.c
@@ -10,3 +10,9 @@ asm_unified(".include \"asm/macros.inc\"");
 u32 sub_815FA3C(u32* param_1) {
     return *param_1;
 }
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_815FA40.s\"");
+#else
+#error "TODO: write sub_815FA40 to match asm/nonmatching/sub_815FA40.s, then delete this #error"
+#endif
