6b1510a0ffee5575d29851255c6826d69fe0eea3
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:40:48 -0600

    Draft m2c seeds for sub_8138F64.c's #error placeholders
    Unblocks the whole translation unit (sub_8138F64). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8138F64.c
+++ src/sub_8138F64.c
@@ -10,5 +10,21 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8138F64.s\"");
 #else
-#error "TODO: write sub_8138F64 to match asm/nonmatching/sub_8138F64.s, then delete this #error"
+s32 sub_80FBD44(void *, u8 *);                      /* extern */
+
+s32 sub_8138F64(void *arg0, u16 arg1) {
+    u8 sp0;
+    u16 var_r0_10;
+
+    var_r0_10 = arg1;
+    if ((u32) var_r0_10 >= (u32) (*(u8 *)((s8 *)(arg0) + (0x10B)))) {
+        if ((sub_80FBD44(arg0, &sp0) << 0x18) != 0) {
+            var_r0_10 = (u16) sp0;
+            goto block_3;
+        }
+        return 0;
+    }
+block_3:
+    return (*(s32 *)((s8 *)(((var_r0_10 * 4) + arg0)) + (0x28)));
+}
 #endif
