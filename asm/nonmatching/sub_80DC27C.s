39e773781d915ef719f154f48bb65ba56451e27e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:43:52 -0600

    Extract sub_80DC27C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/nonmatching/sub_80DC27C.s
+++ asm/nonmatching/sub_80DC27C.s
@@ -1,28 +1,20 @@
-	.include "asm/macros.inc"
-
 	.syntax unified
 	.text
 
-	thumb_func_start sub_80DC310
-sub_80DC310:
-	push {r4, r5, lr}
+	thumb_func_start sub_80DC27C
+sub_80DC27C:
+	push {r4, lr}
 	adds r4, r0, #0x0
-	adds r0, #0x80
-	ldr r5, [r0, #0x00]
-	cmp r5, #0x00
-	bne _080DC394
-	adds r1, r4, #0x0
-	adds r1, #0xAC
-	ldrh r0, [r1, #0x00]
-	subs r0, #0x01
-	strh r0, [r1, #0x00]
-	lsls r0, r0, #0x10
+	ldr r0, [r4, #0x08]
+	ldrb r1, [r0, #0x12]
+	movs r0, #0x08
+	ands r0, r1
 	cmp r0, #0x00
-	bge _080DC394
+	beq _080DC300
 	adds r0, r4, #0x0
-	bl sub_807F47C
-	movs r0, #0x81
-	bl stop_sfx_80195A8
+	bl sub_807F4FC
+	ldr r0, _080DC308 @ =0x080DC3B9
+	str r0, [r4, #0x58]
 	adds r1, r4, #0x0
 	adds r1, #0x84
 	ldr r0, [r4, #0x2C]
@@ -30,9 +22,6 @@ sub_80DC310:
 	adds r0, r2, #0x0
 	adds r0, #0xD8
 	ldr r0, [r0, #0x00]
-	movs r3, #0xD0
-	lsls r3, r3, #0x06
-	adds r0, r0, r3
 	str r0, [r1, #0x00]
 	adds r1, #0x04
 	adds r0, r2, #0x0
@@ -52,24 +41,34 @@ sub_80DC310:
 	strb r0, [r2, #0x00]
 	adds r1, r4, #0x0
 	adds r1, #0x7C
-	movs r0, #0xC0
-	lsls r0, r0, #0x03
+	movs r2, #0x00
+	movs r0, #0x80
+	lsls r0, r0, #0x02
 	strh r0, [r1, #0x00]
 	adds r0, r4, #0x0
 	adds r0, #0x7A
-	strh r5, [r0, #0x00]
+	strh r2, [r0, #0x00]
 	adds r0, r4, #0x0
 	bl sub_8085B38
 	adds r0, r4, #0x0
-	movs r1, #0x02
+	movs r1, #0x09
 	movs r2, #0x00
 	movs r3, #0x00
 	bl sub_8082E1C
-	ldr r0, _080DC39C @ =0x080DC3A1
+	movs r1, #0x01
+	negs r1, r1
+	movs r0, #0x81
+	bl play_sfx_80195B4
+	adds r1, r4, #0x0
+	adds r1, #0xAC
+	movs r0, #0x02
+	strh r0, [r1, #0x00]
+	ldr r0, _080DC30C @ =0x080DC311
 	str r0, [r4, #0x4C]
-_080DC394:
-	pop {r4, r5}
+_080DC300:
+	pop {r4}
 	pop {r0}
 	bx r0
 	.byte 0x00, 0x00
-_080DC39C: .4byte sub_80DC3A0
+_080DC308: .4byte sub_80DC3B8
+_080DC30C: .4byte sub_80DC310
