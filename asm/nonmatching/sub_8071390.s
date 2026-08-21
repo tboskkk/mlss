392cbe29a6c1c18d79c0ed5d2936c962516167d3
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:27:02 -0600

    Extract sub_8071390
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8071390.s
@@ -0,0 +1,74 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8071390
+sub_8071390:
+	push {r4, r5, lr}
+	adds r4, r0, #0x0
+	ldr r0, [r4, #0x08]
+	ldrb r1, [r0, #0x12]
+	movs r5, #0x08
+	adds r0, r5, #0x0
+	ands r0, r1
+	cmp r0, #0x00
+	beq _08071416
+	movs r1, #0x01
+	negs r1, r1
+	movs r0, #0x6C
+	bl play_sfx_80195B4
+	ldr r2, _0807141C @ =0x000040B4
+	adds r0, r4, #0x0
+	movs r1, #0x03
+	movs r3, #0x00
+	bl sub_8082E1C
+	ldr r0, [r4, #0x30]
+	movs r1, #0x03
+	movs r2, #0x00
+	movs r3, #0x00
+	bl sub_8082E1C
+	adds r2, r4, #0x0
+	adds r2, #0x84
+	ldr r0, [r4, #0x2C]
+	ldr r1, [r0, #0x28]
+	adds r0, r1, #0x0
+	adds r0, #0xD8
+	ldr r0, [r0, #0x00]
+	movs r3, #0xA0
+	lsls r3, r3, #0x08
+	adds r0, r0, r3
+	str r0, [r2, #0x00]
+	adds r2, #0x04
+	adds r1, #0xDC
+	ldr r0, [r1, #0x00]
+	str r0, [r2, #0x00]
+	adds r1, r4, #0x0
+	adds r1, #0x8C
+	ldr r0, [r4, #0x40]
+	str r0, [r1, #0x00]
+	subs r1, #0x10
+	movs r2, #0x00
+	movs r0, #0x80
+	lsls r0, r0, #0x01
+	strh r0, [r1, #0x00]
+	adds r3, r4, #0x0
+	adds r3, #0x79
+	ldrb r0, [r3, #0x00]
+	movs r1, #0x20
+	orrs r0, r1
+	strb r0, [r3, #0x00]
+	adds r0, r4, #0x0
+	adds r0, #0x7A
+	strh r2, [r0, #0x00]
+	adds r0, r4, #0x0
+	bl sub_8085B38
+	adds r0, r4, #0x0
+	adds r0, #0xAC
+	strh r5, [r0, #0x00]
+	ldr r0, _08071420 @ =0x08071425
+	str r0, [r4, #0x4C]
+_08071416:
+	pop {r4, r5}
+	pop {r0}
+	bx r0
+_0807141C: .4byte 0x000040B4
+_08071420: .4byte sub_8071424
--- src/sub_8070DCC.c
+++ src/sub_8070DCC.c
@@ -126,3 +126,9 @@ asm_unified(".include \"asm/nonmatching/sub_80712D8.s\"");
 #else
 #error "TODO: write sub_80712D8 to match asm/nonmatching/sub_80712D8.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8071390.s\"");
+#else
+#error "TODO: write sub_8071390 to match asm/nonmatching/sub_8071390.s, then delete this #error"
+#endif
