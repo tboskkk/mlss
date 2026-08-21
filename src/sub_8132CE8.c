657efe63164e018619a145b0e3f12a03512dc066
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:39:47 -0600

    Draft m2c seeds for sub_8132CE8.c's #error placeholders
    Unblocks the whole translation unit (sub_8132CE8, sub_8132D4C, sub_8132D78, sub_8132DA0, sub_8132DE4, sub_8132E0C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8132CE8.c
+++ src/sub_8132CE8.c
@@ -10,35 +10,92 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132CE8.s\"");
 #else
-#error "TODO: write sub_8132CE8 to match asm/nonmatching/sub_8132CE8.s, then delete this #error"
+s32 sub_807C298(void *);                        /* extern */
+
+void sub_8132CE8(void *arg0) {
+    s32 temp_r2_30;
+    void *temp_r1_8;
+
+    temp_r1_8 = (*(void **)((s8 *)(arg0) + (8)));
+    (*(u16 *)((s8 *)(temp_r1_8) + (0xC))) = (u16) ((*(u16 *)((s8 *)(temp_r1_8) + (0xC))) + 0x1200);
+    (*(u16 *)((s8 *)(arg0) + (0xB2))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0xB2))) + (*(s32 *)((s8 *)(arg0) + (0x94))));
+    temp_r2_30 = (*(s32 *)((s8 *)(arg0) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */);
+    (*(s32 *)((s8 *)(arg0) + (0x10))) = temp_r2_30;
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x14))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    (*(s32 *)((s8 *)(arg0) + (0x18))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x18))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+    if (temp_r2_30 < 0xFFFFF000) {
+        sub_807C298(arg0);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132D4C.s\"");
 #else
-#error "TODO: write sub_8132D4C to match asm/nonmatching/sub_8132D4C.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_808750C;
+
+void sub_8132D4C(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8082E1C(arg0, 0, 0, 0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_808750C;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132D78.s\"");
 #else
-#error "TODO: write sub_8132D78 to match asm/nonmatching/sub_8132D78.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+
+void sub_8132D78(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8082E1C(arg0, 0, 0, 0);
+        (*(s32 *)((s8 *)(arg0) + (0x4C))) = 0;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132DA0.s\"");
 #else
-#error "TODO: write sub_8132DA0 to match asm/nonmatching/sub_8132DA0.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_808750C;
+
+void sub_8132DA0(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8082E1C(arg0, 0, 0, 0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_808750C;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132DE4.s\"");
 #else
-#error "TODO: write sub_8132DE4 to match asm/nonmatching/sub_8132DE4.s, then delete this #error"
+s32 sub_8082B00();                                  /* extern */
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_8132E34;
+
+void sub_8132DE4(void *arg0) {
+    if (sub_8082B00() == 0) {
+        sub_8082E1C(arg0, 8, 0, 0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8132E34;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8132E0C.s\"");
 #else
-#error "TODO: write sub_8132E0C to match asm/nonmatching/sub_8132E0C.s, then delete this #error"
+s32 sub_8082B00();                                  /* extern */
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_8132E68;
+
+void sub_8132E0C(void *arg0) {
+    if (sub_8082B00() == 0) {
+        sub_8082E1C(arg0, 4, 0, 0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8132E68;
+    }
+}
 #endif
