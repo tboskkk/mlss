bfa14505525142e752ffb184d3cc80056bef7474
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:42:18 -0600

    Extract sub_818CB58
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_8171FF8.c
+++ src/sub_8171FF8.c
@@ -2076,3 +2076,9 @@ asm_unified(".include \"asm/nonmatching/sub_818CB18.s\"");
 #else
 #error "TODO: write sub_818CB18 to match asm/nonmatching/sub_818CB18.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_818CB58.s\"");
+#else
+#error "TODO: write sub_818CB58 to match asm/nonmatching/sub_818CB58.s, then delete this #error"
+#endif
