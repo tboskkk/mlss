7f3a66fa160f0078f8fc7c537eedf7b8fa37d478
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 18:04:35 -0600

    Draft m2c seeds for sub_803C424.c's #error placeholders
    Unblocks the whole translation unit (sub_803C440, sub_803C468, sub_803C4A0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_803C424.c
+++ src/sub_803C424.c
@@ -25,17 +25,39 @@ u32* sub_803C424(u32* param_1) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_803C440.s\"");
 #else
-#error "TODO: write sub_803C440 to match asm/nonmatching/sub_803C440.s, then delete this #error"
+s32 sub_803C440(void *arg0) {
+    s32 var_r1_7;
+    s8 temp_r0_17;
+
+    var_r1_7 = 0;
+    temp_r0_17 = (s8) (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x200)))) + (0x344)));
+    if ((temp_r0_17 == 0x1C) || (temp_r0_17 == 0x15)) {
+        var_r1_7 = 1;
+    }
+    return var_r1_7;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_803C468.s\"");
 #else
-#error "TODO: write sub_803C468 to match asm/nonmatching/sub_803C468.s, then delete this #error"
+s32 sub_803C468(void *arg0) {
+    if ((u32) (u16) ((*(u16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x200)))) + (4))) - 0x2A) > 1U) {
+        (*(u16 *)((s8 *)(arg0) + (0x20A))) = (u16) (0xFFFFFE7F & (*(u16 *)((s8 *)(arg0) + (0x20A))));
+        return 0;
+    }
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_803C4A0.s\"");
 #else
-#error "TODO: write sub_803C4A0 to match asm/nonmatching/sub_803C4A0.s, then delete this #error"
+s32 sub_803C4A0(void *arg0) {
+    if ((u32) (u16) ((*(u16 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x200)))) + (4))) - 0x27) > 1U) {
+        (*(u16 *)((s8 *)(arg0) + (0x20A))) = (u16) (0xFFFFFE7F & (*(u16 *)((s8 *)(arg0) + (0x20A))));
+        return 0;
+    }
+    return 1;
+}
 #endif
