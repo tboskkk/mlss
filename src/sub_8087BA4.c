e2ae124faf606de17b490bfd8cbb19c511e1e287
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:22:28 -0600

    Draft m2c seeds for sub_8087BA4.c's #error placeholders
    Unblocks the whole translation unit (sub_8087BD0, sub_8087C20). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8087BA4.c
+++ src/sub_8087BA4.c
@@ -26,11 +26,51 @@ void sub_8087BA4(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8087BD0.s\"");
 #else
-#error "TODO: write sub_8087BD0 to match asm/nonmatching/sub_8087BD0.s, then delete this #error"
+s32 sub_80DF024(u16, s32, s32, s32, void *);    /* extern */
+
+void sub_8087BD0(void *arg0) {
+    s32 var_r1_17;
+    s32 var_r2_23;
+    s32 var_r3_29;
+    u16 temp_r0_13;
+    void *temp_r4_9;
+
+    temp_r4_9 = (*(void **)((s8 *)(arg0) + (8)));
+    temp_r0_13 = (*(u16 *)((s8 *)((*(void **)((s8 *)(temp_r4_9) + (0xE4)))) + (2)));
+    if (temp_r0_13 != 0) {
+        var_r1_17 = (*(s32 *)((s8 *)(temp_r4_9) + (0x40)));
+        if (var_r1_17 < 0) {
+            var_r1_17 += 0xFF;
+        }
+        var_r2_23 = (*(s32 *)((s8 *)(temp_r4_9) + (0x44)));
+        if (var_r2_23 < 0) {
+            var_r2_23 += 0xFF;
+        }
+        var_r3_29 = (*(s32 *)((s8 *)(temp_r4_9) + (0x48)));
+        if (var_r3_29 < 0) {
+            var_r3_29 += 0xFF;
+        }
+        sub_80DF024(temp_r0_13, var_r1_17 >> 8, var_r2_23 >> 8, var_r3_29 >> 8, temp_r4_9 + 8);
+    }
+    (*(s32 *)((s8 *)(temp_r4_9) + (0x54))) = 0;
+    (*(s32 *)((s8 *)(arg0) + (4))) = 0;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8087C20.s\"");
 #else
-#error "TODO: write sub_8087C20 to match asm/nonmatching/sub_8087C20.s, then delete this #error"
+s32 sub_8086700(void *);                        /* extern */
+
+void sub_8087C20(void *arg0) {
+    u8 temp_r4_15;
+    void *temp_r2_8;
+
+    temp_r2_8 = (*(void **)((s8 *)(arg0) + (8)));
+    temp_r4_15 = 2 & (*(u8 *)((s8 *)(temp_r2_8) + (0x111)));
+    if (temp_r4_15 == 0) {
+        sub_8086700(temp_r2_8 + 8);
+        (*(s32 *)((s8 *)(arg0) + (4))) = (s32) temp_r4_15;
+    }
+}
 #endif
