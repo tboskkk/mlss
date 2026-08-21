44ddc192100d340d7dc4ebfdc0b611086d71e297
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:26:35 -0600

    Extract sub_8070A7C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8070A7C.s
@@ -0,0 +1,21 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8070A7C
+sub_8070A7C:
+	push {lr}
+	adds r2, r0, #0x0
+	adds r1, r2, #0x0
+	adds r1, #0xAC
+	ldrh r0, [r1, #0x00]
+	subs r0, #0x01
+	strh r0, [r1, #0x00]
+	lsls r0, r0, #0x10
+	cmp r0, #0x00
+	bgt _08070A96
+	adds r0, r2, #0x0
+	bl sub_807C298
+_08070A96:
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
--- src/sub_80708F8.c
+++ src/sub_80708F8.c
@@ -62,3 +62,9 @@ asm_unified(".include \"asm/nonmatching/sub_8070A50.s\"");
 #else
 #error "TODO: write sub_8070A50 to match asm/nonmatching/sub_8070A50.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8070A7C.s\"");
+#else
+#error "TODO: write sub_8070A7C to match asm/nonmatching/sub_8070A7C.s, then delete this #error"
+#endif
