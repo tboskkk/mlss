222d128557497a0a41ae27ba91b74b8897b08a6d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:31:52 -0600

    Draft m2c seeds for sub_80EAA5C.c's #error placeholders
    Unblocks the whole translation unit (sub_80EAA5C, sub_80EAA84, sub_80EAACC). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80EAA5C.c
+++ src/sub_80EAA5C.c
@@ -10,17 +10,44 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EAA5C.s\"");
 #else
-#error "TODO: write sub_80EAA5C to match asm/nonmatching/sub_80EAA5C.s, then delete this #error"
+s32 sub_80E9C4C(s32, s32, s32, s32, s32, s32);  /* extern */
+
+s32 sub_80EAA5C(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    sub_80E9C4C(arg3, arg1 + 0x18, 0, 0, M2C_ERROR(/* unknown instruction: ldsh $r2, ($mem_loc_fictive_) */), (s32) (*(u16 *)((s8 *)((void *)0x0300034C) + (0x2E))));
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EAA84.s\"");
 #else
-#error "TODO: write sub_80EAA84 to match asm/nonmatching/sub_80EAA84.s, then delete this #error"
+s32 sub_80E9C4C(s32, s32, s32, s32, s32, s32);  /* extern */
+
+s32 sub_80EAA84(s32 arg0, s32 arg1, void *arg2, s32 arg3) {
+    u16 temp_r2_9;
+    u16 var_r2_13;
+
+    temp_r2_9 = (*(u16 *)((s8 *)(arg2) + (4)));
+    if (temp_r2_9 == 0) {
+        var_r2_13 = (*(u16 *)((s8 *)((void *)0x0300034C) + (0x2A)));
+    } else if (temp_r2_9 == 1) {
+        var_r2_13 = (*(u16 *)((s8 *)((void *)0x0300034C) + (0x28)));
+    } else {
+        var_r2_13 = 0;
+    }
+    sub_80E9C4C(arg3, arg1 + 0x18, 0, 0, M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */), (s32) var_r2_13);
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EAACC.s\"");
 #else
-#error "TODO: write sub_80EAACC to match asm/nonmatching/sub_80EAACC.s, then delete this #error"
+s32 sub_8018818();                              /* extern */
+
+s32 sub_80EAACC(s32 arg0, s32 arg1, s32 *arg2) {
+    *(u16 *)0x030003C4 = (0xFFFFFC00 & *(u16 *)0x030003C4) | (0x3FF & ~*arg2 & 0x3FF);
+    sub_8018818();
+    return 1;
+}
 #endif
