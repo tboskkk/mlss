babb7cf64163c4d0043457d04f74b66f23d74f7b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 18:04:38 -0600

    Draft m2c seeds for sub_80DA1EC.c's #error placeholders
    Unblocks the whole translation unit (sub_80DA1EC, sub_80DA208). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80DA1EC.c
+++ src/sub_80DA1EC.c
@@ -10,11 +10,23 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA1EC.s\"");
 #else
-#error "TODO: write sub_80DA1EC to match asm/nonmatching/sub_80DA1EC.s, then delete this #error"
+extern s32 sub_80D9D5C;
+
+void sub_80DA1EC(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80D9D5C;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA208.s\"");
 #else
-#error "TODO: write sub_80DA208 to match asm/nonmatching/sub_80DA208.s, then delete this #error"
+extern s32 sub_80DA264;
+
+void sub_80DA208(void *arg0) {
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DA264;
+    }
+}
 #endif
