35f0ce06809909cb9616e31235ab55069ce9d120
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 19:58:39 -0600

    Extract sub_801BD30
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_801BC98.c
+++ src/sub_801BC98.c
@@ -18,3 +18,9 @@ asm_unified(".include \"asm/nonmatching/time_init.s\"");
 #else
 #error "TODO: write time_init to match asm/nonmatching/time_init.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_801BD30.s\"");
+#else
+#error "TODO: write sub_801BD30 to match asm/nonmatching/sub_801BD30.s, then delete this #error"
+#endif
