87ca2173f132ef1c3e773c3688df15e821f5fbd2
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:46:07 -0600

    Extract btl_80FC5C0
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/btl_80FC438.c
+++ src/btl_80FC438.c
@@ -12,3 +12,9 @@ asm_unified(".include \"asm/nonmatching/btl_80FC438.s\"");
 #else
 #error "TODO: write btl_80FC438 to match asm/nonmatching/btl_80FC438.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/btl_80FC5C0.s\"");
+#else
+#error "TODO: write btl_80FC5C0 to match asm/nonmatching/btl_80FC5C0.s, then delete this #error"
+#endif
