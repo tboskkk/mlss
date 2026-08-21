0bc9ef545f06c83564d3203f1d0e75241b3dd75c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:32:32 -0600

    Extract sub_815EF80
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_815EF80.s
@@ -0,0 +1,27 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_815EF80
+sub_815EF80:
+	push {r4, r5, lr}
+	adds r4, r0, #0x0
+	adds r5, r1, #0x0
+	bl sub_815FAA4
+	ldr r2, [r4, #0x34]
+	cmp r2, #0x00
+	beq _0815EFA6
+	ldr r1, [r5, #0x00]
+	ldr r0, [r4, #0x04]
+	subs r0, r0, r1
+	asrs r0, r0, #0x08
+	strh r0, [r2, #0x00]
+	ldr r2, [r4, #0x34]
+	ldr r1, [r5, #0x04]
+	ldr r0, [r4, #0x08]
+	subs r0, r0, r1
+	asrs r0, r0, #0x08
+	strh r0, [r2, #0x02]
+_0815EFA6:
+	pop {r4, r5}
+	pop {r0}
+	bx r0
