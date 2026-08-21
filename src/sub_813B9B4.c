86a1bde542248e18f4d2588932302e6254364822
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:41:22 -0600

    Draft m2c seeds for sub_813B9B4.c's #error placeholders
    Unblocks the whole translation unit (sub_813BA0C, sub_813BA80, sub_813BAF0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_813B9B4.c
+++ src/sub_813B9B4.c
@@ -46,17 +46,93 @@ void sub_813B9B4(u32* p1, u32* p2, s16 arg2)
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813BA0C.s\"");
 #else
-#error "TODO: write sub_813BA0C to match asm/nonmatching/sub_813BA0C.s, then delete this #error"
+s8 sub_8139F08(s32, void *, s32);               /* extern */
+extern s32 sub_813B224;
+
+void sub_813BA0C(s32 arg0, void *arg1, void *arg2) {
+    s32 *var_r0_17;
+    u16 temp_r0_47;
+    u32 var_r0_26;
+
+    if (sub_8139F08(arg0, arg1, 0) != -1) {
+        var_r0_17 = &sub_813B224;
+        goto block_7;
+    }
+    if ((*(u16 *)((s8 *)(arg1) + (0x2DE))) == 0) {
+        var_r0_26 = 0;
+    } else {
+        (*(s32 *)((s8 *)(arg1) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0xC))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        (*(s32 *)((s8 *)(arg1) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        temp_r0_47 = (*(u16 *)((s8 *)(arg1) + (0x2DE))) - 1;
+        (*(u16 *)((s8 *)(arg1) + (0x2DE))) = temp_r0_47;
+        var_r0_26 = (u32) (0 - temp_r0_47) >> 0x1F;
+    }
+    if (var_r0_26 == 0) {
+        var_r0_17 = (*(s32 **)((s8 *)(arg2) + (4)));
+block_7:
+        (*(s32 **)((s8 *)(arg2) + (0))) = var_r0_17;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813BA80.s\"");
 #else
-#error "TODO: write sub_813BA80 to match asm/nonmatching/sub_813BA80.s, then delete this #error"
+s8 sub_8139F08(s32, void *, s32);               /* extern */
+
+void sub_813BA80(s32 arg0, void *arg1, void *arg2) {
+    s32 var_r0_17;
+    u16 temp_r0_46;
+    u32 var_r0_25;
+
+    if (sub_8139F08(arg0, arg1, 0) != -1) {
+        var_r0_17 = (*(s32 *)((s8 *)(arg2) + (8)));
+        goto block_7;
+    }
+    if ((*(u16 *)((s8 *)(arg1) + (0x2DE))) == 0) {
+        var_r0_25 = 0;
+    } else {
+        (*(s32 *)((s8 *)(arg1) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0xC))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        (*(s32 *)((s8 *)(arg1) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        temp_r0_46 = (*(u16 *)((s8 *)(arg1) + (0x2DE))) - 1;
+        (*(u16 *)((s8 *)(arg1) + (0x2DE))) = temp_r0_46;
+        var_r0_25 = (u32) (0 - temp_r0_46) >> 0x1F;
+    }
+    if (var_r0_25 == 0) {
+        var_r0_17 = (*(s32 *)((s8 *)(arg2) + (4)));
+block_7:
+        (*(s32 *)((s8 *)(arg2) + (0))) = var_r0_17;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813BAF0.s\"");
 #else
-#error "TODO: write sub_813BAF0 to match asm/nonmatching/sub_813BAF0.s, then delete this #error"
+s8 sub_8139E88(s32, void *, s32);               /* extern */
+
+void sub_813BAF0(s32 arg0, void *arg1, void *arg2) {
+    s32 var_r0_17;
+    u16 temp_r0_46;
+    u32 var_r0_25;
+
+    if (sub_8139E88(arg0, arg1, 0) != -1) {
+        var_r0_17 = (*(s32 *)((s8 *)(arg2) + (8)));
+        goto block_7;
+    }
+    if ((*(u16 *)((s8 *)(arg1) + (0x2DE))) == 0) {
+        var_r0_25 = 0;
+    } else {
+        (*(s32 *)((s8 *)(arg1) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0xC))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        (*(s32 *)((s8 *)(arg1) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+        temp_r0_46 = (*(u16 *)((s8 *)(arg1) + (0x2DE))) - 1;
+        (*(u16 *)((s8 *)(arg1) + (0x2DE))) = temp_r0_46;
+        var_r0_25 = (u32) (0 - temp_r0_46) >> 0x1F;
+    }
+    if (var_r0_25 == 0) {
+        var_r0_17 = (*(s32 *)((s8 *)(arg2) + (4)));
+block_7:
+        (*(s32 *)((s8 *)(arg2) + (0))) = var_r0_17;
+    }
+}
 #endif
