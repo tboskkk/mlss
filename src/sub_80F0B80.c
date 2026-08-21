e22e33da29211b1e2db8ec9bd905144a02684a82
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:33:10 -0600

    Draft m2c seeds for sub_80F0B80.c's #error placeholders
    Unblocks the whole translation unit (sub_80F0BA4, sub_80F0BC0, sub_80F0BD8, sub_80F0BF0, sub_80F0C08). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80F0B80.c
+++ src/sub_80F0B80.c
@@ -18,29 +18,94 @@ s32 sub_80F0B80(void)
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F0BA4.s\"");
 #else
-#error "TODO: write sub_80F0BA4 to match asm/nonmatching/sub_80F0BA4.s, then delete this #error"
+s32 sub_805C78C(s32, u8, u8);                   /* extern */
+
+s32 sub_80F0BA4(void *arg0, void *arg2) {
+    sub_805C78C((*(s32 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x14)))) + (0x304))), (*(u8 *)((s8 *)(arg2) + (0))), (*(u8 *)((s8 *)(arg2) + (4))));
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F0BC0.s\"");
 #else
-#error "TODO: write sub_80F0BC0 to match asm/nonmatching/sub_80F0BC0.s, then delete this #error"
+s32 sub_805C7B4(s32, u8);                       /* extern */
+
+s32 sub_80F0BC0(void *arg0, u8 *arg2) {
+    sub_805C7B4((*(s32 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x14)))) + (0x304))), *arg2);
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F0BD8.s\"");
 #else
-#error "TODO: write sub_80F0BD8 to match asm/nonmatching/sub_80F0BD8.s, then delete this #error"
+s32 sub_805B490(s32, u16);                      /* extern */
+
+s32 sub_80F0BD8(void *arg0, u16 *arg2) {
+    sub_805B490((*(s32 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x14)))) + (0x304))), *arg2);
+    return 0;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F0BF0.s\"");
 #else
-#error "TODO: write sub_80F0BF0 to match asm/nonmatching/sub_80F0BF0.s, then delete this #error"
+s32 sub_805B618(s32, u16);                      /* extern */
+
+s32 sub_80F0BF0(void *arg0, u16 *arg2) {
+    sub_805B618((*(s32 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x14)))) + (0x304))), *arg2);
+    return 0;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F0C08.s\"");
 #else
-#error "TODO: write sub_80F0C08 to match asm/nonmatching/sub_80F0C08.s, then delete this #error"
+s32 sub_80F0C08(s32 arg0, void *arg1, void *arg2) {
+    s32 var_r6_16;
+    u8 var_r0_75;
+    void *temp_r3_20;
+    void *temp_r3_59;
+    void *var_r12_15;
+
+    var_r12_15 = *(void **)0x03000FD0 + 0x2A0;
+    var_r6_16 = 0;
+loop_1:
+    temp_r3_20 = *(void **)0x03000FD0;
+    if (((s32) ((u32) ((*(u8 *)((s8 *)(temp_r3_20) + (0x55B))) << 0x1B) >> 0x1C) >> var_r6_16) & 1) {
+        if (arg1 != NULL) {
+            if (((*(u8 *)((s8 *)(arg1) + (0x54))) + 4) == (*(u8 *)((s8 *)(var_r12_15) + (0x1C)))) {
+                goto block_11;
+            }
+            goto block_6;
+        }
+        if ((*(u8 *)((s8 *)(temp_r3_20) + (0x548))) != (*(u8 *)((s8 *)(var_r12_15) + (0x1C)))) {
+            goto block_6;
+        }
+        goto block_11;
+    }
+block_6:
+    temp_r3_59 = *(void **)0x03000FD0;
+    if (((s32) ((u32) ((*(u8 *)((s8 *)(temp_r3_59) + (0x55B))) << 0x1B) >> 0x1C) >> (var_r6_16 + 1)) & 1) {
+        if (arg1 != NULL) {
+            var_r0_75 = (*(u8 *)((s8 *)(arg1) + (0x54))) + 4;
+        } else {
+            var_r0_75 = (*(u8 *)((s8 *)(temp_r3_59) + (0x548)));
+        }
+        if (var_r0_75 == (*(u8 *)((s8 *)((var_r12_15 + 0x20)) + (0x1C)))) {
+block_11:
+            (*(s32 *)((s8 *)(arg2) + (0))) = (s32) (*(s32 *)((s8 *)(arg2) + (0x14)));
+            return 0;
+        }
+        goto block_12;
+    }
+block_12:
+    var_r12_15 += 0x40;
+    var_r6_16 += 2;
+    if (var_r6_16 > 3) {
+        return 1;
+    }
+    goto loop_1;
+}
 #endif
