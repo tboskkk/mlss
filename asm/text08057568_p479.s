3c74d09ba11b6b66344530d976a223a17ab94d39
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:49:23 -0600

    Extract sub_8112450
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p479.s
+++ asm/text08057568_p479.s
@@ -3,39 +3,3 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_8112450
-sub_8112450:
-	push {r4, lr}
-	adds r4, r0, #0x0
-	adds r1, r4, #0x0
-	adds r1, #0xAC
-	ldrh r0, [r1, #0x00]
-	subs r0, #0x01
-	strh r0, [r1, #0x00]
-	lsls r0, r0, #0x10
-	cmp r0, #0x00
-	bne _0811248E
-	adds r0, r4, #0x0
-	movs r1, #0x05
-	movs r2, #0x00
-	movs r3, #0x00
-	bl sub_8082E1C
-	ldr r2, [r4, #0x08]
-	ldrb r1, [r2, #0x12]
-	movs r0, #0x07
-	negs r0, r0
-	ands r0, r1
-	movs r1, #0x02
-	orrs r0, r1
-	strb r0, [r2, #0x12]
-	ldr r0, _08112494 @ =0x081126E5
-	str r0, [r4, #0x4C]
-	movs r1, #0x01
-	negs r1, r1
-	movs r0, #0xA3
-	bl play_sfx_80195B4
-_0811248E:
-	pop {r4}
-	pop {r0}
-	bx r0
-_08112494: .4byte sub_81126E4
