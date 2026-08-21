aad51ad73169c34f9d33a56f5ed9b5c0a5e00cca
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:33:46 -0600

    Extract sub_81637AC
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p83.s
+++ asm/text08057568_p83.s
@@ -3,56 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_81637AC
-sub_81637AC:
-	push {r4, r5, r6, r7, lr}
-	adds r5, r0, #0x0
-	ldr r0, [r5, #0x00]
-	cmp r0, #0x00
-	beq _081637FC
-	cmp r0, #0x01
-	bne _081637FC
-	movs r6, #0x00
-	adds r4, r5, #0x4
-	adds r7, r4, #0x0
-_081637C0:
-	ldr r1, [r4, #0x00]
-	ldr r0, [r1, #0x44]
-	cmp r0, #0x00
-	beq _081637D0
-	ldr r0, [r1, #0x40]
-	movs r1, #0x00
-	cmp r0, #0x01
-	bne _081637D2
-_081637D0:
-	movs r1, #0x01
-_081637D2:
-	cmp r1, #0x00
-	beq _081637DA
-	adds r6, #0x01
-	b _081637EA
-_081637DA:
-	ldr r0, [r4, #0x00]
-	bl sub_8161E38
-	ldr r0, [r4, #0x00]
-	movs r1, #0x00
-	movs r2, #0x00
-	bl sub_8161C9C
-_081637EA:
-	adds r4, #0x04
-	adds r0, r7, #0x0
-	adds r0, #0x08
-	cmp r4, r0
-	ble _081637C0
-	cmp r6, #0x03
-	bne _081637FC
-	movs r0, #0x00
-	str r0, [r5, #0x00]
-_081637FC:
-	pop {r4, r5, r6, r7}
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
 	thumb_func_start sub_8163804
 sub_8163804:
 	push {r4, r5, r6, lr}
--- src/sub_8163748.c
+++ src/sub_8163748.c
@@ -22,3 +22,9 @@ asm_unified(".include \"asm/nonmatching/sub_816375C.s\"");
 #else
 #error "TODO: write sub_816375C to match asm/nonmatching/sub_816375C.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_81637AC.s\"");
+#else
+#error "TODO: write sub_81637AC to match asm/nonmatching/sub_81637AC.s, then delete this #error"
+#endif
