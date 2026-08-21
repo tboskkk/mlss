fcf818a3e4f3401cc4b3eeacff22aacf79216894
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:16:11 -0600

    Draft m2c seeds for sub_8047EFC.c's #error placeholders
    Unblocks the whole translation unit (sub_8047F60). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8047EFC.c
+++ src/sub_8047EFC.c
@@ -29,5 +29,26 @@ void sub_8047EFC(void *arg0, u16 arg1, s16 arg2, s16 arg3, s32 arg4) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8047F60.s\"");
 #else
-#error "TODO: write sub_8047F60 to match asm/nonmatching/sub_8047F60.s, then delete this #error"
+void sub_8047F60(void *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5) {
+    if (arg5 == 0) {
+        (*(s32 *)((s8 *)(arg0) + (0xC))) = arg1;
+        (*(s32 *)((s8 *)(arg0) + (0x10))) = arg2;
+        (*(s32 *)((s8 *)(arg0) + (0x18))) = arg3;
+    } else {
+        (*(s32 *)((s8 *)(arg0) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0xC))) + arg1);
+        (*(s32 *)((s8 *)(arg0) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x10))) + arg2);
+        (*(s32 *)((s8 *)(arg0) + (0x18))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x18))) + ((*(s32 *)((s8 *)(arg0) + (0x14))) + arg3));
+    }
+    (*(s32 *)((s8 *)(arg0) + (0x14))) = 0;
+    if (arg4 != 0) {
+        (*(s32 *)((s8 *)(arg0) + (0x34))) = (s32) (*(s32 *)((s8 *)(arg0) + (0xC)));
+        (*(s32 *)((s8 *)(arg0) + (0x38))) = (s32) (*(s32 *)((s8 *)(arg0) + (0x10)));
+        (*(s32 *)((s8 *)(arg0) + (0x3C))) = 0;
+        (*(s32 *)((s8 *)(arg0) + (0x40))) = (s32) (*(s32 *)((s8 *)(arg0) + (0x18)));
+        if ((*(s32 *)((s8 *)(arg0) + (0x290))) != 0) {
+            (*(s32 (**)(void *, s32))((s8 *)(((*(s32 *)((s8 *)(arg0) + (0x338))) + 0x78)) + (4)))(arg0 + M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */), 1);
+        }
+    }
+    (*(u8 *)((s8 *)(arg0) + (0x217))) = (u8) ((*(u8 *)((s8 *)(arg0) + (0x217))) | 1);
+}
 #endif
