2be712c7d1821d9330a45b645aeadf8e1ff46a1e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:20:46 -0600

    Extract sub_806800C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p229.s
+++ asm/text08057568_p229.s
@@ -3,46 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_806800C
-sub_806800C:
-	push {r4, lr}
-	add sp, #-0x004
-	adds r4, r0, #0x0
-	bl sub_80871A8
-	cmp r0, #0x00
-	bne _08068046
-	ldr r0, _08068050 @ =0x0806819D
-	str r0, [r4, #0x4C]
-	ldr r1, [r4, #0x38]
-	cmp r1, #0x00
-	bge _08068026
-	adds r1, #0xFF
-_08068026:
-	asrs r1, r1, #0x08
-	ldr r2, [r4, #0x3C]
-	cmp r2, #0x00
-	bge _08068030
-	adds r2, #0xFF
-_08068030:
-	asrs r2, r2, #0x08
-	ldr r3, [r4, #0x40]
-	cmp r3, #0x00
-	bge _0806803A
-	adds r3, #0xFF
-_0806803A:
-	asrs r3, r3, #0x08
-	str r4, [sp, #0x000]
-	ldr r0, _08068054 @ =0x000010C6
-	bl sub_80DF024
-	movs r0, #0x00
-_08068046:
-	add sp, #0x004
-	pop {r4}
-	pop {r1}
-	bx r1
-	.byte 0x00, 0x00
-_08068050: .4byte sub_806819C
-_08068054: .4byte 0x000010C6
 	thumb_func_start sub_8068058
 sub_8068058:
 	push {r4, lr}
--- src/sub_8067A4C.c
+++ src/sub_8067A4C.c
@@ -48,3 +48,9 @@ asm_unified(".include \"asm/nonmatching/sub_8067F94.s\"");
 #else
 #error "TODO: write sub_8067F94 to match asm/nonmatching/sub_8067F94.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_806800C.s\"");
+#else
+#error "TODO: write sub_806800C to match asm/nonmatching/sub_806800C.s, then delete this #error"
+#endif
