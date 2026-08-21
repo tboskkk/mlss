d417e928dbab504e0814b5c4cd92f61a23b338f9
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:59:27 -0600

    Extract sub_80FAD2C
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80FAD2C.s
@@ -0,0 +1,40 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80FAD2C
+sub_80FAD2C:
+	push {lr}
+	ldr r3, [r2, #0x04]
+	movs r1, #0xA8
+	muls r1, r3
+	ldr r0, [r0, #0x24]
+	adds r0, r0, r1
+	ldr r2, [r2, #0x00]
+	cmp r2, #0x00
+	beq _080FAD44
+	cmp r2, #0x01
+	beq _080FAD52
+	b _080FAD60
+_080FAD44:
+	movs r1, #0xA4
+	lsls r1, r1, #0x01
+	adds r2, r0, r1
+	ldrh r1, [r2, #0x00]
+	movs r0, #0x04
+	orrs r0, r1
+	b _080FAD5E
+_080FAD52:
+	movs r1, #0xA4
+	lsls r1, r1, #0x01
+	adds r2, r0, r1
+	ldrh r1, [r2, #0x00]
+	ldr r0, _080FAD68 @ =0x0000FFFB
+	ands r0, r1
+_080FAD5E:
+	strh r0, [r2, #0x00]
+_080FAD60:
+	movs r0, #0x01
+	pop {r1}
+	bx r1
+	.byte 0x00, 0x00
+_080FAD68: .4byte 0x0000FFFB
---
+++ src/sub_80FAD2C.c
@@ -0,0 +1,14 @@
+#include "global.h"
+#include "common.h"
+
+// TODO: add other includes (a matching *.h, other subsystem headers) as
+// this file grows. Left minimal since split_func.py doesn't know yet what
+// sub_80FAD2C needs.
+
+asm_unified(".include \"asm/macros.inc\"");
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80FAD2C.s\"");
+#else
+#error "TODO: write sub_80FAD2C to match asm/nonmatching/sub_80FAD2C.s, then delete this #error"
+#endif
