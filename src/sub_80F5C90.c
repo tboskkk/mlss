9760eedfb5294bf0ec29c081c1cc230c7c025a87
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:37:36 -0600

    Draft m2c seeds for sub_80F5C90.c's #error placeholders
    Unblocks the whole translation unit (sub_80F5FF8). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80F5C90.c
+++ src/sub_80F5C90.c
@@ -37,5 +37,33 @@ asm_unified(".include \"asm/nonmatching/sub_80F5F2C.s\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F5FF8.s\"");
 #else
-#error "TODO: write sub_80F5FF8 to match asm/nonmatching/sub_80F5FF8.s, then delete this #error"
+s32 sub_80F5FF8(void *arg0, void *arg2) {
+    s32 temp_r0_24;
+    s32 temp_r0_40;
+    void *temp_r2_13;
+    void *temp_r2_59;
+    void *temp_r2_71;
+    void *temp_r2_82;
+    void *temp_r5_31;
+    void *temp_r7_14;
+
+    temp_r2_13 = (*(void **)((s8 *)(arg0) + (0x2C)));
+    temp_r7_14 = arg2 + 4;
+    (*(u8 *)((s8 *)(temp_r2_13) + (0x179D))) = (u8) ((0x7F & (*(u8 *)((s8 *)(temp_r2_13) + (0x179D)))) | ((*(s32 *)((s8 *)(arg2) + (0))) << 7));
+    temp_r0_24 = (*(s32 *)((s8 *)(arg2) + (4)));
+    if (temp_r0_24 < 0) {
+        (*(s32 *)((s8 *)(arg2) + (4))) = (s32) (0 - temp_r0_24);
+    }
+    temp_r5_31 = (*(void **)((s8 *)(arg0) + (0x2C)));
+    temp_r0_40 = ((s32) (*(s32 *)((s8 *)(arg2) + (4))) / (s32) (*(s32 *)((s8 *)(temp_r7_14) + (4)))) & 0x1F;
+    (*(u8 *)((s8 *)(temp_r5_31) + (0x179F))) = (u8) ((-0x7D & (*(u8 *)((s8 *)(temp_r5_31) + (0x179F)))) | (temp_r0_40 * 4));
+    (*(u16 *)((s8 *)(temp_r5_31) + (0x179E))) = (u16) ((0xFFFFFC1F & (*(u16 *)((s8 *)(temp_r5_31) + (0x179E)))) | (temp_r0_40 << 5));
+    temp_r2_59 = (*(void **)((s8 *)(arg0) + (0x2C)));
+    (*(u8 *)((s8 *)(temp_r2_59) + (0x179E))) = (u8) ((-0x20 & (*(u8 *)((s8 *)(temp_r2_59) + (0x179E)))) | ((*(s32 *)((s8 *)(temp_r7_14) + (4))) & 0x1F));
+    temp_r2_71 = (*(void **)((s8 *)(arg0) + (0x2C)));
+    (*(u8 *)((s8 *)(temp_r2_71) + (0x179D))) = (u8) ((-0x7D & (*(u8 *)((s8 *)(temp_r2_71) + (0x179D)))) | (((*(s32 *)((s8 *)((temp_r7_14 + 4)) + (4))) & 0x1F) * 4));
+    temp_r2_82 = (*(void **)((s8 *)(arg0) + (0x2C)));
+    (*(u8 *)((s8 *)(temp_r2_82) + (0x179D))) = (u8) ((-4 & (*(u8 *)((s8 *)(temp_r2_82) + (0x179D)))) | 2);
+    return 1;
+}
 #endif
