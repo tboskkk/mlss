f41d535250f45c1d7be2efc1f7eabffcc319ba00
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:32:26 -0600

    Extract sub_815EE30
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- asm/text08057568_p418.s
+++ asm/text08057568_p418.s
@@ -3,16 +3,6 @@
 	.syntax unified
 	.text
 
-	thumb_func_start sub_815EE30
-sub_815EE30:
-	push {lr}
-	ldr r2, _0815EE40 @ =0x08CDCB50
-	str r2, [r0, #0x30]
-	bl sub_815FB14
-	pop {r0}
-	bx r0
-	.byte 0x00, 0x00
-_0815EE40: .4byte 0x08CDCB50
 	thumb_func_start sub_815EE44
 sub_815EE44:
 	push {r4, r5, lr}
--- src/sub_815EDF4.c
+++ src/sub_815EDF4.c
@@ -18,3 +18,9 @@ asm_unified(".include \"asm/nonmatching/sub_815EE08.s\"");
 #else
 #error "TODO: write sub_815EE08 to match asm/nonmatching/sub_815EE08.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_815EE30.s\"");
+#else
+#error "TODO: write sub_815EE30 to match asm/nonmatching/sub_815EE30.s, then delete this #error"
+#endif
