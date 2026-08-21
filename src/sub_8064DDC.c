4fd473b63891e647d5bca7606183e844cab2968e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:19:00 -0600

    Extract sub_80651CC
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_8064DDC.c
+++ src/sub_8064DDC.c
@@ -54,3 +54,9 @@ asm_unified(".include \"asm/nonmatching/sub_80651B0.s\"");
 #else
 #error "TODO: write sub_80651B0 to match asm/nonmatching/sub_80651B0.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80651CC.s\"");
+#else
+#error "TODO: write sub_80651CC to match asm/nonmatching/sub_80651CC.s, then delete this #error"
+#endif
