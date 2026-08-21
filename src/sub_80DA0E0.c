58c1e8dfb878bf24c75ad853ae440c74631d0261
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:29:07 -0600

    Draft m2c seeds for sub_80DA0E0.c's #error placeholders
    Unblocks the whole translation unit (sub_80DA100, sub_80DA140, sub_80DA180). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80DA0E0.c
+++ src/sub_80DA0E0.c
@@ -22,17 +22,66 @@ void sub_80DA0E0(void *arg0) {
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA100.s\"");
 #else
-#error "TODO: write sub_80DA100 to match asm/nonmatching/sub_80DA100.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_80DA1EC;
+
+void sub_80DA100(void *arg0) {
+    void *temp_r2_17;
+
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        sub_8082E1C(arg0, 3, 0, 0);
+        temp_r2_17 = (*(void **)((s8 *)(arg0) + (8)));
+        (*(u8 *)((s8 *)(temp_r2_17) + (0x12))) = (u8) ((-7 & (*(u8 *)((s8 *)(temp_r2_17) + (0x12)))) | 2);
+        (*(s16 *)((s8 *)(arg0) + (0xAC))) = 8;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DA1EC;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA140.s\"");
 #else
-#error "TODO: write sub_80DA140 to match asm/nonmatching/sub_80DA140.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_80DA208;
+
+void sub_80DA140(void *arg0) {
+    void *temp_r2_17;
+
+    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
+        sub_8082E1C(arg0, 3, 0, 0);
+        temp_r2_17 = (*(void **)((s8 *)(arg0) + (8)));
+        (*(u8 *)((s8 *)(temp_r2_17) + (0x12))) = (u8) ((-7 & (*(u8 *)((s8 *)(temp_r2_17) + (0x12)))) | 2);
+        (*(s16 *)((s8 *)(arg0) + (0xAC))) = 0x20;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DA208;
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA180.s\"");
 #else
-#error "TODO: write sub_80DA180 to match asm/nonmatching/sub_80DA180.s, then delete this #error"
+s32 sub_807FB64(void *);                        /* extern */
+s32 sub_8085B38(void *);                        /* extern */
+extern s32 sub_80DA224;
+
+void sub_80DA180(void *arg0) {
+    void *temp_r1_17;
+    void *temp_r2_18;
+    void *temp_r2_8;
+
+    temp_r2_8 = (*(void **)((s8 *)(arg0) + (8)));
+    if (8 & (*(u8 *)((s8 *)(temp_r2_8) + (0x12)))) {
+        sub_807FB64(temp_r2_8);
+        temp_r1_17 = arg0 + 0x84;
+        temp_r2_18 = (*(void **)((s8 *)(arg0) + (0x28)));
+        (*(s32 *)((s8 *)(arg0) + (0x84))) = (s32) (*(s32 *)((s8 *)(temp_r2_18) + (0xD8)));
+        (*(s32 *)((s8 *)(temp_r1_17) + (4))) = (s32) (*(s32 *)((s8 *)(temp_r2_18) + (0xDC)));
+        (*(s32 *)((s8 *)((temp_r1_17 + 4)) + (4))) = (s32) (*(s32 *)((s8 *)(temp_r2_18) + (0xE0)));
+        (*(u8 *)((s8 *)(arg0) + (0x79))) = (u8) ((*(u8 *)((s8 *)(arg0) + (0x79))) | 0x20);
+        (*(s16 *)((s8 *)(arg0) + (0x7C))) = 0x400;
+        (*(s16 *)((s8 *)(arg0) + (0x7A))) = 0;
+        sub_8085B38(arg0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DA224;
+    }
+}
 #endif
