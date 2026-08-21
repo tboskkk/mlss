b504e30426a39893b4a5db905e44e9f87a55e4cc
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:40:01 -0600

    Draft m2c seeds for sub_8133AE8.c's #error placeholders
    Unblocks the whole translation unit (sub_8133B54). 1 left empty (seed didn't compile): sub_8133AE8. Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8133AE8.c
+++ src/sub_8133AE8.c
@@ -10,11 +10,34 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8133AE8.s\"");
 #else
-#error "TODO: write sub_8133AE8 to match asm/nonmatching/sub_8133AE8.s, then delete this #error"
+/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
+   the REST of this translation unit still builds and can be diffed under
+   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
+   still gets the verbatim retail bytes. */
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8133B54.s\"");
 #else
-#error "TODO: write sub_8133B54 to match asm/nonmatching/sub_8133B54.s, then delete this #error"
+s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
+extern s32 sub_81334F4;
+extern s32 sub_8133BC0;
+
+void sub_8133B54(void *arg0) {
+    s32 *var_r0_37;
+
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        if ((s32) M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) < (s32) (*(s32 *)((s8 *)(arg0) + (0x9C)))) {
+            (*(s32 *)((s8 *)(arg0) + (0x18))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x18))) + 0x1000);
+            sub_8082E1C(arg0, 0xC, 0, 0);
+            play_sfx_80195B4(0xD5, -1);
+            var_r0_37 = &sub_8133BC0;
+        } else {
+            (*(s16 *)((s8 *)(arg0) + (0xAC))) = 0;
+            sub_8082E1C(arg0, 0xE, 0, 0);
+            var_r0_37 = &sub_81334F4;
+        }
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = var_r0_37;
+    }
+}
 #endif
