6a09704244a8ed11253b588e389030ca5d85b9f2
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:44:18 -0600

    Extract sub_80E5810
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_80E5810.s
@@ -0,0 +1,54 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_80E5810
+sub_80E5810:
+	push {r4, r5, r6, lr}
+	mov r6, r8
+	push {r6}
+	ldr r6, _080E5868 @ =0x03000FB4
+	ldr r5, _080E586C @ =0x08001AF8
+	ldr r0, _080E5870 @ =0x08001010
+	mov r8, r0
+	subs r5, r5, r0
+	ldr r4, _080E5874 @ =0x0819941C
+	ldr r0, _080E5878 @ =0x08198784
+	subs r4, r4, r0
+	adds r4, r5, r4
+	ldr r1, _080E587C @ =0x08000F9C
+	ldr r0, _080E5880 @ =0x08000DB4
+	subs r1, r1, r0
+	adds r1, r4, r1
+	ldr r2, _080E5884 @ =0x08200130
+	movs r0, #0x00
+	bl alloc_heap_8018CEC
+	adds r1, r0, #0x0
+	str r1, [r6, #0x00]
+	lsls r2, r5, #0x09
+	lsrs r2, r2, #0x0B
+	movs r0, #0x80
+	lsls r0, r0, #0x13
+	orrs r2, r0
+	mov r0, r8
+	bl CpuSet
+	ldr r0, [r6, #0x00]
+	adds r0, r0, r5
+	ldr r1, _080E5888 @ =0x0851F9E8
+	bl sub_81994EC
+	ldr r0, [r6, #0x00]
+	adds r0, r0, r4
+	bl sub_80E3FD8
+	pop {r3}
+	mov r8, r3
+	pop {r4, r5, r6}
+	pop {r0}
+	bx r0
+_080E5868: .4byte 0x03000FB4
+_080E586C: .4byte 0x08001AF8
+_080E5870: .4byte 0x08001010
+_080E5874: .4byte 0x0819941C
+_080E5878: .4byte 0x08198784
+_080E587C: .4byte 0x08000F9C
+_080E5880: .4byte 0x08000DB4
+_080E5884: .4byte 0x08200130
+_080E5888: .4byte 0x0851F9E8
--- src/sub_80E57FC.c
+++ src/sub_80E57FC.c
@@ -12,3 +12,9 @@ asm_unified(".include \"asm/nonmatching/sub_80E57FC.s\"");
 #else
 #error "TODO: write sub_80E57FC to match asm/nonmatching/sub_80E57FC.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_80E5810.s\"");
+#else
+#error "TODO: write sub_80E5810 to match asm/nonmatching/sub_80E5810.s, then delete this #error"
+#endif
