0bcb04ef0fd4b61808bd00759146bec26190b2e4
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:53:26 -0600

    Extract sub_8148264
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_814765C.c
+++ src/sub_814765C.c
@@ -72,3 +72,9 @@ asm_unified(".include \"asm/nonmatching/sub_8148038.s\"");
 #else
 #error "TODO: write sub_8148038 to match asm/nonmatching/sub_8148038.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8148264.s\"");
+#else
+#error "TODO: write sub_8148264 to match asm/nonmatching/sub_8148264.s, then delete this #error"
+#endif
