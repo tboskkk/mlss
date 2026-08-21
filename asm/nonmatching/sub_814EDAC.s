53e39b4bf0fe5f46147f64dabcc763068cef229f
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:01:18 -0600

    Extract sub_814EDAC
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_814EDAC.s
@@ -0,0 +1,57 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_814EDAC
+sub_814EDAC:
+	push {r4, r5, r6, lr}
+	adds r6, r0, #0x0
+	movs r0, #0x80
+	lsls r0, r0, #0x01
+	adds r4, r6, r0
+	movs r1, #0xD8
+	lsls r1, r1, #0x01
+	adds r0, r6, r1
+	ldr r5, [r0, #0x00]
+	adds r0, r5, #0x0
+	adds r1, r4, #0x0
+	movs r2, #0x04
+	bl CpuSet
+	adds r5, #0x08
+	movs r0, #0x00
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x08]
+	movs r0, #0x01
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x0A]
+	adds r5, #0x02
+	movs r0, #0x00
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x0C]
+	adds r5, #0x01
+	movs r0, #0x00
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x0E]
+	adds r5, #0x01
+	movs r0, #0x88
+	lsls r0, r0, #0x01
+	adds r1, r6, r0
+	adds r0, r5, #0x0
+	movs r2, #0x04
+	bl CpuSet
+	adds r5, #0x08
+	movs r0, #0x00
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x28]
+	movs r0, #0x01
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x2A]
+	adds r5, #0x02
+	movs r0, #0x00
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x2C]
+	movs r0, #0x01
+	ldsb r0, [r5, r0]
+	strh r0, [r4, #0x2E]
+	pop {r4, r5, r6}
+	pop {r0}
+	bx r0
--- src/sub_814E8B0.c
+++ src/sub_814E8B0.c
@@ -30,3 +30,9 @@ asm_unified(".include \"asm/nonmatching/sub_814ECE0.s\"");
 #else
 #error "TODO: write sub_814ECE0 to match asm/nonmatching/sub_814ECE0.s, then delete this #error"
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_814EDAC.s\"");
+#else
+#error "TODO: write sub_814EDAC to match asm/nonmatching/sub_814EDAC.s, then delete this #error"
+#endif
