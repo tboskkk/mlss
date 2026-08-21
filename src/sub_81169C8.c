31c177987dc01cb30f8b27880a2191b24135d5e0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-21 09:19:13 -0600

    Extract sub_8116D98
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

--- src/sub_81169C8.c
+++ src/sub_81169C8.c
@@ -41,3 +41,9 @@ asm_unified(".include \"asm/nonmatching/sub_8116CF4.s\"");
    NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
    still gets the verbatim retail bytes. */
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8116D98.s\"");
+#else
+#error "TODO: write sub_8116D98 to match asm/nonmatching/sub_8116D98.s, then delete this #error"
+#endif
