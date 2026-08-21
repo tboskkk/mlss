6623d6c7264b8249eb5707cd03eb778470b0c4e3
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:34:33 -0600

    Draft m2c seeds for sub_80F7CD0.c's #error placeholders
    Unblocks the whole translation unit (sub_80F7CD0, sub_80F7D0C). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80F7CD0.c
+++ src/sub_80F7CD0.c
@@ -10,11 +10,41 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F7CD0.s\"");
 #else
-#error "TODO: write sub_80F7CD0 to match asm/nonmatching/sub_80F7CD0.s, then delete this #error"
+s32 sub_807F708(s32, s32, s32, u16, s32);       /* extern */
+
+s32 sub_80F7CD0(s32 arg0, s32 arg1, void *arg2) {
+    s32 temp_r1_11;
+    s32 temp_r2_9;
+    s32 var_r5_17;
+
+    temp_r2_9 = (*(s32 *)((s8 *)(arg2) + (0)));
+    temp_r1_11 = 1 & temp_r2_9;
+    var_r5_17 = 0;
+    if (temp_r2_9 > 1) {
+        var_r5_17 = 0x7FFF;
+    }
+    sub_807F708(((s32) ((0 - temp_r1_11) | temp_r1_11) >> 0x1F) & 4, var_r5_17, (*(s32 *)((s8 *)(arg2) + (4))), (*(u16 *)((s8 *)(arg2) + (8))), (s32) (*(u16 *)((s8 *)(arg2) + (0xC))));
+    return 1;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F7D0C.s\"");
 #else
-#error "TODO: write sub_80F7D0C to match asm/nonmatching/sub_80F7D0C.s, then delete this #error"
+s32 sub_807F754(s32, s32, s32);                 /* extern */
+
+s32 sub_80F7D0C(s32 arg0, s32 arg1, void *arg2) {
+    s32 temp_r1_10;
+    s32 temp_r2_8;
+    s32 var_r1_16;
+
+    temp_r2_8 = (*(s32 *)((s8 *)(arg2) + (0)));
+    temp_r1_10 = 1 & temp_r2_8;
+    var_r1_16 = 0;
+    if (temp_r2_8 > 1) {
+        var_r1_16 = 0x7FFF;
+    }
+    sub_807F754(((s32) ((0 - temp_r1_10) | temp_r1_10) >> 0x1F) & 4, var_r1_16, (*(s32 *)((s8 *)(arg2) + (4))));
+    return 1;
+}
 #endif
