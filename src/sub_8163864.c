d009bb7b22f6ca2a517e345ba10e45cf7fefdcf5
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:45:32 -0600

    Draft m2c seeds for sub_8163864.c's #error placeholders
    Unblocks the whole translation unit (sub_8163864, sub_8163888). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_8163864.c
+++ src/sub_8163864.c
@@ -10,11 +10,31 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8163864.s\"");
 #else
-#error "TODO: write sub_8163864 to match asm/nonmatching/sub_8163864.s, then delete this #error"
+s32 sub_8161C9C(void *, s32, s32);              /* extern */
+
+void sub_8163864(void **arg0, s32 arg1, s32 arg2) {
+    void *var_r4_9;
+
+    var_r4_9 = *arg0;
+    if (var_r4_9 != NULL) {
+        do {
+            sub_8161C9C(var_r4_9, arg1, arg2);
+            var_r4_9 = (*(void **)((s8 *)(var_r4_9) + (8)));
+        } while (var_r4_9 != NULL);
+    }
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8163888.s\"");
 #else
-#error "TODO: write sub_8163888 to match asm/nonmatching/sub_8163888.s, then delete this #error"
+s32 sub_8163BE4(void *);                        /* extern */
+
+void sub_8163888(void *arg0, s32 arg1) {
+    (*(s32 *)((s8 *)(arg0) + (0xC))) = 0x08CDD108;
+    sub_8163BE4(arg0);
+    if (1 & arg1) {
+        free_heap_8018DA8(arg0);
+    }
+}
 #endif
