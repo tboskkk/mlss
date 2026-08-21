01b0bedfdce8e4d094683c4bfbcc6960e4de0717
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:31:51 -0600

    Draft m2c seeds for sub_80EA9B8.c's #error placeholders
    Unblocks the whole translation unit (sub_80EAA04). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_80EA9B8.c
+++ src/sub_80EA9B8.c
@@ -55,5 +55,36 @@ s32 sub_80EA9B8(s32 arg0, s32 *arg1, void *arg2)
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80EAA04.s\"");
 #else
-#error "TODO: write sub_80EAA04 to match asm/nonmatching/sub_80EAA04.s, then delete this #error"
+s32 sub_80EAA04(s32 arg0, s32 *arg1, void *arg2) {
+    s32 temp_r0_20;
+    s32 var_r2_17;
+    s32 var_r3_18;
+    u16 var_r5_10;
+
+    var_r5_10 = (*(u16 *)((s8 *)((void *)0x0300034C) + (0x28)));
+    if (((s32) (*(s32 *)((s8 *)(arg2) + (0))) >> 1) == 0) {
+        var_r5_10 = (*(u16 *)((s8 *)((void *)0x0300034C) + (0x2A)));
+    }
+    var_r2_17 = (*(s32 *)((s8 *)(arg2) + (8)));
+    var_r3_18 = var_r2_17;
+    temp_r0_20 = (*(s32 *)((s8 *)(arg2) + (4))) + 0xB;
+    switch (temp_r0_20) {                           /* irregular */
+    case 11:
+        var_r2_17 = 0;
+        break;
+    case 12:
+        var_r2_17 = ~var_r2_17;
+        var_r3_18 = var_r2_17;
+        break;
+    }
+    if ((*(s32 *)((s8 *)(arg2) + (0))) & 1) {
+        if ((var_r3_18 & var_r5_10) != var_r2_17) {
+            goto block_11;
+        }
+    } else if ((var_r3_18 & var_r5_10) == var_r2_17) {
+block_11:
+        *arg1 = (*(s32 *)((s8 *)(arg2) + (0xC)));
+    }
+    return 1;
+}
 #endif
