3a6d0e26daf713fb8807db1a3489a1ebe591a1ba
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 22:57:45 -0600

    Extract sub_808DDE0
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ src/sub_808DDE0.c
@@ -0,0 +1,14 @@
+#include "global.h"
+#include "common.h"
+
+// TODO: add other includes (a matching *.h, other subsystem headers) as
+// this file grows. Left minimal since split_func.py doesn't know yet what
+// sub_808DDE0 needs.
+
+asm_unified(".include \"asm/macros.inc\"");
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_808DDE0.s\"");
+#else
+#error "TODO: write sub_808DDE0 to match asm/nonmatching/sub_808DDE0.s, then delete this #error"
+#endif
