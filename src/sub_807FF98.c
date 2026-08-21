df5c4638a270b6635079ce4c907df678e5f8dfe8
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 18:04:38 -0600

    Draft m2c seeds for sub_807FF98.c's #error placeholders
    Unblocks the whole translation unit (sub_807FF98, sub_807FFB8, sub_807FFD8). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_807FF98.c
+++ src/sub_807FF98.c
@@ -10,17 +10,50 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_807FF98.s\"");
 #else
-#error "TODO: write sub_807FF98 to match asm/nonmatching/sub_807FF98.s, then delete this #error"
+void sub_807FF98(s32 arg0) {
+    void *temp_r0_9;
+    void *temp_r2_8;
+
+    temp_r2_8 = *(void **)0x03000FD8;
+    temp_r0_9 = (*(void **)((s8 *)(temp_r2_8) + (0x3C)));
+    (*(void **)((s8 *)(temp_r2_8) + (0x3C))) = (void *) (*(void **)((s8 *)(temp_r0_9) + (0)));
+    (*(void **)((s8 *)(temp_r0_9) + (0))) = (void *) (*(void **)((s8 *)(temp_r2_8) + (0x44)));
+    (*(s32 *)((s8 *)(temp_r0_9) + (4))) = arg0;
+    (*(s16 *)((s8 *)(temp_r0_9) + (0x10))) = 0;
+    (*(void **)((s8 *)(temp_r2_8) + (0x44))) = temp_r0_9;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_807FFB8.s\"");
 #else
-#error "TODO: write sub_807FFB8 to match asm/nonmatching/sub_807FFB8.s, then delete this #error"
+void sub_807FFB8(s32 arg0) {
+    void *temp_r0_9;
+    void *temp_r2_8;
+
+    temp_r2_8 = *(void **)0x03000FD8;
+    temp_r0_9 = (*(void **)((s8 *)(temp_r2_8) + (0x3C)));
+    (*(void **)((s8 *)(temp_r2_8) + (0x3C))) = (void *) (*(void **)((s8 *)(temp_r0_9) + (0)));
+    (*(void **)((s8 *)(temp_r0_9) + (0))) = (void *) (*(void **)((s8 *)(temp_r2_8) + (0x40)));
+    (*(s32 *)((s8 *)(temp_r0_9) + (4))) = arg0;
+    (*(s16 *)((s8 *)(temp_r0_9) + (0x10))) = 0;
+    (*(void **)((s8 *)(temp_r2_8) + (0x40))) = temp_r0_9;
+}
 #endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_807FFD8.s\"");
 #else
-#error "TODO: write sub_807FFD8 to match asm/nonmatching/sub_807FFD8.s, then delete this #error"
+void sub_807FFD8(void **arg0, s32 arg1) {
+    void *temp_r0_10;
+    void *temp_r3_9;
+
+    temp_r3_9 = *(void **)0x03000FD8;
+    temp_r0_10 = (*(void **)((s8 *)(temp_r3_9) + (0x3C)));
+    (*(void **)((s8 *)(temp_r3_9) + (0x3C))) = (void *) (*(void **)((s8 *)(temp_r0_10) + (0)));
+    (*(void **)((s8 *)(temp_r0_10) + (0))) = (void *) *arg0;
+    (*(s32 *)((s8 *)(temp_r0_10) + (4))) = arg1;
+    (*(s16 *)((s8 *)(temp_r0_10) + (0x10))) = 0;
+    *arg0 = temp_r0_10;
+}
 #endif
