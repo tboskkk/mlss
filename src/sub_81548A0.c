622abd631cbd6890f2ea46967a9cd0fc6c9c0e47
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 23:43:25 -0600

    Draft m2c seeds for sub_81548A0.c's #error placeholders
    Unblocks the whole translation unit (sub_81548A0). Seeds are compilable, not necessarily matching -- the normal permuter/validator pipeline takes it from here.

--- src/sub_81548A0.c
+++ src/sub_81548A0.c
@@ -10,5 +10,38 @@ asm_unified(".include \"asm/macros.inc\"");
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81548A0.s\"");
 #else
-#error "TODO: write sub_81548A0 to match asm/nonmatching/sub_81548A0.s, then delete this #error"
+s32 sub_81548A0(void *arg0, u16 arg2, s32 arg4) {
+    s32 temp_r0_77;
+    u8 temp_r0_12;
+
+    temp_r0_12 = (u8) arg4;
+    switch (temp_r0_12) {                           /* irregular */
+    case 5:
+        break;
+    case 3:
+        (*(s32 (**)(void *))((s8 *)((*(void **)((s8 *)(arg0) + (0x30)))) + (0x34)))(arg0 + M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */));
+        (*(u16 *)((s8 *)(arg0) + (0x18))) = 0xFE0CU;
+        (*(u8 *)((s8 *)(arg0) + (0x24))) = 1U;
+        break;
+    case 4:
+        (*(s32 (**)(void *))((s8 *)((*(void **)((s8 *)(arg0) + (0x30)))) + (0x3C)))(arg0 + M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */));
+        (*(u8 *)((s8 *)(arg0) + (0x24))) = 2U;
+        break;
+    default:
+        if ((*(u8 *)((s8 *)(arg0) + (0x24))) == 1) {
+            (*(u16 *)((s8 *)(arg0) + (0x14))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0x1C))) + (*(u16 *)((s8 *)(arg0) + (0x14))));
+            (*(u16 *)((s8 *)(arg0) + (0x18))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0x18))) + ((s16) arg2 + (*(u16 *)((s8 *)(arg0) + (0x20)))));
+            (*(s32 *)((s8 *)(arg0) + (4))) = (s32) ((*(s32 *)((s8 *)(arg0) + (4))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
+            temp_r0_77 = (*(s32 *)((s8 *)(arg0) + (8))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */);
+            (*(s32 *)((s8 *)(arg0) + (8))) = temp_r0_77;
+            if (temp_r0_77 > (s32) (*(s32 *)((s8 *)(arg0) + (0x3C)))) {
+                (*(s32 *)((s8 *)(arg0) + (4))) = (s32) (*(s32 *)((s8 *)(arg0) + (0x38)));
+                (*(s32 *)((s8 *)(arg0) + (8))) = (s32) (*(s32 *)((s8 *)(arg0) + (0x3C)));
+                (*(u16 *)((s8 *)(arg0) + (0x18))) = 0xFE0CU;
+            }
+        }
+        break;
+    }
+    return 0;
+}
 #endif
