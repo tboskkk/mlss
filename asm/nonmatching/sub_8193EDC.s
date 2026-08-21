df5ded7d6db3d7a3570576d0e1cb201b1b1cc76d
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-20 20:44:26 -0600

    Extract sub_8193EDC
    Factory pipeline (tools/factory) -- mechanical extraction via split_func.py, not yet matched.

---
+++ asm/nonmatching/sub_8193EDC.s
@@ -0,0 +1,308 @@
+	.syntax unified
+	.text
+
+	thumb_func_start sub_8193EDC
+sub_8193EDC:
+	push {r4, r5, r6, r7, lr}
+	mov r7, r10
+	mov r6, r9
+	mov r5, r8
+	push {r5, r6, r7}
+	add sp, #-0x038
+	mov r8, r0
+	ldr r0, [r0, #0x0C]
+	mov r9, r0
+	mov r1, r9
+	adds r1, #0x04
+	mov r9, r1
+	subs r1, #0x04
+	ldr r1, [r1, #0x00]
+	mov r10, r1
+	mov r2, r9
+	adds r2, #0x04
+	mov r9, r2
+	subs r2, #0x04
+	ldr r2, [r2, #0x00]
+	str r2, [sp, #0x01C]
+	mov r3, r8
+	ldr r0, [r3, #0x08]
+	ldr r1, [r0, #0x34]
+	str r1, [sp, #0x018]
+	bl sub_80841B8
+	str r0, [sp, #0x020]
+	movs r4, #0x00
+	str r4, [sp, #0x024]
+	ldr r0, [sp, #0x018]
+	ldr r1, [sp, #0x020]
+	bl sub_807F9A8
+	mov r7, sp
+	adds r7, #0x10
+	str r7, [sp, #0x02C]
+	mov r0, sp
+	adds r0, #0x14
+	str r0, [sp, #0x030]
+	mov r1, sp
+	adds r1, #0x18
+	str r1, [sp, #0x034]
+	movs r2, #0x07
+	str r2, [sp, #0x028]
+_08193F36:
+	mov r3, r9
+	ldm r3!, {r0}
+	str r0, [sp, #0x00C]
+	ldm r3!, {r1}
+	str r1, [sp, #0x010]
+	adds r3, #0x04
+	mov r9, r3
+	subs r3, #0x04
+	ldm r3!, {r2}
+	str r2, [sp, #0x014]
+	mov r4, r10
+	muls r4, r0
+	adds r0, r4, #0x0
+	cmp r0, #0x00
+	bge _08193F56
+	adds r0, #0xFF
+_08193F56:
+	asrs r3, r0, #0x08
+	str r3, [sp, #0x00C]
+	mov r0, r10
+	muls r0, r1
+	cmp r0, #0x00
+	bge _08193F64
+	adds r0, #0xFF
+_08193F64:
+	asrs r6, r0, #0x08
+	str r6, [sp, #0x010]
+	mov r0, r10
+	muls r0, r2
+	cmp r0, #0x00
+	bge _08193F72
+	adds r0, #0xFF
+_08193F72:
+	asrs r2, r0, #0x08
+	str r2, [sp, #0x014]
+	adds r1, r2, #0x0
+	ldr r7, _08194050 @ =0x08198584
+	adds r7, #0x40
+	movs r4, #0x00
+	ldsh r0, [r7, r4]
+	cmp r0, #0x00
+	bge _08193F86
+	adds r0, #0x3F
+_08193F86:
+	asrs r0, r0, #0x06
+	muls r1, r0
+	ldr r0, _08194054 @ =0x08198504
+	mov r12, r0
+	mov r5, r12
+	adds r5, #0x40
+	movs r4, #0x00
+	ldsh r0, [r5, r4]
+	cmp r0, #0x00
+	bge _08193F9C
+	adds r0, #0x3F
+_08193F9C:
+	asrs r0, r0, #0x06
+	muls r0, r3
+	subs r0, r1, r0
+	cmp r0, #0x00
+	bge _08193FA8
+	adds r0, #0xFF
+_08193FA8:
+	asrs r4, r0, #0x08
+	str r4, [sp, #0x014]
+	movs r1, #0x00
+	ldsh r0, [r7, r1]
+	cmp r0, #0x00
+	bge _08193FB6
+	adds r0, #0x3F
+_08193FB6:
+	asrs r0, r0, #0x06
+	adds r1, r3, #0x0
+	muls r1, r0
+	movs r3, #0x00
+	ldsh r0, [r5, r3]
+	cmp r0, #0x00
+	bge _08193FC6
+	adds r0, #0x3F
+_08193FC6:
+	asrs r0, r0, #0x06
+	muls r0, r2
+	adds r0, r1, r0
+	cmp r0, #0x00
+	bge _08193FD2
+	adds r0, #0xFF
+_08193FD2:
+	asrs r2, r0, #0x08
+	str r2, [sp, #0x00C]
+	adds r1, r6, #0x0
+	ldr r5, _08194050 @ =0x08198584
+	adds r5, #0x80
+	movs r7, #0x00
+	ldsh r0, [r5, r7]
+	cmp r0, #0x00
+	bge _08193FE6
+	adds r0, #0x3F
+_08193FE6:
+	asrs r0, r0, #0x06
+	muls r1, r0
+	mov r3, r12
+	adds r3, #0x80
+	movs r7, #0x00
+	ldsh r0, [r3, r7]
+	cmp r0, #0x00
+	bge _08193FF8
+	adds r0, #0x3F
+_08193FF8:
+	asrs r0, r0, #0x06
+	muls r0, r4
+	subs r0, r1, r0
+	cmp r0, #0x00
+	bge _08194004
+	adds r0, #0xFF
+_08194004:
+	asrs r0, r0, #0x08
+	str r0, [sp, #0x010]
+	movs r1, #0x00
+	ldsh r0, [r5, r1]
+	cmp r0, #0x00
+	bge _08194012
+	adds r0, #0x3F
+_08194012:
+	asrs r0, r0, #0x06
+	adds r1, r4, #0x0
+	muls r1, r0
+	movs r4, #0x00
+	ldsh r0, [r3, r4]
+	cmp r0, #0x00
+	bge _08194022
+	adds r0, #0x3F
+_08194022:
+	asrs r0, r0, #0x06
+	muls r0, r6
+	adds r0, r1, r0
+	cmp r0, #0x00
+	bge _0819402E
+	adds r0, #0xFF
+_0819402E:
+	asrs r0, r0, #0x08
+	str r0, [sp, #0x014]
+	mov r7, r8
+	ldrb r1, [r7, #0x1B]
+	movs r0, #0x80
+	ands r0, r1
+	cmp r0, #0x00
+	beq _08194058
+	movs r0, #0x12
+	ldsh r1, [r7, r0]
+	adds r0, r2, #0x0
+	cmp r0, #0x00
+	bge _0819404A
+	adds r0, #0xFF
+_0819404A:
+	asrs r0, r0, #0x08
+	subs r0, r1, r0
+	b _0819406A
+_08194050: .4byte 0x08198584
+_08194054: .4byte 0x08198504
+_08194058:
+	mov r3, r8
+	movs r4, #0x12
+	ldsh r1, [r3, r4]
+	adds r0, r2, #0x0
+	cmp r0, #0x00
+	bge _08194066
+	adds r0, #0xFF
+_08194066:
+	asrs r0, r0, #0x08
+	adds r0, r1, r0
+_0819406A:
+	str r0, [sp, #0x00C]
+	mov r7, r8
+	movs r0, #0x14
+	ldsh r1, [r7, r0]
+	ldr r0, [sp, #0x010]
+	cmp r0, #0x00
+	bge _0819407A
+	adds r0, #0xFF
+_0819407A:
+	asrs r0, r0, #0x08
+	adds r0, r1, r0
+	str r0, [sp, #0x010]
+	mov r2, r8
+	movs r3, #0x16
+	ldsh r1, [r2, r3]
+	ldr r0, [sp, #0x014]
+	cmp r0, #0x00
+	bge _0819408E
+	adds r0, #0xFF
+_0819408E:
+	asrs r0, r0, #0x08
+	adds r0, r1, r0
+	str r0, [sp, #0x014]
+	add r0, sp, #0x00C
+	ldr r1, [sp, #0x02C]
+	ldr r2, [sp, #0x030]
+	movs r3, #0x00
+	bl sub_807FBD8
+	ldr r2, [sp, #0x00C]
+	ldr r3, [sp, #0x010]
+	ldr r0, [sp, #0x014]
+	lsls r0, r0, #0x0E
+	str r0, [sp, #0x000]
+	movs r0, #0x00
+	str r0, [sp, #0x004]
+	str r0, [sp, #0x008]
+	ldr r0, [sp, #0x034]
+	ldr r1, [sp, #0x020]
+	bl sub_807C564
+	ldr r4, [sp, #0x024]
+	cmp r4, #0x00
+	bne _081940CA
+	ldr r0, _08194114 @ =0x03000D74
+	ldr r0, [r0, #0x00]
+	ldr r0, [r0, #0x38]
+	str r0, [sp, #0x018]
+	movs r7, #0x01
+	str r7, [sp, #0x024]
+_081940CA:
+	ldr r0, [sp, #0x028]
+	subs r0, #0x01
+	str r0, [sp, #0x028]
+	cmp r0, #0x00
+	blt _081940D6
+	b _08193F36
+_081940D6:
+	ldr r1, [sp, #0x024]
+	cmp r1, #0x00
+	beq _081940E4
+	ldr r0, _08194114 @ =0x03000D74
+	ldr r1, [r0, #0x00]
+	ldr r0, [sp, #0x018]
+	str r0, [r1, #0x38]
+_081940E4:
+	ldr r2, [sp, #0x01C]
+	add r10, r2
+	lsls r0, r2, #0x01
+	movs r1, #0x03
+	bl __divsi3
+	str r0, [sp, #0x01C]
+	mov r3, r8
+	ldr r3, [r3, #0x0C]
+	mov r9, r3
+	mov r4, r10
+	mov r7, r9
+	stm r7!, {r4}
+	ldr r0, [sp, #0x01C]
+	str r0, [r7, #0x00]
+	add sp, #0x038
+	pop {r3, r4, r5}
+	mov r8, r3
+	mov r9, r4
+	mov r10, r5
+	pop {r4, r5, r6, r7}
+	pop {r0}
+	bx r0
+	.byte 0x00, 0x00
+_08194114: .4byte 0x03000D74
