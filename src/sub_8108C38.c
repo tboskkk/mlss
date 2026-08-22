9f019fc680fadc07816bcf8b3847922d201c6504
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 10:33:15 -0600

    Match sub_8108C90
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8108C38.c
+++ src/sub_8108C38.c
@@ -35,16 +35,19 @@ void sub_8108C64(void *arg0) {
 }
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8108C90.s\"");
-#else
-void sub_8108C90(void *arg0) {
-    u16 temp_r1_11;
-
-    temp_r1_11 = (*(u16 *)((s8 *)(arg0) + (0x11E)));
-    if (8 & temp_r1_11) {
-        (*(u16 *)((s8 *)(arg0) + (0x11E))) = (u16) (0xFFF7 & temp_r1_11);
-        (*(s16 *)((s8 *)(arg0) + (0x10A))) = 0x100;
-    }
+void sub_8108C90(void *arg0)
+{
+  u16 temp_r1_11;
+  s8 *new_var2;
+  unsigned int new_var3;
+  int new_var;
+  temp_r1_11 = *((u16 *) (((s8 *) arg0) + 0x11E));
+  new_var3 = 0x100;
+  if (8 & temp_r1_11)
+  {
+    new_var = 0xFFF7;
+    new_var = new_var & temp_r1_11;
+    *((u16 *) (((s8 *) arg0) + 0x11E)) = (u16) new_var;
+    *((s16 *) ((new_var2 = (s8 *) arg0) + 0x10A)) = new_var3;
+  }
 }
-#endif
