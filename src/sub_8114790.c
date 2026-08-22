5147b53b55bb91c6f94b37034892c452e46f3d41
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 17:53:48 -0600

    Match sub_8114854
    Factory pipeline (tools/factory), source=rescore. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8114790.c
+++ src/sub_8114790.c
@@ -33,9 +33,6 @@ asm_unified(".include \"asm/nonmatching/sub_81147D0.s\"");
    still gets the verbatim retail bytes. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8114854.s\"");
-#else
 s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
 
 void sub_8114854(void *arg0) {
@@ -49,7 +46,6 @@ void sub_8114854(void *arg0) {
         (*(s32 *)((s8 *)(arg0) + (0x4C))) = 0;
     }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_811488C.s\"");
