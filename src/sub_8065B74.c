e35500637ca4fb9cc505eb5e1139e18907febdf2
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 17:59:10 -0600

    Match sub_8065BD8
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8065B74.c
+++ src/sub_8065B74.c
@@ -30,16 +30,12 @@ s32 sub_8065B74(void *arg0) {
 }
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8065BD8.s\"");
-#else
 void sub_8065BD8(void *arg0)
 {
-  unsigned int temp_r0_17;
+  u16 temp_r0_17;
   if (8 & (*((u8 *) (((s8 *) (*((void **) (((s8 *) arg0) + 8)))) + 0x12))))
   {
-    temp_r0_17 = (*((u16 *) (((s8 *) arg0) + 0xAC))) - 1;
-    *((u16 *) (((s8 *) arg0) + 0xAC)) = temp_r0_17;
+    temp_r0_17 = (*((u16 *) (((s8 *) arg0) + 0xAC)) = (*((u16 *) (((s8 *) arg0) + 0xAC))) - 1);
     if (((s32) (temp_r0_17 << 0x10)) > 0)
     {
       play_sfx_80195B4(0xAF, -1);
@@ -51,7 +47,6 @@ void sub_8065BD8(void *arg0)
     play_sfx_80195B4(0xD5, -1);
   }
 }
-#endif
 
 void sub_8065C34(void *arg0) {
     if (sub_8082B00() == 0) {
