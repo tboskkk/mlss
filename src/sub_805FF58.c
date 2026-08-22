3ee33371c91a99211d2842833cdd67de60a62390
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 17:56:48 -0600

    Match sub_806021C
    Factory pipeline (tools/factory), source=rescore. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_805FF58.c
+++ src/sub_805FF58.c
@@ -10,6 +10,9 @@ asm_unified(".include \"asm/macros.inc\"");
 
 
 
+
+void sub_80603D8(void *arg0);
+
 void sub_8060404(void *arg0);
 
 extern s32 sub_805F6BC;
@@ -59,14 +62,14 @@ asm_unified(".include \"asm/nonmatching/sub_80601D4.s\"");
    still gets the verbatim retail bytes. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_806021C.s\"");
-#else
-/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
-   the REST of this translation unit still builds and can be diffed under
-   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
-   still gets the verbatim retail bytes. */
-#endif
+void sub_806021C(void *arg0) {
+    (*(s32 *)((s8 *)(arg0) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x10))) + 0xFFFFFE9A);
+    if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
+        sub_8082E1C(arg0, 1, 0, 0);
+        (*(s16 *)((s8 *)(arg0) + (0xAC))) = 8;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = (s32 *) &sub_80603D8;
+    }
+}
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_806025C.s\"");
