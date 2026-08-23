d48daf875c81707b96ae202a9438675c1084b501
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 19:43:33 -0600

    Match sub_81580A4
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8155684.c
+++ src/sub_8155684.c
@@ -149,18 +149,22 @@ asm_unified(".include \"asm/nonmatching/sub_8158044.s\"");
    still gets the verbatim retail bytes. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81580A4.s\"");
-#else
 s32 sub_81649AC(s32, s32, s32, s32, s32);   /* extern */
-
-void sub_81580A4(void *arg0, s32 arg1) {
-    void *temp_r3_10;
-
-    temp_r3_10 = *(void **)0x03001018;
-    sub_81649AC((*(s32 *)((s8 *)(temp_r3_10) + (0x1CA8))), arg1, (s32) ((*(s32 *)((s8 *)((*(void **)((s8 *)(arg0) + (0x4C)))) + (4))) - (*(s32 *)((s8 *)(temp_r3_10) + (0x1CB8)))) >> 8, 0x50, 1);
+void sub_81580A4(void *arg0, s32 arg1)
+{
+  s32 new_var3;
+  s8 *new_var;
+  void *new_var2;
+  void *temp_r3_10;
+  if (temp_r3_10)
+  {
+  }
+  new_var2 = *((void **) (((s8 *) arg0) - -0x4C));
+  temp_r3_10 = *((void **) 0x03001018);
+  new_var3 = ((s32) ((*((s32 *) (((s8 *) new_var2) + 4))) - (*((s32 *) (((s8 *) temp_r3_10) + 0x1CB8))))) >> 8;
+  new_var = ((s8 *) temp_r3_10) + 0x1CA8;
+  sub_81649AC(*((s32 *) new_var), arg1, new_var3, 0x50, 1);
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_81580E0.s\"");
