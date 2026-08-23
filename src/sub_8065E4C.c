4167336cf875dd2b1f36ce640171a56864f5798b
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 09:24:04 -0600

    Match sub_8065ED4
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8065E4C.c
+++ src/sub_8065E4C.c
@@ -10,11 +10,10 @@ asm_unified(".include \"asm/macros.inc\"");
 
 s32 sub_8082E1C();
 
-extern s32 sub_8065ED4;
-
+void sub_8065ED4(void *arg0);
 void sub_8065E4C(void *arg0) {
     if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
-        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8065ED4;
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = (s32 *) &sub_8065ED4;
     }
 }
 
@@ -49,14 +48,23 @@ void sub_8065EA0(void *arg0)
   }
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8065ED4.s\"");
-#else
-/* Undraftable by m2c: its seed did not compile. Deliberately left empty so
-   the REST of this translation unit still builds and can be diffed under
-   NONMATCHING=1. The #ifndef branch above is unaffected -- the real ROM
-   still gets the verbatim retail bytes. */
-#endif
+void sub_8065ED4(void *arg0) {
+    u8 var_r0_8;
+    void *var_r1_9;
+
+    var_r0_8 = 0;
+    var_r1_9 = (*(void **)((s8 *)(arg0) + (0x30)));
+    if (var_r1_9 != NULL) {
+        do {
+            var_r0_8 += 1;
+            var_r1_9 = (*(void **)((s8 *)(var_r1_9) + (0x30)));
+        } while (var_r1_9 != NULL);
+    }
+    if (var_r0_8 == 0) {
+        sub_8082E1C(arg0, 0xC, 0, 0);
+        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_808750C;
+    }
+}
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8065F0C.s\"");
