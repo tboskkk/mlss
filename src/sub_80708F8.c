50ffaa17291309a5aa86762cc7a56806c7a5e315
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 10:13:29 -0600

    Match sub_80708F8
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80708F8.c
+++ src/sub_80708F8.c
@@ -10,21 +10,17 @@ asm_unified(".include \"asm/macros.inc\"");
 
 s32 sub_8082E1C();
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80708F8.s\"");
-#else
 extern s32 sub_8070938;
-
-void sub_80708F8(void *arg0) {
-    u16 temp_r0_11;
-
-    temp_r0_11 = (*(u16 *)((s8 *)(arg0) + (0xAC))) - 1;
-    (*(u16 *)((s8 *)(arg0) + (0xAC))) = temp_r0_11;
-    if ((s32) (temp_r0_11 << 0x10) <= 0) {
-        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8070938;
-    }
+void sub_80708F8(void *arg0)
+{
+  long temp_r0_11;
+  temp_r0_11 = (*((u16 *) (((s8 *) arg0) + 0xAC))) - 1;
+  *((u16 *) (((s8 *) arg0) + 0xAC)) = temp_r0_11;
+  if (((s32) (temp_r0_11 << 0x10)) <= 0)
+  {
+    *((s32 **) (((s8 *) arg0) + 0x4C)) = &sub_8070938;
+  }
 }
-#endif
 
 extern s32 sub_806F034;
 void sub_8070918(void *arg0)
