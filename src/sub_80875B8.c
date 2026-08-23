338ecd7f49fcacaf6330ab6a03f49daecfe7e0c7
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 23:32:15 -0600

    Match sub_80875B8
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80875B8.c
+++ src/sub_80875B8.c
@@ -7,19 +7,15 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80875B8.s\"");
-#else
 extern s32 sub_8086090;
 
 void sub_80875B8(void *arg0) {
-    if ((s32) M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) > 0) {
+    if ((s32) (s16) (*(u16 *)((s8 *)(arg0) + (0xAC))) > 0) {
         (*(u16 *)((s8 *)(arg0) + (0xAC))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0xAC))) - 1);
         return;
     }
     (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8086090;
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80875E0.s\"");
