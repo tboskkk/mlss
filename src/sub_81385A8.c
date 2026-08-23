9ca9df5aad35d6a06068942bffba1a7df1b73222
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 09:08:39 -0600

    Match sub_81385A8
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_81385A8.c
+++ src/sub_81385A8.c
@@ -7,30 +7,30 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81385A8.s\"");
-#else
-s32 sub_8138F1C(s32, s32, s32, s32);            /* extern */
-
-void sub_81385A8(void *arg0) {
-    s32 temp_r0_11;
-    s32 temp_r0_16;
-    s32 temp_r0_38;
-
-    temp_r0_11 = (*(s32 *)((s8 *)(arg0) + (0x25C)));
-    if (temp_r0_11 != -1) {
-        temp_r0_16 = temp_r0_11 + 1;
-        (*(s32 *)((s8 *)(arg0) + (0x25C))) = temp_r0_16;
-        temp_r0_38 = (sub_8138F1C(temp_r0_16, (*(s32 *)((s8 *)(arg0) + (0x1C0))), M2C_ERROR(/* unknown instruction: ldsh $r2, ($mem_loc_fictive_) */), (*(s32 *)((s8 *)(arg0) + (0x254)))) + (*(s32 *)((s8 *)(arg0) + (0x24C)))) - (*(s32 *)((s8 *)(arg0) + (0x14)));
-        (*(s32 *)((s8 *)(arg0) + (0x18))) = temp_r0_38;
-        if (temp_r0_38 <= 0) {
-            (*(s32 *)((s8 *)(arg0) + (0x18))) = 0;
-            (*(s32 *)((s8 *)(arg0) + (0x25C))) = -1;
-            (*(u8 *)((s8 *)(arg0) + (0x2B5))) = (u8) (-9 & (*(u8 *)((s8 *)(arg0) + (0x2B5))));
-        }
+s32 sub_8138F1C(s32, s32, s16, s32);                /* extern */
+void sub_81385A8(void *arg0)
+{
+  s32 temp_r0_11;
+  int new_var;
+  s32 temp_r0_16;
+  s32 temp_r0_38;
+  temp_r0_11 = *((s32 *) (((s8 *) arg0) + 0x25C));
+  if (temp_r0_11 != (-1))
+  {
+    temp_r0_16 = temp_r0_11 + 1;
+    temp_r0_38 += 0;
+    *((s32 *) (((s8 *) arg0) + 0x25C)) = temp_r0_16;
+    temp_r0_38 = (sub_8138F1C(temp_r0_16, *((s32 *) (((s8 *) arg0) + 0x1C0)), *((s16 *) (((s8 *) arg0) + 0x1BE)), *((s32 *) (((s8 *) arg0) + 0x254))) + (*((s32 *) (((s8 *) arg0) + 0x24C)))) - (*((s32 *) (((s8 *) arg0) + 0x14)));
+    *((s32 *) (((s8 *) arg0) + 0x18)) = temp_r0_38;
+    new_var = -9;
+    if (temp_r0_38 <= 0)
+    {
+      *((s32 *) (((s8 *) arg0) + 0x18)) = 0;
+      *((s32 *) (((s8 *) arg0) + 0x25C)) = -1;
+      *((u8 *) (((s8 *) arg0) + 0x2B5)) = (u8) (new_var & (*((u8 *) (((s8 *) arg0) + 0x2B5))));
     }
+  }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8138610.s\"");
