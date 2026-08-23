fe856744bf0d6ba8c13dc7ad6051d701cae7df70
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 18:42:53 -0600

    Match sub_809CA6C
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_809C9DC.c
+++ src/sub_809C9DC.c
@@ -28,21 +28,15 @@ asm_unified(".include \"asm/nonmatching/sub_809CA24.s\"");
    still gets the verbatim retail bytes. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_809CA6C.s\"");
-#else
-s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
 extern s32 sub_809C660;
-
-void sub_809CA6C(void *arg0) {
-    void *temp_r2_13;
-
-    sub_8082E1C(arg0, 2, -1, 0);
-    temp_r2_13 = (*(void **)((s8 *)(arg0) + (8)));
-    (*(u8 *)((s8 *)(temp_r2_13) + (0x12))) = (u8) ((-7 & (*(u8 *)((s8 *)(temp_r2_13) + (0x12)))) | 2);
-    (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_809C660;
+void sub_809CA6C(void *arg0)
+{
+  void *temp_r2_13;
+  sub_8082E1C(arg0, 2, -1, 0);
+  temp_r2_13 = *((void **) (((s8 *) arg0) + 8));
+  *((u8 *) (((s8 *) temp_r2_13) + 0x12)) = (u8) (((-7) & (*(((s8 *) temp_r2_13) + 0x12))) | 2);
+  *((s32 **) (((s8 *) arg0) + 0x4C)) = &sub_809C660;
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_809CA9C.s\"");
