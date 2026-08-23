5ae276d13b1e69dc0efcfff5d20cb7857e29b187
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 13:21:56 -0600

    Match sub_813BA80
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_813B9B4.c
+++ src/sub_813B9B4.c
@@ -7,6 +7,9 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
+
+s8 sub_8139F08();
+
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813B9B4.s\"");
 #else
@@ -75,36 +78,37 @@ block_7:
 }
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_813BA80.s\"");
-#else
-s8 sub_8139F08(s32, void *, s32);               /* extern */
+void sub_813BA80(s32 arg0, void *arg1, void *arg2)
+{
+  int new_var;
+  s32 var_r0_17;
+  u16 temp_r0_46;
+  u32 var_r0_25;
+  new_var = 0;
+  if (sub_8139F08(arg0, arg1, 0) != (-1))
+  {
+    var_r0_17 = *((s32 *) (((s8 *) arg2) + 8));
+    goto block_7;
+  }
+  if ((*((u16 *) (((s8 *) arg1) + 0x2DE))) == new_var)
+  {
+    var_r0_25 = 0;
+  }
+  else
+  {
+    *((s32 *) (((s8 *) arg1) + 0xC)) = (s32) ((*((s32 *) (((s8 *) arg1) + 0xC))) + (*((s16 *) (((s8 *) arg1) + 0x242))));
+    *((s32 *) (((s8 *) arg1) + 0x10)) = (s32) ((*((s32 *) (((s8 *) arg1) + 0x10))) + (*((s16 *) (((s8 *) arg1) + 0x244))));
+    temp_r0_46 = (*((u16 *) (((s8 *) arg1) + 0x2DE)) = (*((u16 *) (((s8 *) arg1) + 0x2DE))) - 1);
+    var_r0_25 = ((u32) (new_var - temp_r0_46)) >> 0x1F;
+  }
+  if (var_r0_25 == new_var)
+  {
+    var_r0_17 = *((s32 *) (((s8 *) arg2) + 4));
+    block_7:
+    *((s32 *) (((s8 *) arg2) + 0)) = var_r0_17;
 
-void sub_813BA80(s32 arg0, void *arg1, void *arg2) {
-    s32 var_r0_17;
-    u16 temp_r0_46;
-    u32 var_r0_25;
-
-    if (sub_8139F08(arg0, arg1, 0) != -1) {
-        var_r0_17 = (*(s32 *)((s8 *)(arg2) + (8)));
-        goto block_7;
-    }
-    if ((*(u16 *)((s8 *)(arg1) + (0x2DE))) == 0) {
-        var_r0_25 = 0;
-    } else {
-        (*(s32 *)((s8 *)(arg1) + (0xC))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0xC))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
-        (*(s32 *)((s8 *)(arg1) + (0x10))) = (s32) ((*(s32 *)((s8 *)(arg1) + (0x10))) + M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */));
-        temp_r0_46 = (*(u16 *)((s8 *)(arg1) + (0x2DE))) - 1;
-        (*(u16 *)((s8 *)(arg1) + (0x2DE))) = temp_r0_46;
-        var_r0_25 = (u32) (0 - temp_r0_46) >> 0x1F;
-    }
-    if (var_r0_25 == 0) {
-        var_r0_17 = (*(s32 *)((s8 *)(arg2) + (4)));
-block_7:
-        (*(s32 *)((s8 *)(arg2) + (0))) = var_r0_17;
-    }
+  }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813BAF0.s\"");
