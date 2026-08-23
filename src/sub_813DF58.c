31d054f8bf563afd0b14f6d891a9f981acf2b31a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 19:23:11 -0600

    Match sub_813DFF8
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_813DF58.c
+++ src/sub_813DF58.c
@@ -31,26 +31,22 @@ asm_unified(".include \"asm/nonmatching/sub_813DFA4.s\"");
    still gets the verbatim retail bytes. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_813DFF8.s\"");
-#else
 s32 sub_8138944(void *, s32);                   /* extern */
 s32 sub_8138FA4(void *, s32);                   /* extern */
 s32 sub_8139030(void *, s32);                   /* extern */
 s32 sub_81398C4(s32, void *);                   /* extern */
 extern s32 sub_8139420;
-
-void sub_813DFF8(s32 arg0, void *arg1) {
-    sub_8138944(arg1, 0);
-    sub_8138FA4(arg1, 0xB);
-    sub_8139030(arg1, 0);
-    (*(s32 (**)(s32, void *))((s8 *)(arg1) + (0x198))) = sub_81398C4;
-    (*(s32 **)((s8 *)(arg1) + (0x1A0))) = &sub_8139420;
-    (*(s32 **)((s8 *)(arg1) + (0x1A4))) = &sub_8139420;
-    (*(u8 *)((s8 *)(arg1) + (0x214))) = (u8) ((-8 & (*(u8 *)((s8 *)(arg1) + (0x214)))) | 4);
-    sub_81398C4(arg0, arg1);
+void sub_813DFF8(s32 arg0, void *arg1)
+{
+  sub_8138944(arg1, 0);
+  sub_8138FA4(arg1, 0xB);
+  sub_8139030(arg1, 0);
+  *((s32 (**)(s32, void *)) (((s8 *) arg1) + 0x198)) = sub_81398C4;
+  *((s32 **) (((s8 *) arg1) + 0x1A0)) = &sub_8139420;
+  *((s32 **) (((s8 *) arg1) + 0x1A4)) = &sub_8139420;
+  *((u8 *) (((s8 *) arg1) + 0x214)) = (u8) (((-8) & (*(((s8 *) arg1) + 0x214))) | 4);
+  sub_81398C4(arg0, arg1);
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813E05C.s\"");
