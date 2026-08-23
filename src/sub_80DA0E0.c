1cbb457b7e7e53d9f3fc193620e50c05b69d7b5e
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 19:03:42 -0600

    Match sub_80DA140
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80DA0E0.c
+++ src/sub_80DA0E0.c
@@ -38,24 +38,19 @@ void sub_80DA100(void *arg0)
   }
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80DA140.s\"");
-#else
-s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
 extern s32 sub_80DA208;
-
-void sub_80DA140(void *arg0) {
+void sub_80DA140(void *arg0)
+{
+  if ((*((s32 *) (((s8 *) arg0) + 0x80))) == 0)
+  {
     void *temp_r2_17;
-
-    if ((*(s32 *)((s8 *)(arg0) + (0x80))) == 0) {
-        sub_8082E1C(arg0, 3, 0, 0);
-        temp_r2_17 = (*(void **)((s8 *)(arg0) + (8)));
-        (*(u8 *)((s8 *)(temp_r2_17) + (0x12))) = (u8) ((-7 & (*(u8 *)((s8 *)(temp_r2_17) + (0x12)))) | 2);
-        (*(s16 *)((s8 *)(arg0) + (0xAC))) = 0x20;
-        (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_80DA208;
-    }
+    sub_8082E1C(arg0, 3, 0, 0);
+    temp_r2_17 = *((void **) (((s8 *) arg0) + 8));
+    *((u8 *) (((s8 *) temp_r2_17) + 0x12)) = (u8) (((-7) & (*(((s8 *) temp_r2_17) + 0x12))) | 2);
+    *((s16 *) (((s8 *) arg0) + 0xAC)) = 0x20;
+    *((s32 **) (((s8 *) arg0) + 0x4C)) = &sub_80DA208;
+  }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80DA180.s\"");
