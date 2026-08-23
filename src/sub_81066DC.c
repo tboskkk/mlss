7de3d43eb84d08c0e860e5ffb5223b12a061fd3c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 09:02:49 -0600

    Match sub_81066DC
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_81066DC.c
+++ src/sub_81066DC.c
@@ -7,27 +7,26 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81066DC.s\"");
-#else
 s32 sub_807F6D0();                              /* extern */
 extern s32 sub_81063A8;
-
-void sub_81066DC(void *arg0) {
-    void *temp_r1_18;
-
-    if (M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) != 0) {
-        (*(u16 *)((s8 *)(arg0) + (0x10))) = (u16) ((*(u16 *)((s8 *)(arg0) + (0x10))) - 1);
-        return;
-    }
-    temp_r1_18 = *(void **)0x03000FD8;
-    if (((*(s32 *)((s8 *)(temp_r1_18) + (0x27C))) | (*(s32 *)((s8 *)(temp_r1_18) + (0x280)))) == 0) {
-        sub_807F6D0();
-        (*(u16 *)((s8 *)(arg0) + (0x10))) = 0x20U;
-        (*(s32 **)((s8 *)(arg0) + (4))) = &sub_81063A8;
-    }
+void sub_81066DC(void *arg0)
+{
+  void *temp_r1_18;
+  s16 new_var;
+  new_var = (s16) (*((u16 *) (((s8 *) arg0) + 0x10)));
+  if (new_var != 0)
+  {
+    *((u16 *) (((s8 *) arg0) + 0x10)) = (u16) ((*((u16 *) (((s8 *) arg0) + 0x10))) - 1);
+    return;
+  }
+  temp_r1_18 = *((void **) 0x03000FD8);
+  if (((*((s32 *) (((s8 *) temp_r1_18) + 0x27C))) | (*((s32 *) (((s8 *) temp_r1_18) + 0x280)))) == 0)
+  {
+    sub_807F6D0();
+    *((u16 *) (((s8 *) arg0) + 0x10)) = 0x20U;
+    *((s32 **) (((s8 *) arg0) + 4)) = &sub_81063A8;
+  }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8106724.s\"");
