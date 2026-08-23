e4d1a9e59221bf3a73a8cab83857d9173fa7efe0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 03:04:28 -0600

    Match sub_80885C4
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_8088508.c
+++ src/sub_8088508.c
@@ -7,6 +7,9 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
+
+s32 sub_8082E1C();
+
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8088508.s\"");
 #else
@@ -56,27 +59,27 @@ void sub_8088560(void *arg0) {
 }
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80885C4.s\"");
-#else
-s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
 extern s32 sub_808864C;
-
-void sub_80885C4(void *arg0) {
-    s32 temp_r1_11;
-    void *temp_r2_39;
-
-    temp_r1_11 = 6 & (*(u8 *)((s8 *)(arg0) + (0x76)));
-    if ((temp_r1_11 == 2) || (temp_r1_11 == 4)) {
-        if (M2C_ERROR(/* unknown instruction: ldsh $r1, ($mem_loc_fictive_) */) == -1) {
-            sub_8082E1C(arg0, 4, 0x2000, 0);
-        } else {
-            sub_8082E1C(arg0, 4, 0x204D, 0);
-        }
-        temp_r2_39 = (*(void **)((s8 *)(arg0) + (8)));
-        (*(u8 *)((s8 *)(temp_r2_39) + (0x12))) = (u8) ((-7 & (*(u8 *)((s8 *)(temp_r2_39) + (0x12)))) | 2);
+void sub_80885C4(void *arg0)
+{
+  int new_var;
+  s32 temp_r1_11;
+  void *temp_r2_39;
+  temp_r1_11 = 6 & (*((u8 *) (((s8 *) arg0) + 0x76)));
+  new_var = -7;
+  if ((temp_r1_11 == 2) || (temp_r1_11 == 4))
+  {
+    if ((*((s16 *) (((s8 *) (*((void **) (((s8 *) arg0) + 0x28)))) + 0xEC))) == (-1))
+    {
+      sub_8082E1C(arg0, 4, 0x2000, 0);
     }
-    (*(s32 **)((s8 *)(arg0) + (0x50))) = (s32 *) (*(s32 **)((s8 *)(arg0) + (0x4C)));
-    (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_808864C;
+    else
+    {
+      sub_8082E1C(arg0, 4, 0x204D, 0);
+    }
+    temp_r2_39 = *((void **) (((s8 *) arg0) + 8));
+    *((u8 *) (((s8 *) temp_r2_39) + 0x12)) = (u8) ((new_var & (*((u8 *) (((s8 *) temp_r2_39) + 0x12)))) | 2);
+  }
+  *((s32 **) (((s8 *) arg0) + 0x50)) = (s32 *) (*((s32 **) (((s8 *) arg0) + 0x4C)));
+  *((s32 **) (((s8 *) arg0) + 0x4C)) = &sub_808864C;
 }
-#endif
