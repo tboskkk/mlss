832e348969a1ab832e1b2ae4f0d1d018fabb128c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 19:44:39 -0600

    Match sub_815F2C4
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_815F2C4.c
+++ src/sub_815F2C4.c
@@ -10,19 +10,19 @@ asm_unified(".include \"asm/macros.inc\"");
 
 s32 sub_8021308();
 s32 sub_815FB14();
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_815F2C4.s\"");
-#else
 void *sub_815FA3C(void *);                          /* extern */
-
-void sub_815F2C4(void *arg0, s32 arg1) {
-    (*(s16 *)((s8 *)(arg0) + (0x18))) = 0x64;
-    (*(s16 *)((s8 *)(arg0) + (0x20))) = (s16) (0 - arg1);
-    (*(s16 *)((s8 *)(arg0) + (0x38))) = 0;
-    (*(s16 *)((s8 *)(sub_815FA3C(arg0)) + (0xC))) = 0;
-    (*(s8 *)((s8 *)(arg0) + (0x24))) = 3;
+void sub_815F2C4(void *arg0, s32 arg1)
+{
+  s8 *new_var;
+  unsigned char new_var2;
+  new_var2 = 0;
+  new_var = (s8 *) arg0;
+  *((s16 *) (new_var + 0x18)) = 0x64;
+  *((s16 *) (new_var + 0x20)) = (s16) (new_var2 - arg1);
+  *((s16 *) (new_var + 0x38)) = 0;
+  *((s16 *) (((s8 *) sub_815FA3C(arg0)) + 0xC)) = 0;
+  *((s8 *) (new_var + 0x24)) = 3;
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_815F2E8.s\"");
