636d58aed5d0c7b13de6e078e10218674e475176
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-23 00:29:48 -0600

    Match sub_8158308
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- asm/nonmatching/sub_8158308.s
+++
@@ -1,9 +0,0 @@
-	.syntax unified
-	.text
-
-	thumb_func_start sub_8158308
-sub_8158308:
-	movs r1, #0x1C
-	ldsh r0, [r0, r1]
-	bx lr
-	.byte 0x00, 0x00
--- src/sub_8158308.c
+++ src/sub_8158308.c
@@ -7,13 +7,9 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8158308.s\"");
-#else
-s32 sub_8158308(s32 arg0) {
-    return M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */);
+s16 sub_8158308(void *arg0) {
+    return (*(s16 *)((s8 *)(arg0) + (0x1C)));
 }
-#endif
 
 s32 sub_8158258(void *);                        /* extern */
 
