4a0c77d4435ad3cfc4636cfa0d85fda4fb4a0963
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 19:12:52 -0600

    Match sub_80F0724
    Factory pipeline (tools/factory), source=permuter. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80F0724.c
+++ src/sub_80F0724.c
@@ -7,29 +7,21 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80F0724.s\"");
-#else
-void sub_80F0724(void* p0, void* p1)
+s32 sub_803E9F0(s32);                               /* extern */
+s32 sub_80F0724(void *arg0, void *arg1)
 {
-    u32 r4;
-    u32 r0;
-    
-    r4 = (u32)p1;
-    r0 = *(u32*)((u8*)p0 + 0x14);
-    sub_803E9F0(r0);
-    r0 <<= 0x18;
-    if (r0 == 0)
-    {
-        *(u32*)((u8*)p1) = *(u32*)((u8*)p1 + 0x14);
-        return;
-    }
-    else
-    {
-        return;
-    }
+  int new_var;
+  new_var = 1;
+  if ((sub_803E9F0(*((s32 *) (((s8 *) arg0) + 0x14))) << 0x18) == 0)
+  {
+    return new_var;
+  }
+  if (new_var)
+  {
+    *((s32 *) (((s8 *) arg1) + 0)) = (s32) (*((s32 *) (((s8 *) arg1) + 0x14)));
+  }
+  return 0;
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80F0744.s\"");
