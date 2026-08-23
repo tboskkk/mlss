7e2f57f31102beb1fafb0c35212f151c18155e9f
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 21:31:05 -0600

    Match sub_8050710
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80504BC.c
+++ src/sub_80504BC.c
@@ -174,21 +174,19 @@ void sub_80506D0(void *arg0) {
     }
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8050710.s\"");
-#else
 s32 sub_8040020();                              /* extern */
 
 void sub_8050710(void *arg0) {
+    void *temp_r1_18;
     void *temp_r4_12;
 
     sub_8040020();
     temp_r4_12 = (*(void **)((s8 *)(arg0) + (0x344)));
     if (temp_r4_12 != NULL) {
-        (*(s32 (**)(void *))((s8 *)((*(void **)((s8 *)(temp_r4_12) + (0x338)))) + (0x2C)))(temp_r4_12 + M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */));
+        temp_r1_18 = (*(void **)((s8 *)(temp_r4_12) + (0x338)));
+        (*(s32 (**)(void *))((s8 *)(temp_r1_18) + (0x2C)))(temp_r4_12 + (*(s16 *)((s8 *)(temp_r1_18) + (0x28))));
     }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8050740.s\"");
