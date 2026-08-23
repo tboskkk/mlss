a26c7d852acb108c1c3fcd077b57113bda1affef
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-22 22:36:53 -0600

    Match sub_8070A50
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80708F8.c
+++ src/sub_80708F8.c
@@ -7,6 +7,9 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
+
+s32 sub_8082E1C();
+
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80708F8.s\"");
 #else
@@ -61,10 +64,6 @@ asm_unified(".include \"asm/nonmatching/sub_80709E4.s\"");
    still gets the verbatim retail bytes. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8070A50.s\"");
-#else
-s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
 extern s32 sub_8070DCC;
 
 void sub_8070A50(void *arg0) {
@@ -73,7 +72,6 @@ void sub_8070A50(void *arg0) {
         (*(s32 **)((s8 *)(arg0) + (0x4C))) = &sub_8070DCC;
     }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_8070A7C.s\"");
