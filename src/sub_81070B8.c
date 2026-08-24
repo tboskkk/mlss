419e8c7bc1eb6a86877ddfaa7845ffdd9fddd833
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-24 15:04:51 -0600

    Match sub_81070B8
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_81070B8.c
+++ src/sub_81070B8.c
@@ -7,33 +7,41 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_81070B8.s\"");
-#else
 s32 sub_8082B00();                                  /* extern */
-void sub_80FD44C(void *arg0);                       /* extern */
-void sub_810124C(void *arg0);                       /* extern */
-
-void sub_81070B8(void *arg0) {
-    s32 *var_r0_37;
-    u8 temp_r2_11;
-    void *temp_r1_9;
-
-    temp_r1_9 = *(void **)0x03000FD8;
-    temp_r2_11 = (*(u8 *)((s8 *)(temp_r1_9) + (0xB)));
-    if (2 & temp_r2_11) {
-        (*(u8 *)((s8 *)(temp_r1_9) + (0xB))) = (u8) (-3 & temp_r2_11);
-        return;
+extern s32 sub_80FD44C;
+extern s32 sub_810124C;
+void sub_81070B8(void *arg0)
+{
+  u8 temp_r2_11;
+  s32 *var_r0_37;
+  int new_var2;
+  void *temp_r1_9;
+  void *temp_r3_10;
+  int new_var;
+  temp_r1_9 = *((void **) 0x03000FD8);
+  temp_r3_10 = *((void **) (((s8 *) temp_r1_9) + 0x7C));
+  ;
+  if ((*((u8 *) (((s8 *) temp_r1_9) + 0xB))) & 2)
+  {
+    new_var2 = 3;
+    *((u8 *) (((s8 *) temp_r1_9) + 0xB)) = (u8) ((-new_var2) & (*((u8 *) (((s8 *) temp_r1_9) + 0xB))));
+    return;
+  }
+  if ((*((s16 *) (((s8 *) temp_r3_10) + 0xF6))) != 0)
+  {
+    new_var = 0x7E;
+    if (!(0x38 & (*((u8 *) (((s8 *) temp_r3_10) + new_var)))))
+    {
+ do { var_r0_37 = &sub_80FD44C; } while (0);
+      goto block_7;
     }
-    if (M2C_ERROR(/* unknown instruction: ldsh $r0, ($mem_loc_fictive_) */) != 0) {
-        if (!(0x38 & (*(u8 *)((s8 *)((*(void **)((s8 *)(temp_r1_9) + (0x7C)))) + (0x7E))))) {
-            var_r0_37 = (s32 *) &sub_80FD44C;
-            goto block_7;
-        }
-    } else if ((sub_8082B00() << 0x18) == 0) {
-        var_r0_37 = (s32 *) &sub_810124C;
-block_7:
-        (*(s32 **)((s8 *)(arg0) + (4))) = var_r0_37;
-    }
+  }
+  else
+    if ((sub_8082B00() << 0x18) == 0)
+  {
+    var_r0_37 = &sub_810124C;
+    block_7:
+    *((s32 **) (((s8 *) arg0) + 4)) = var_r0_37;
+
+  }
 }
-#endif
