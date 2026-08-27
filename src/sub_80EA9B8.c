9f644aa3c26b691f294b3742d45e4b94e274dbd4
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-26 20:38:13 -0600

    Match sub_80EAA04
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80EA9B8.c
+++ src/sub_80EA9B8.c
@@ -52,39 +52,49 @@ s32 sub_80EA9B8(s32 arg0, s32 *arg1, void *arg2)
   return 1;
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80EAA04.s\"");
-#else
-s32 sub_80EAA04(s32 arg0, s32 *arg1, void *arg2) {
-    s32 temp_r0_20;
-    s32 var_r2_17;
-    s32 var_r3_18;
-    u16 var_r5_10;
-
-    var_r5_10 = (*(u16 *)((s8 *)((void *)0x0300034C) + (0x28)));
-    if (((s32) (*(s32 *)((s8 *)(arg2) + (0))) >> 1) == 0) {
-        var_r5_10 = (*(u16 *)((s8 *)((void *)0x0300034C) + (0x2A)));
-    }
-    var_r2_17 = (*(s32 *)((s8 *)(arg2) + (8)));
-    var_r3_18 = var_r2_17;
-    temp_r0_20 = (*(s32 *)((s8 *)(arg2) + (4))) + 0xB;
-    switch (temp_r0_20) {                           /* irregular */
+s32 sub_80EAA04(s32 arg0, s32 *arg1, void *arg2)
+{
+  s32 temp_r0_20;
+  s32 var_r2_17;
+  s8 *new_var;
+  s32 var_r3_18;
+  u16 var_r5_10;
+  new_var = (s8 *) ((void *) 0x0300034C);
+  var_r5_10 = *((u16 *) (new_var + 0x28));
+  if ((((s32) (*((s32 *) (((s8 *) arg2) + 0)))) >> 1) == 0)
+  {
+    var_r5_10 = *((u16 *) (new_var + 0x2A));
+  }
+  var_r2_17 = *((s32 *) (((s8 *) arg2) + 8));
+  var_r3_18 = var_r2_17;
+  temp_r0_20 = (*((s32 *) (((s8 *) arg2) + 4))) + 0xB;
+  switch (temp_r0_20)
+  {
     case 11:
-        var_r2_17 = 0;
-        break;
+      var_r2_17 = 0;
+      break;
+
     case 12:
-        var_r2_17 = ~var_r2_17;
-        var_r3_18 = var_r2_17;
-        break;
+      var_r2_17 = ~var_r2_17;
+      var_r3_18 = var_r2_17;
+      break;
+
+  }
+
+  if ((*((s32 *) (((s8 *) arg2) + 0))) & 1)
+  {
+    if ((var_r3_18 & var_r5_10) != var_r2_17)
+    {
+ do { } while (0);
+      goto block_11;
     }
-    if ((*(s32 *)((s8 *)(arg2) + (0))) & 1) {
-        if ((var_r3_18 & var_r5_10) != var_r2_17) {
-            goto block_11;
-        }
-    } else if ((var_r3_18 & var_r5_10) == var_r2_17) {
-block_11:
-        *arg1 = (*(s32 *)((s8 *)(arg2) + (0xC)));
-    }
-    return 1;
+  }
+  else
+    if ((var_r3_18 & var_r5_10) == var_r2_17)
+  {
+    block_11:
+    *arg1 = *((s32 *) (((s8 *) arg2) + 0xC));
+
+  }
+  return 1;
 }
-#endif
