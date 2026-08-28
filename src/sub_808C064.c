93a040cd0f9687aa781d82272518c44c16a3fa58
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-28 17:37:30 -0600

    Match sub_808C0E8
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_808C064.c
+++ src/sub_808C064.c
@@ -64,17 +64,11 @@ asm_unified(".include \"asm/nonmatching/sub_808C0DA.s\"");
    counts this as unmatched. Write the C here, replacing this comment. */
 #endif
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_808C0E8.s\"");
-#else
-s32 sub_8087540(void *);                        /* extern */
-
 void sub_808C0E8(void *arg0) {
     if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
         sub_8087540(arg0);
     }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_808C102.s\"");
