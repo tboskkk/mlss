23b54b89b9246dad1c5dad1364f47927af0abc85
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-28 20:58:40 -0600

    Match sub_80D2EF0
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_80D2D28.c
+++ src/sub_80D2D28.c
@@ -132,36 +132,33 @@ void sub_80D2E7C(void *arg0)
   }
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_80D2EF0.s\"");
-#else
-s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
-s32 sub_8087CE4(void *);                        /* extern */
-void sub_80CD18C(void *arg0);                       /* extern */
-
-void sub_80D2EF0(void *arg0) {
-    s32 temp_r1_26;
-    void *temp_r2_38;
-    void *temp_r2_44;
-    void *temp_r4_10;
-    void *temp_r5_12;
-
-    temp_r4_10 = (*(void **)((s8 *)(*(void **)0x03000FD8) + (0x70)));
-    temp_r5_12 = temp_r4_10 + 8;
-    sub_8087CE4(temp_r5_12);
-    if (0x20 & (*(u8 *)((s8 *)(temp_r4_10) + (0x81)))) {
-        temp_r1_26 = 6 & (*(u8 *)((s8 *)(temp_r4_10) + (0x7E)));
-        if ((temp_r1_26 == 2) || (temp_r1_26 == 4)) {
-            sub_8082E1C(temp_r5_12, 0, -1, 0);
-            temp_r2_38 = (*(void **)((s8 *)(temp_r5_12) + (8)));
-            (*(u8 *)((s8 *)(temp_r2_38) + (0x11))) = (u8) (-0x41 & (*(u8 *)((s8 *)(temp_r2_38) + (0x11))));
-            temp_r2_44 = (*(void **)((s8 *)(temp_r5_12) + (8)));
-            (*(u8 *)((s8 *)(temp_r2_44) + (0x12))) = (u8) (-7 & (*(u8 *)((s8 *)(temp_r2_44) + (0x12))));
-        }
-        (*(s32 **)((s8 *)(arg0) + (0x4C))) = (s32 *) &sub_80CD18C;
+extern s32 sub_80CD18C;
+void sub_80D2EF0(void *arg0)
+{
+  s32 temp_r1_26;
+  void *temp_r2_38;
+  void *temp_r2_44;
+  void *temp_r4_10;
+  int new_var;
+  void *temp_r5_12;
+  temp_r4_10 = (void *) (*((void **) (((s8 *) (*((void **) 0x03000FD8))) + 0x70)));
+  temp_r5_12 = (void *) (temp_r4_10 + 8);
+  sub_8087CE4(temp_r5_12);
+  if (0x20 & (*((u8 *) (((s8 *) temp_r4_10) + 0x81))))
+  {
+    new_var = -7;
+    temp_r1_26 = 6 & (*((u8 *) (((s8 *) temp_r4_10) + 0x7E)));
+    if ((temp_r1_26 == 2) || (temp_r1_26 == 4))
+    {
+      sub_8082E1C(temp_r5_12, 0, -1, 0);
+      temp_r2_38 = (void *) (*((void **) (((s8 *) temp_r5_12) + 8)));
+      *((u8 *) (((s8 *) temp_r2_38) + 0x11)) = (u8) ((-0x41) & (*(((s8 *) temp_r2_38) + 0x11)));
+      temp_r2_44 = (void *) (*((void **) (((s8 *) temp_r5_12) + 8)));
+      *((u8 *) (((s8 *) temp_r2_44) + 0x12)) = (u8) (new_var & (*((u8 *) (((s8 *) temp_r2_44) + 0x12))));
     }
+    *((s32 **) (((s8 *) arg0) + 0x4C)) = &sub_80CD18C;
+  }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_80D2F5C.s\"");
