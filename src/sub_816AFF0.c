d0421ae868adf223aa072c94796e26a84309770c
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-28 19:01:02 -0600

    Match sub_816B000
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_816AFF0.c
+++ src/sub_816AFF0.c
@@ -11,15 +11,15 @@ void sub_816AFF0(void *arg0) {
     (*(s32 *)((s8 *)(arg0) + (0x490))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0x490))) | 8);
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_816B000.s\"");
-#else
-void sub_816B000(void *arg0) {
-    if (8 & (*(u16 *)((s8 *)((void *)0x0300034C) + (0x2A)))) {
-        (*(s32 *)((s8 *)(arg0) + (0x474))) = 4;
-    }
+void sub_816B000(void *arg0)
+{
+  int new_var;
+  new_var = 0x2A;
+  if (8 & (*((u16 *) (((s8 *) ((void *) 0x0300034C)) + new_var))))
+  {
+    *((s32 *) (((s8 *) arg0) + 0x474)) = 4;
+  }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_816B024.s\"");
