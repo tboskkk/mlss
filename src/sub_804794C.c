aeb3fcdae14f6e3bcf142eebe5218737b8a9e43a
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-30 12:53:10 -0600

    Match sub_8047958
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_804794C.c
+++ src/sub_804794C.c
@@ -11,13 +11,12 @@ void sub_804794C(void *arg0, s16 arg1) {
     (*(s16 *)((s8 *)(arg0) + (0x23E))) = arg1;
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_8047958.s\"");
-#else
-void sub_8047958(void *arg0) {
-    (*(u8 *)((s8 *)(arg0) + (0x2B6))) = (u8) (-0x31 & (*(u8 *)((s8 *)(arg0) + (0x2B6))));
+void sub_8047958(void *arg0)
+{
+  u8 *new_var;
+  new_var = (u8 *) (((s8 *) arg0) + 0x2B6);
+  *new_var = (u8) ((-0x31) & (*(((s8 *) arg0) + 0x2B6)));
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_804796C.s\"");
