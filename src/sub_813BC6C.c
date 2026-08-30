1b1aced85b4eb0baee3eb5a814eea85622cfaec9
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-30 02:45:04 -0600

    Match sub_813BC6C
    Factory pipeline (tools/factory), source=m2c. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_813BC6C.c
+++ src/sub_813BC6C.c
@@ -7,14 +7,10 @@
 
 asm_unified(".include \"asm/macros.inc\"");
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_813BC6C.s\"");
-#else
 void sub_813BC6C(void *arg0, void *arg1) {
     (*(s32 *)((s8 *)(arg1) + (0))) = (s32) ((*(s32 *)((s8 *)(arg0) + (0))) << 8);
     (*(s32 *)((s8 *)(arg1) + (4))) = (s32) ((*(s32 *)((s8 *)(arg0) + (4))) << 8);
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_813BC7A.s\"");
