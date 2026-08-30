a75df8c00ecd09481c22eb110c8911e0e91dd5a0
tboskkk <mmorpgmakertboskk@gmail.com>
2026-08-30 08:48:53 -0600

    Match sub_806C1C8
    Factory pipeline (tools/factory), source=tier2. Verified with a from-scratch build before commit, per the project's standing rule.

--- src/sub_806C1A8.c
+++ src/sub_806C1A8.c
@@ -28,18 +28,12 @@ void sub_806C1A8(struct Entity *arg0) {
     }
 }
 
-#ifndef NONMATCHING
-asm_unified(".include \"asm/nonmatching/sub_806C1C8.s\"");
-#else
-s32 sub_8082E1C(void *, s32, s32, s32);         /* extern */
-
 void sub_806C1C8(void *arg0) {
     if (8 & (*(u8 *)((s8 *)((*(void **)((s8 *)(arg0) + (8)))) + (0x12)))) {
         sub_8082E1C(arg0, 0, 0, 0);
         (*(s32 *)((s8 *)(arg0) + (0x4C))) = 0;
     }
 }
-#endif
 
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/sub_806C1EE.s\"");
